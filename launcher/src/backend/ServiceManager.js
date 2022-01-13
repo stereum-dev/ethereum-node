1d4e7136b03d86415842c925d207b5b1fd350271
stefa2k <stefan.kobrc@rocklogic.at>
2022-01-13 09:09:41 +0100

    launcher: ServiceManager cleanup

--- launcher/src/backend/ServiceManager.js
+++ launcher/src/backend/ServiceManager.js
@@ -33,8 +33,6 @@ export class ServiceManager {
                         },
                     },
                 },
-            }).then(res => {
-                return res;
             });
     }
 
