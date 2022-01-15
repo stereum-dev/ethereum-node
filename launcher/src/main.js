8649b51062cab3366974612fed5a80e9e9b54583
mabasian <54101509+mabasian@users.noreply.github.com>
2022-01-16 00:19:47 +0100

    make ui
    make a Trapezium on ui design and add this to the main js for use every wher

--- launcher/src/main.js
+++ launcher/src/main.js
@@ -1,9 +1,10 @@
 import Vue, { createApp } from "vue"; // CHANGE
 import App from "./App.vue";
 
+import router from "./router/index.js";
 import BaseButton from "./components/UI/BaseButton.vue";
 import BackgroundPage from "./components/layers/BackgroundPage.vue";
-import router from "./router/index.js";
+import TheTrapezium from './components/UI/TheTrapezium.vue';
 //import store from "./store/index.js";
 //import ControlService from "@/store/ControlService";
 Vue.configureCompat({ WATCH_ARRAY: false });
@@ -13,6 +14,7 @@ const app = createApp(App);
 
 app.component("base-button", BaseButton);
 app.component("background-page", BackgroundPage);
+app.component("the-trapezium",TheTrapezium);
 
 app.use(router);
 
