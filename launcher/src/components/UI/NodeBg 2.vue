602a04a836066b6a1d5705783b5de6fb7f739b65
mabasian <54101509+mabasian@users.noreply.github.com>
2022-01-17 22:41:57 +0100

    update

---
+++ launcher/src/components/UI/NodeBg 2.vue
@@ -0,0 +1,32 @@
+<template>
+  
+   <section id="main">
+        <section id="container">
+            <h1>Test</h1>
+        </section>
+    </section>
+   
+</template>
+
+<style scoped>
+#body{
+margin: 0;
+padding: 0;}
+#main{
+    width: 100%;
+    height: 100vh;
+    background:#336666;
+    overflow: hidden;
+   
+}
+#container{
+    position: relative;
+    width: 97%;
+    height: 90%;
+    margin: 4% auto;
+    background: #000;  
+    border: 5px solid #4C4848;
+    box-sizing: border-box;
+    border-radius: 40px;
+}
+</style>
---
+++ launcher/src/pages/TheNode 2.vue
@@ -0,0 +1,10 @@
+<template>
+   <section id="main"> <node-bg></node-bg></section>
+</template>
+
+<style scoped>
+#main{
+padding: 0;
+margin: 0;
+}
+</style>
