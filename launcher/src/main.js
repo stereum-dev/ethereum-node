deb955bcdc5a00d2a4be2b569ad777d758823409
mabasian <54101509+mabasian@users.noreply.github.com>
2022-01-18 15:42:28 +0100

    node header js
    node header as a base component

--- launcher/src/main.js
+++ launcher/src/main.js
@@ -6,6 +6,7 @@ import BaseButton from "./components/UI/BaseButton.vue";
 import BackgroundPage from "./components/layers/BackgroundPage.vue";
 import TheTrapezium from './components/UI/TheTrapezium.vue';
 import NodeBg from './components/UI/NodeBg.vue';
+import NodeHeader from 'launcher/src/components/layers/NodeHeader'
 //import store from "./store/index.js";
 //import ControlService from "@/store/ControlService";
 Vue.configureCompat({ WATCH_ARRAY: false });
@@ -17,6 +18,7 @@ app.component("base-button", BaseButton);
 app.component("background-page", BackgroundPage);
 app.component("the-trapezium",TheTrapezium);
 app.component("node-bg",NodeBg);
+app.component("node-header",NodeHeader);
 
 app.use(router);
 
