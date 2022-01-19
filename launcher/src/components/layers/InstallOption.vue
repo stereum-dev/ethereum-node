a36a3ecd0a712db09b4f7cdbc9e260326b1abaed
mabasian <54101509+mabasian@users.noreply.github.com>
2022-01-19 16:16:15 +0100

    installation option stracture
    one click changed

---
+++ launcher/src/components/layers/InstallOption.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="container">
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum illo
+        laudantium qui facilis doloremque dolorem impedit totam nihil ullam
+        omnis maxime ipsa blanditiis odio quidem at alias, quos distinctio
+        dolor!
+      </p>
+      <hr>
+      <div class="row">
+        <div class="boxes"></div>
+        <div class="boxes"></div>
+        <div class="boxes"></div>
+        <div class="boxes"></div>
+      </div>
+      <div class="row">
+        <div class="boxes"></div>
+        <div class="boxes"></div>
+        <div class="boxes"></div>
+        <div class="boxes"></div>
+      </div>
+      <hr>
+    </div>
+</template>
+
+<style scoped>
+.container {
+  border: 1px solid red;
+  width:60%;
+  height: 50vh;
+  margin: auto;
+  background: yellow;
+  border-radius: 40px;
+}
+.row{
+    background: blue;
+    width: 100%;
+    height: 30%;
+    border: 2px solid green;
+    box-sizing: border-box;
+    margin: 3% 0;
+}
+.boxes{
+    width: 22%;
+    height: 100%;
+    background: magenta;
+    box-sizing: border-box;
+    display: inline-block;
+    vertical-align: middle;
+    margin: 0 1.3%;
+}
+.container hr {
+    width: 80%;
+    color: #494949;
+    height: 2px;
+    background: grey;
+  }
+</style>
--- launcher/src/pages/ClickInstall.vue
+++ launcher/src/pages/ClickInstall.vue
@@ -1,14 +1,14 @@
 <template>
   <background-page>
-    <one-click></one-click>
+    <install-option></install-option>
   </background-page>
 </template>
 <script>
 import BackgroundPage from "../components/layers/BackgroundPage.vue";
-import OneClick from "../components/layers/OneClick.vue";
+import InstallOption from "../components/layers/InstallOption.vue";
 
 export default {
-  components: { BackgroundPage, OneClick },
+  components: { BackgroundPage, InstallOption },
 };
 </script>
 <style scoped></style>
