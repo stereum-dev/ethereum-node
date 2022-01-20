63343b7a11debbb824dba062aa04623190d52eb0
mabasian <54101509+mabasian@users.noreply.github.com>
2022-01-20 15:31:51 +0100

    right side bar added to node with dummy style

--- launcher/src/components/UI/RightNode.vue
+++ launcher/src/components/UI/RightNode.vue
@@ -1,12 +1,19 @@
 <template>
+  <div>
     <div id="container"></div>
+  </div>
 </template>
 
 <style>
-#container{
-    background: red;
-    border-left: #808080;
-    width: 50vw;
-    height: 50vh;
+#container {
+  background: red;
+  border-left: 3px solid #808080;
+  width: 15%;
+  height: 88.8%;
+  right: 0.6%;
+  box-sizing: border-box;
+  position: absolute;
+  border-radius: 0 0 40px 0;
+  top: 9%;
 }
 </style>
--- launcher/src/pages/TheNode.vue
+++ launcher/src/pages/TheNode.vue
@@ -1,31 +1,29 @@
 <template>
   <section id="parent">
-    <node-bg><right-node id="sider"></right-node></node-bg><node-header id="head"></node-header>
+    <node-bg> </node-bg>
+    <node-header id="head"></node-header>
+    <right-node > </right-node>
   </section>
 </template>
 
 <script>
-import RightNode from '../components/UI/RightNode.vue';
+import RightNode from "../components/UI/RightNode.vue";
 export default {
-  components:{RightNode}
-}
+  components: { RightNode },
+};
 </script>
 
 <style scoped>
 #parent {
   position: relative;
-  
   box-sizing: border-box;
 }
+
 #head {
   position: absolute;
   top: 0;
 }
 
+
 /*baraye test*/
-#sider{
-  position: absolute;
-  right: 0;
-  top: 0;
-}
 </style>
