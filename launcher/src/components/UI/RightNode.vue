2bd204b8a2e1da47262a86010dc0ec91ee5b784f
mabasian <54101509+mabasian@users.noreply.github.com>
2022-01-20 12:17:59 +0100

    right side bar of node page component added

---
+++ launcher/src/components/UI/RightNode.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="container"></div>
+</template>
+
+<style>
+#container{
+    background: red;
+    border-left: #808080;
+    width: 50vw;
+    height: 50vh;
+}
+</style>
--- launcher/src/pages/TheNode.vue
+++ launcher/src/pages/TheNode.vue
@@ -1,15 +1,31 @@
 <template>
   <section id="parent">
-    <node-bg></node-bg><node-header id="head"></node-header>
+    <node-bg><right-node id="sider"></right-node></node-bg><node-header id="head"></node-header>
   </section>
 </template>
+
+<script>
+import RightNode from '../components/UI/RightNode.vue';
+export default {
+  components:{RightNode}
+}
+</script>
 
 <style scoped>
 #parent {
   position: relative;
+  
+  box-sizing: border-box;
 }
 #head {
   position: absolute;
+  top: 0;
+}
+
+/*baraye test*/
+#sider{
+  position: absolute;
+  right: 0;
   top: 0;
 }
 </style>
