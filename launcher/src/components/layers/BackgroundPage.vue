1428f8a266720e1a7dd22e54161860162abd662b
mabasian <54101509+mabasian@users.noreply.github.com>
2022-01-22 18:18:03 +0100

    update

--- launcher/src/components/layers/BackgroundPage.vue
+++ launcher/src/components/layers/BackgroundPage.vue
@@ -1,14 +1,14 @@
 <template>
-  <section class="body">
-    <section id="main">
+
+    <div id="main">
       <section id="container">
         <section>
           <img src="stereum_logo_extern.png" alt="" />
           <slot></slot>
         </section>
       </section>
-    </section>
-  </section>
+    </div>
+
 </template>
 
 <script>
@@ -16,26 +16,29 @@ export default {};
 </script>
 
 <style scoped>
-.body {
-  margin: 0;
-  padding: 0;
-}
+
 #container {
   border-radius: 40px;
-
-  padding: 1rem;
-  margin: 2rem auto;
-  width: 90vw;
-  height: 90vh;
+box-sizing: border-box;
+top: 0;
+left: 0;
+  margin: auto;
+  width: 95%;
+  height: 95%;
   background-color: #336666;
+  position: relative;
 }
 #main {
   background-color: #000;
-
-  justify-content: center;
-  align-items: center;
-  height: 97vh;
-  display: flex;
+width: 100%;
+height: 100%;
+position: fixed;
+z-index: -1;
+top: 0;
+left: 0;
+box-sizing: border-box;
+padding: 0;
+margin: 0;
 }
 
 img {
