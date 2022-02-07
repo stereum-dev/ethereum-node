de0ef057686081b392de89c8f8e5127dca882a7d
maxbehzadi82 <69126271+maxbehzadi82@users.noreply.github.com>
2022-02-07 23:44:35 +0100

    Merge branch 'main' of https://github.com/maxbehzadi82/ethereum-node

--- launcher/src/components/UI/NodeDrag.vue
+++ launcher/src/components/UI/NodeDrag.vue
@@ -8,6 +8,7 @@
   </div>
 </template>
 
+<<<<<<< HEAD
 <style scoped>
 .drag-node {
   padding: 5px;
@@ -27,3 +28,25 @@
   resize: both;
 }
 </style>
+=======
+
+.drag-node {
+    padding: 5px;
+    display: flex;
+    box-sizing: border-box;
+}
+
+.drag_input {
+    background: orange;
+    color: white;
+    padding: 10px;
+    border: 5px solid black;
+    margin: 5px;
+    width: 20%;
+    height: 3rem;
+    border-radius: 10px;
+    resize: both;
+}
+</style>
+
+>>>>>>> a27a790f384b9afb72d068349f032dfc25b518e6
--- launcher/src/components/UI/NodeGrid.vue
+++ launcher/src/components/UI/NodeGrid.vue
@@ -1,4 +1,5 @@
 <template>
+
   <div class="nodeparent">
     <div class="execution">
       <the-trapezium><node-drag></node-drag></the-trapezium>
@@ -14,6 +15,28 @@
     <div class="sidebar">SIDEBAR</div>
     <div class="footer">FOOTER</div>
   </div>
+    <div class="nodeparent">
+        <div class="execution">
+            <the-trapezium>
+                <node-drag></node-drag>
+            </the-trapezium>
+        </div>
+        <div class="consensus">
+            <the-trapezium>
+                <node-drag></node-drag>
+            </the-trapezium>
+        </div>
+        <div class="validator">
+            <the-trapezium>
+                <node-drag></node-drag>
+            </the-trapezium>
+        </div>
+        <div class="nodesetup">NODE SETUP</div>
+        <div class="service">SERVICE</div>
+        <div class="sidebar">SIDEBAR</div>
+        <div class="footer">FOOTER</div>
+    </div>
+
 </template>
 
 <script>
@@ -21,19 +44,20 @@ import TheTrapezium from "./TheTrapezium.vue";
 import NodeDrag from "./NodeDrag.vue";
 import NodeSetupConfig from "../layers/NodeSetupConfig.vue";
 export default {
-  components: { TheTrapezium, NodeDrag, NodeSetupConfig },
+
 };
 </script>
 
 <style scoped>
 .nodeparent {
-  display: grid;
-  height: 93%;
-  grid-template-columns: 70% 20% 10%;
-  grid-template-rows: repeat(4, 23.75%) 5%;
-  position: relative;
-  top: 7%;
-  margin: 0;
+    display: grid;
+    height: 93%;
+    grid-template-columns: 70% 20% 10%;
+    grid-template-rows: repeat(4, 23.75%) 5%;
+    grid-row-gap: 2px;
+    position: relative;
+    top: 7%;
+    margin: 0;
 }
 
 .execution {
@@ -59,38 +83,60 @@ export default {
   grid-column-end: 2;
   border: 2px dotted red;
   align-self: center;
+
+
+    color: white;
+    grid-column-start: 1;
+    grid-column-end: 2;
+    align-self: center;
+}
+
+.consensus {
+    color: red;
+    grid-column-start: 1;
+    grid-column-end: 2;
+    align-self: center;
+}
+
+.validator {
+    color: white;
+    grid-column-start: 1;
+    grid-column-end: 2;
+    align-self: center;
+
+
 }
 
 .nodesetup {
-  background: orange;
-  color: white;
-  grid-column-start: 1;
-  grid-column-end: 2;
+    background: orange;
+    color: white;
+    grid-column-start: 1;
+    grid-column-end: 2;
 }
 
 .service {
-  background: magenta;
-  color: white;
-  grid-row-start: 1;
-  grid-row-end: 5;
-  grid-column-start: 2;
-  grid-column-end: 3;
+    background: magenta;
+    color: white;
+    grid-row-start: 1;
+    grid-row-end: 5;
+    grid-column-start: 2;
+    grid-column-end: 3;
 }
 
 .sidebar {
-  background: green;
-  color: white;
-  grid-row-start: 1;
-  grid-row-end: 5;
-  grid-column-start: 3;
-  grid-column-end: 4;
+    background: green;
+    color: white;
+    grid-row-start: 1;
+    grid-row-end: 5;
+    grid-column-start: 3;
+    grid-column-end: 4;
 }
 
 .footer {
-  background: purple;
-  color: white;
-  grid-column-start: 1;
-  grid-column-end: 4;
-  border-radius: 0 0 25px 25px;
+    background: purple;
+    color: white;
+    grid-column-start: 1;
+    grid-column-end: 4;
+    border-radius: 0 0 25px 25px;
 }
 </style>
