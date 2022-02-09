8af2154aebd677528c96e808c333581015163d43
mabasian <54101509+mabasian@users.noreply.github.com>
2022-02-09 12:36:56 +0100

    ADD: arrow btn tp service plugin (#66)
    * change to vue3
    
    without any errors
    
    * new Update
    
    it's a last dbug, with the better css
    
    * Update OneClick.vue
    
    page of component one click is created, with the style and template. scripts are not update
    
    * new update
    
    some new update with dbug
    
    * new update for node page
    
    node page added without content but in the route installed
    
    * make ui
    
    make a Trapezium on ui design and add this to the main js for use every wher
    
    * update for the Node page
    
    it's just simple page without any component. it has a small bug
    
    * nodeBg added
    
    the node page's back ground added to main.js. it has some css problems at he moment but it's ok. the routing at the moment is symple. without any conditions! 😏
    
    * update oneclick with v-for
    
    added v-for and data to the row but they are not inline block
    
    * update
    
    * update
    
    * node header
    
    a component added to header for the node page
    
    * node header js
    
    node header as a base component
    
    * header added
    
    the function is good but css has problem, positioning is not true
    
    * solve the css problems
    
    header with the logo fixed on node page
    
    * update
    
    update the css for header in the node page
    
    * installation option stracture
    
    one click changed
    
    * update installOption
    
    css solved
    
    * right side bar of node page component added
    
    * update
    
    * update
    
    * update
    
    * right side bar added to node with dummy style
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * FE Update
    
    * new plugin for node page
    
    new plugin made and connected tgo node page
    footerNode
    ServicePlugin
    theTrapezion
    
    * update
    
    * gridnode added
    
    add a grid page to be a cabinet of node page
    
    * update
    
    stracture of node page is ready
    
    * control update
    
    control panel
    control grid
    routing added
    
    * update
    
    * conflicts solved
    
    * update fixed lockjson
    
    * jest update
    
    * trapezium positions
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * nodedrag added
    
    * colors set to design
    
    * ADD:Service-plugin_style
    
    * update
    
    * ADD:arrow btn to service plugin

--- launcher/src/components/UI/NodeGrid.vue
+++ launcher/src/components/UI/NodeGrid.vue
@@ -1,20 +1,5 @@
 <template>
 
-  <div class="nodeparent">
-    <div class="execution">
-      <the-trapezium><node-drag></node-drag></the-trapezium>
-    </div>
-    <div class="consensus">
-      <the-trapezium><node-drag></node-drag></the-trapezium>
-    </div>
-    <div class="validator">
-      <the-trapezium><node-drag></node-drag></the-trapezium>
-    </div>
-    <node-setup-config></node-setup-config>
-    <div class="service">SERVICE</div>
-    <div class="sidebar">SIDEBAR</div>
-    <div class="footer">FOOTER</div>
-  </div>
     <div class="nodeparent">
         <div class="execution">
             <the-trapezium>
@@ -33,20 +18,35 @@
         </div>
         <div class="nodesetup">NODE SETUP</div>
 
-        <div class="service"><div class="title">SERVICE PLUGIN</div><service-plugin></service-plugin></div>
-
+    
+        <div class="service">
+            <div class="title">SERVICE PLUGIN</div>
+            <div class="service-container">
+                <base-button class="btn">
+                    <div class="arrow-up"></div>
+                </base-button>
+                <service-plugin></service-plugin>
+                <base-button class="btn">
+                    <div class="arrow-down"></div>
+                </base-button>
+            </div>
+        </div>
+    
         <div class="sidebar">SIDEBAR</div>
         <div class="footer">FOOTER</div>
     </div>
-
 </template>
 
 <script>
 
-import TheTrapezium from "./TheTrapezium.vue";
-import NodeDrag from "./NodeDrag.vue";
-import NodeSetupConfig from "../layers/NodeSetupConfig.vue";
+import TheTrapezium from "../UI/TheTrapezium.vue";
+import NodeDrag from '../UI/NodeDrag.vue';
+import ServicePlugin from "../layers/ServicePlugin.vue";
 export default {
+
+
+    components: { TheTrapezium, NodeDrag, ServicePlugin },
+
 
 
 };
@@ -58,37 +58,13 @@ export default {
     height: 93%;
     grid-template-columns: 70% 20% 10%;
     grid-template-rows: repeat(4, 23.75%) 5%;
-    grid-row-gap: 2px;
+    grid-row-gap: 1px;
     position: relative;
     top: 7%;
     margin: 0;
 }
 
 .execution {
-  background: blue;
-  color: white;
-  grid-column-start: 1;
-  grid-column-end: 2;
-  align-self: center;
-}
-
-.consensus {
-  background: yellow;
-  color: red;
-  grid-column-start: 1;
-  grid-column-end: 2;
-  align-self: center;
-}
-
-.validator {
-  background: gray;
-  color: white;
-  grid-column-start: 1;
-  grid-column-end: 2;
-  border: 2px dotted red;
-  align-self: center;
-
-
     color: white;
     grid-column-start: 1;
     grid-column-end: 2;
@@ -107,8 +83,7 @@ export default {
     grid-column-start: 1;
     grid-column-end: 2;
     align-self: center;
-
-
+    box-sizing: border-box;
 }
 
 .nodesetup {
@@ -130,14 +105,54 @@ export default {
     justify-content: center;
     align-content: center;
 }
-.title{
-height: 5%;
-background: #000;
-margin:1rem 0;
-font-weight: bold;
-padding: .5px;
-text-align: center;
 
+.title {
+    height: 5%;
+    background: #000;
+    margin: 1rem 0;
+    font-weight: bold;
+    padding: .5px;
+    text-align: center;
+    font-size: 90%;
+}
+
+.service-container {
+    width: 90%;
+    height: 85%;
+    background: #4F4F4F;
+    align-self: center;
+    border-radius: 20px;
+    overflow: hidden;
+    padding: 5%;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+}
+
+.btn {
+    width: 90%;
+    margin: 3%;
+    height: 5%;
+    display: flex;
+    justify-content: center;
+    align-self: center;
+    overflow: hidden;
+}
+
+.arrow-up {
+    width: 0;
+    height: 50%;
+    border-bottom: solid .5rem #eee;
+    border-left: solid 50px transparent;
+    border-right: solid 50px transparent;
+    resize: both;
+}
+.arrow-down{
+    width: 0;
+  height: 50%;
+  border-top: solid .5rem #eee;
+  border-left: solid 50px transparent;
+  border-right: solid 50px transparent;
 }
 
 .sidebar {
