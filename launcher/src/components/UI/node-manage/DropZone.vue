d475fae9fe71ff2fd6ee0d580638b708e4d23390
maxbehzadi82 <69126271+maxbehzadi82@users.noreply.github.com>
2022-02-23 12:26:10 +0100

    ADD: DropZone, fix conflicts (#80)
    * REFACTOR: FormSetup, TheFirst & BaseLogo styles changed
    
    * REFACTOR:FormSetup style changed
    
    * ADD:new icons added
    
    * REFACTOR:FormSetup styles changed
    
    * ADD:Node side menu added
    
    * ADD:NodeManage routing ,toggle sidebar added
    
    * ADD:arrow added to FormSetup select tag
    
    * ADD: NodeManage toggle sidebar done
    
    * ADD:change-menu ,confirm-box,config-box added
    
    * ADD: plus btn adds icon to the list
    
    * ADD:change box done
    
    * ADD:drag & drop addrd to NodeManage
    
    * FIX:d&d styles fixed
    
    * Fix
    
    * FIX:styles fixed
    
    * ADD:confirm configuration added
    
    * ADD: set props and emits for NodeManage
    
    * ADD: drag & drop based on categories added
    
    * FIX:FormInput add & delete btn changed
    
    * FIX:FormSetup
    
    * ADD:sidebar scroll button added
    
    * FIX: D&D styles
    
    * FIX:D&D Scrollbar
    
    * FIX:styles
    
    * FIX:conflicts fixed
    
    * ADD:modal btn added to trapezoids

---
+++ launcher/src/components/UI/node-manage/DropZone.vue
@@ -0,0 +1,123 @@
+<template>
+  <manage-trapezoid>
+    <template #default>
+      <span class="title">{{ title }}</span>
+      <div
+        class="item-box"
+        @drop="onDrop($event, sidebarPlugins)"
+        @dragenter.prevent
+        @dragover.prevent
+      >
+        <div class="items" v-for="(item, index) in list" :key="index">
+          <img :src="item.source" alt="icon" />
+        </div>
+      </div>
+    </template>
+    <template #plusIcon>
+      <div class="plus-icon-box">
+        <span>+</span>
+      </div>
+    </template>
+  </manage-trapezoid>
+</template>
+<script>
+import ManageTrapezoid from "./ManageTrapezoid.vue";
+export default {
+  components: {
+    ManageTrapezoid,
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: "Title",
+    },
+    list: {
+      type: Array,
+      required: true,
+      default: () => {
+        return [];
+      },
+    },
+  },
+};
+</script>
+<style scoped>
+.title {
+  width: auto;
+  height: 20px;
+  color: white;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 3px 5px;
+  background-color: #334b3f;
+  border-radius: 20px;
+  position: absolute;
+  left: 0;
+  top: -10px;
+}
+.item-box {
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  grid-template-rows: repeat(2, 63px);
+  justify-content: space-between;
+  align-self: center;
+  align-items: center;
+  row-gap: 10px;
+  overflow-x: hidden;
+  overflow-y: auto;
+  position: absolute;
+  top: 21%;
+  left: 21.6%;
+  height: 63px;
+  width: 230px;
+  background-color: transparent;
+  border-top: 1px solid #656565;
+  border-bottom: 1px solid #656565;
+}
+.item-box::-webkit-scrollbar {
+  width: 1px;
+}
+.item-box .items {
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  width: 50px;
+  height: 50px;
+  border: 1px solid rgb(96, 95, 95);
+  border-radius: 10px;
+  margin: 0 auto;
+}
+.item-box .items img {
+  width: 50px;
+  height: 50px;
+}
+.plus-icon-box {
+  background-color: rgb(237, 237, 237);
+  width: 30px;
+  height: 20px;
+  border-radius: 50px;
+  position: absolute;
+  top: 2px;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  color: rgb(65, 65, 65);
+  border: 1px solid rgb(67, 67, 67);
+  box-shadow: inset 0 1px 4px 1px rgb(142, 142, 142);
+  cursor: pointer;
+}
+
+.plus-icon-box:hover {
+  border: 1px solid green;
+  /* box-shadow: 0 1px 4px 1px rgb(142, 142, 142); */
+}
+.plus-icon-box:active {
+  background-color: #fff;
+  border: 1px solid rgb(159, 159, 159);
+  box-shadow: none;
+  font-size: 14px;
+}
+</style>
