5c3b776c0d27af05bd7922e4b554f70ac734229e
mabasian <54101509+mabasian@users.noreply.github.com>
2022-03-03 13:06:22 +0100

    UPDATE: Click installation Insalliation Menu updaed

--- launcher/src/components/layers/InstallitionMenu.vue
+++ launcher/src/components/layers/InstallitionMenu.vue
@@ -1,11 +1,11 @@
 <template>
-  <section>
+  <section class="Parent_ctnInstal">
     <section id="header">
       <h2>welcome</h2>
     </section>
-    <div class="container">
+    <div class="containerInstall">
       <div class="col" v-for="install in installation" :key="install.title">
-        <router-link :to="install.path"
+        <router-link class="lintTtl" :to="install.path"
           ><button-installation
             :title="install.title"
             :img="install.img"
@@ -13,7 +13,7 @@
         ></router-link>
       </div>
     </div>
-    <circle-loading :message="message" :open="running" ></circle-loading>
+    <circle-loading :message="message" :open="running"></circle-loading>
     <div id="txt">
       <p>
         IN THIS STEP YOU CHOOSE HOW YOU WANT TO INSTALL YOUR NODE. IF YOU NEED
@@ -28,73 +28,85 @@ import ButtonInstallation from "./ButtonInstallation.vue";
 import CircleLoading from "../UI/CircleLoading.vue";
 import ControlService from "@/store/ControlService";
 export default {
-created(){
-  console.log("check OS");
-  let response = ControlService.checkOS()
-    .then(result => {return result})
-    .catch(error => {return error})
-  this.display(response)
-},
+  created() {
+    console.log("check OS");
+    let response = ControlService.checkOS()
+      .then((result) => {
+        return result;
+      })
+      .catch((error) => {
+        return error;
+      });
+    this.display(response);
+  },
   components: { ButtonInstallation, CircleLoading },
   data() {
     return {
       running: true,
-      message: '',
+      message: "",
       installation: [
         {
           title: "1CLICK INSTALLATION",
-          img: "/img/icon/clickinstall.png",
+          img: "/img/icon/one click installer.png",
           path: "/clickinstall",
         },
         {
           title: "CUSTOM INSTALLATION",
-          img: "/img/icon/custominstall.png",
+          img: "/img/icon/custom installer.png",
           path: "/manage",
         },
         {
           title: "IMPORT CONFIGURATION",
-          img: "/img/icon/one click installer.png",
+          img: "/img/icon/IMPORT CONFIGURATIONS.png",
           path: "/",
         },
       ],
     };
   },
-  methods:{
-    display : async function(response){
+  methods: {
+    display: async function (response) {
       let data = await response;
       console.log(data);
-      if(data == "Ubuntu" || data == "CentOS"){
-        this.message = data.toUpperCase() + " IS A SUPPORTED OS"
-      } else if(data.name !== undefined) {
-        this.message = data.name.toUpperCase() + ": " + data.message.toUpperCase();
+      if (data == "Ubuntu" || data == "CentOS") {
+        this.message = data.toUpperCase() + " IS A SUPPORTED OS";
+      } else if (data.name !== undefined) {
+        this.message =
+          data.name.toUpperCase() + ": " + data.message.toUpperCase();
       } else {
         this.message = "UNSUPPORTED OS";
       }
       this.running = false;
-    }
-  }
+    },
+  },
 };
 </script>
 <style scope>
-.container {
+.Parent_ctnInstal {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.containerInstall {
   width: 72vw;
   height: 30vh;
   resize: both;
   margin: 0 auto;
   position: relative;
   border-radius: 40px;
+  flex-wrap: nowrap;
+  display: flex;
+  justify-content: center;
 }
 .col {
-  width: 25%;
-  height: 28vh;
+  width: 30%;
+  height: 100%;
   background: rgba(51, 102, 102, 0.4);
-  display: inline-block;
   box-sizing: border-box;
-  border: 3px solid grey;
-  resize: both;
-  margin: 25px;
+  border: 3px solid rgb(88, 86, 86);
+  margin: 1rem;
   border-radius: 20px;
-  flex-direction: row;
+  display: flex;
 }
 #header {
   text-align: center;
@@ -117,8 +129,8 @@ created(){
 #txt {
   width: 70vw;
   height: auto;
-  border: 3px solid grey;
-  margin: 100px auto;
+  border: 3px solid rgb(88, 86, 86);
+  margin: auto;
   background: #336666;
   border-radius: 40px;
   position: relative;
@@ -127,7 +139,6 @@ created(){
 }
 #txt p {
   margin-top: -1px;
-  font-size: 14pt;
   font-weight: bold;
   color: #fff;
 }
@@ -136,5 +147,8 @@ created(){
   width: 98%;
   border: 3px solid red;
   border-radius: 40px;
+}
+.lintTtl {
+  text-decoration: none;
 }
 </style>
