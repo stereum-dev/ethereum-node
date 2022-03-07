72fec0d99f64beebc7f426be456edbc64d3f62e1
mabasian <54101509+mabasian@users.noreply.github.com>
2022-03-07 19:58:45 +0100

    UPDATE:vuex stractures standarded

--- launcher/tests/unit/pages/ClickInstall.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from "@vue/test-utils";
-import ClickInstall from "../../../src/pages/ClickInstall";
-import InstallOption from "../../../src/components/layers/InstallOption";
-
-test("InstallOption Comp exists insdie of ClickInstall", () => {
-  const wrapper = mount(ClickInstall);
-
-  expect(wrapper.findComponent(InstallOption).exists()).toBe(true);
-});
