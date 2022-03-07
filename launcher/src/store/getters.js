aec5158ab917baf75883bbf6a8ed7c2856fa9218
mabasian <54101509+mabasian@users.noreply.github.com>
2022-03-07 17:22:19 +0100

    vuex: done, strature fixed

---
+++ launcher/src/store/getters.js
@@ -0,0 +1,20 @@
+export default {
+  linkFlags_get(state) {
+    return state.linkFlags;
+  },
+  installation_get(state) {
+    return state.installation;
+  },
+  R1clkInstls_get(state) {
+    return state.R1clkInstls;
+  },
+  nodeSbActive_get(state) {
+    return state.configData_nodeSidebarActive;
+  },
+  nodeSbVideo_get(state) {
+    return state.configData_nodeSidebarVideo;
+  },
+  dialogIsVisible_get(state) {
+    return state.dialogIsVisible;
+  },
+};
--- launcher/src/store/index.js
+++ launcher/src/store/index.js
@@ -1,9 +1,177 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import { createStore } from "vuex";
+import rootMutations from "./mutations.js";
+import rootActions from "./actions.js";
+import rootGetters from "./getters.js";
 
-Vue.use(Vuex);
-
-const store = new Vuex.Store({
-  state: {},
-  mutations: {},
+//state vuex
+const store = createStore({
+  state() {
+    return {
+      dialogIsVisible: false,
+      linkFlags: [
+        {
+          langImg: "Img/Flag/ENGLISH.png",
+          langSelect: "Img/Flag/En.png",
+          langName: "english",
+        },
+        {
+          langImg: "Img/Flag/GERMAN.png",
+          langSelect: "Img/Flag/Gr.png",
+          langName: "german",
+        },
+        {
+          langImg: "Img/Flag/FRENCH.png",
+          langSelect: "Img/Flag/Fr.png",
+          langName: "french",
+        },
+        {
+          langImg: "Img/Flag/SPANISH.png",
+          langSelect: "Img/Flag/Sp.png",
+          langName: "spanish",
+        },
+      ],
+      installation: [
+        {
+          title: "1CLICK INSTALLATION",
+          img: "/img/icon/one click installer.png",
+          path: "/clickinstall",
+        },
+        {
+          title: "CUSTOM INSTALLATION",
+          img: "/img/icon/custom installer.png",
+          path: "/manage",
+        },
+        {
+          title: "IMPORT CONFIGURATION",
+          img: "/img/icon/IMPORT CONFIGURATIONS.png",
+          path: "/",
+        },
+      ],
+      configData_nodeSidebarVideo: [
+        {
+          id: 1,
+          name: "First Steps!",
+        },
+        {
+          id: 2,
+          name: "Stake with 32 ETH",
+        },
+        {
+          id: 3,
+          name: "Use SSV network",
+        },
+        {
+          id: 4,
+          name: "Alerts on Mobile App",
+        },
+        {
+          id: 5,
+          name: "Switch Clients",
+        },
+        {
+          id: 6,
+          name: "Switch Machine",
+        },
+      ],
+      configData_nodeSidebarActive: [
+        {
+          id: 1,
+          name: "NONE FUNCTIONAL",
+          status: "deactive",
+        },
+        {
+          id: 2,
+          name: "ACTIVE",
+          status: "active",
+        },
+        {
+          id: 3,
+          name: "SYNC/PROBLEM REC.",
+          status: "notOk",
+        },
+        {
+          id: 4,
+          name: "INACTIVE",
+          status: "off",
+        },
+        {
+          id: 5,
+          name: "ACTIVE",
+          status: "active",
+        },
+        {
+          id: 6,
+          name: "INACTIVE",
+          status: "off",
+        },
+        {
+          id: 7,
+          name: "SYNC/PROBLEM REC.",
+          status: "notOk",
+        },
+        {
+          id: 8,
+          name: "NONE FUNCTIONAL",
+          status: "deactive",
+        },
+        {
+          id: 9,
+          name: "ACTIVE",
+          status: "active",
+        },
+        {
+          id: 10,
+          name: "SYNC/PROBLEM REC.",
+          status: "notOk",
+        },
+        {
+          id: 9,
+          name: "ACTIVE",
+          status: "active",
+        },
+        {
+          id: 10,
+          name: "SYNC/PROBLEM REC.",
+          status: "notOk",
+        },
+        {
+          id: 10,
+          name: "SYNC/PROBLEM REC.",
+          status: "notOk",
+        },
+        {
+          id: 10,
+          name: "SYNC/PROBLEM REC.",
+          status: "notOk",
+        },
+        {
+          id: 9,
+          name: "ACTIVE",
+          status: "active",
+        },
+        {
+          id: 10,
+          name: "SYNC/PROBLEM REC.",
+          status: "notOk",
+        },
+        {
+          id: 10,
+          name: "SYNC/PROBLEM REC.",
+          status: "notOk",
+        },
+      ],
+      R1clkInstls: [
+        { clkId: "staking", img: "/img/icon/STAKING.png" },
+        { clkId: "testnet", img: "/img/icon/TESTNET.png" },
+        { clkId: "ssv", img: "/img/icon/BLOX_SSV.png" },
+        { clkId: "blox", img: "/img/icon/OBOL_SSV.png" },
+        { clkId: "blox", img: "/img/icon/ROCKETPOOL_1.png" },
+      ],
+    };
+  },
+  mutations: rootMutations,
+  actions: rootActions,
+  getters: rootGetters,
 });
+//end of state
+export default store;
