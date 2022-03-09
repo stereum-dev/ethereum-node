05b407edb25c10203cfdb2389af13ff39ca6c8e6
mabasian <54101509+mabasian@users.noreply.github.com>
2022-03-09 15:07:39 +0100

    UPDATE: store stractured for all the pages (#111)
    * change to vue3
    
    without any errors
    
    * new Update
    
    it's a last dbug, with the better css
    
    * Update OneClick.vue
    
    page of component one click is created, with the style and template. scripts are not update
    
    * new update
    
    some new update with dbug
    
    * new update for node page
    
    node page added without content but in the route installed
    
    * make ui
    
    make a Trapezium on ui design and add this to the main js for use every wher
    
    * update for the Node page
    
    it's just simple page without any component. it has a small bug
    
    * nodeBg added
    
    the node page's back ground added to main.js. it has some css problems at he moment but it's ok. the routing at the moment is symple. without any conditions! 😏
    
    * update oneclick with v-for
    
    added v-for and data to the row but they are not inline block
    
    * update
    
    * update
    
    * node header
    
    a component added to header for the node page
    
    * node header js
    
    node header as a base component
    
    * header added
    
    the function is good but css has problem, positioning is not true
    
    * solve the css problems
    
    header with the logo fixed on node page
    
    * update
    
    update the css for header in the node page
    
    * installation option stracture
    
    one click changed
    
    * update installOption
    
    css solved
    
    * right side bar of node page component added
    
    * update
    
    * update
    
    * update
    
    * right side bar added to node with dummy style
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * FE Update
    
    * new plugin for node page
    
    new plugin made and connected tgo node page
    footerNode
    ServicePlugin
    theTrapezion
    
    * update
    
    * gridnode added
    
    add a grid page to be a cabinet of node page
    
    * update
    
    stracture of node page is ready
    
    * control update
    
    control panel
    control grid
    routing added
    
    * update
    
    * conflicts solved
    
    * update fixed lockjson
    
    * jest update
    
    * trapezium positions
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * nodedrag added
    
    * colors set to design
    
    * ADD:Service-plugin_style
    
    * update
    
    * ADD:arrow btn to service plugin
    
    * update
    
    * update
    
    * new pull from base
    
    * UPDATE: change some style at install buttons
    
    * ADD: router link to the header for the manage page
    
    * UPDATE: add service pligin to the base js to use everywhere without export
    
    * DELETE: one of the service plugin from ui
    
    * UPDATE: display none for the boxes on the header
    
    * delete: service plugin test deleted
    
    * UPDATE: Click installation layer updated
    
    * UPDATE: Click installation Insalliation Menu updaed
    
    * ADD: vuex added to project
    
    * in process: structure is not ready,some data added
    
    * vuex: some functions changed with vuex
    
    * vuex: done, strature fixed
    
    * UPDATE:vuex stractures standarded
    
    * ADD: flag module to language functions at store
    
    * UPDATE: store stractured for all the pages

---
+++ launcher/src/store/nodeManage/index.js
@@ -0,0 +1,12 @@
+import nodeManageActions from "./actions.js";
+import nodeManageGetters from "./getters.js";
+import nodeManageMutations from "./mutations.js";
+
+export default {
+  state() {
+    return {};
+  },
+  mutations: nodeManageMutations,
+  actions: nodeManageActions,
+  getters: nodeManageGetters,
+};
