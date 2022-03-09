9d176f57643953e380cf3a5df66f79230fa85a9d
mabasian <54101509+mabasian@users.noreply.github.com>
2022-03-09 14:16:58 +0100

    UPDATE: store stractured for all the pages

---
+++ launcher/src/store/clickInstall/index.js
@@ -0,0 +1,12 @@
+import clickInstallActions from "./actions";
+import clickInstallGetters from "./getters.js";
+import clickInstallMutations from "./mutations.js";
+
+export default {
+  state() {
+    return {};
+  },
+  mutations: clickInstallMutations,
+  actions: clickInstallActions,
+  getters: clickInstallGetters,
+};
