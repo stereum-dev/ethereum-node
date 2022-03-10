bb2b2f2611cabe365ddf2122a143cc1dd6557e5f
mabasian <54101509+mabasian@users.noreply.github.com>
2022-03-10 15:07:07 +0100

    ADD: sandclock added to control dashboard

---
+++ launcher/src/components/UI/the-control/ControlDashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ctrlParent">
+    <sand-clock></sand-clock>
+  </div>
+</template>
+<script>
+import SandClock from "./SandClock.vue";
+export default {
+  components: { SandClock },
+};
+</script>
+<style scoped>
+.ctrlParent {
+  padding: 1rem;
+}
+</style>
