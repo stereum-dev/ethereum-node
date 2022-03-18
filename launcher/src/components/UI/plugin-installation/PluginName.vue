f666a652a48fd66a1b89c5c7698eedc6f74e06a7
maxbehzadi82 <69126271+maxbehzadi82@users.noreply.github.com>
2022-03-18 14:46:21 +0100

    ADD:installation and verify modal structure added.

---
+++ launcher/src/components/UI/plugin-installation/PluginName.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="plugin-parent">
+    <div class="opacity-bg"></div>
+    <div class="plugin-modal-parent">
+      <div class="plugin-modal">
+        <div class="name-box">
+          <div class="name-title-box">
+            <div class="name-title">
+              <span>PLUGIN NAME</span>
+            </div>
+          </div>
+        </div>
+        <div class="options-box"></div>
+        <div class="system-box"></div>
+        <div class="btn-box"></div>
+      </div>
+    </div>
+  </div>
+</template>
