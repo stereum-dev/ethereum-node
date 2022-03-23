967657fe3d24ad65f48888a9d289b30b3d76bc30
mabasian <54101509+mabasian@users.noreply.github.com>
2022-03-23 17:48:01 +0100

    ADD: the thempometer component to contrl pannel

--- launcher/src/components/UI/the-control/ControlDashboard.vue
+++ launcher/src/components/UI/the-control/ControlDashboard.vue
@@ -8,7 +8,7 @@
     <div class="sandFull_cell"><sand-clock></sand-clock></div>
     <div class="discSpace_cell"></div>
     <div class="networkSync_cell"></div>
-    <div class="cpuUsage_cell"></div>
+    <div class="cpuUsage_cell"><the-thermometer></the-thermometer></div>
     <div class="peersConect_cell"></div>
     <div class="networkUsage_cell"></div>
     <div class="validatorComment_cell">
@@ -17,11 +17,12 @@
   </div>
 </template>
 <script>
+import TheThermometer from "./TheThermometer.vue";
 import SandClock from "./SandClock.vue";
 // import BusyRam from "./BusyRam.vue";
 import StorageCounter from "./StorageCounter.vue";
 export default {
-  components: { SandClock, StorageCounter },
+  components: { SandClock, StorageCounter, TheThermometer },
 };
 </script>
 <style scoped>
@@ -47,7 +48,7 @@ export default {
 .sandFull_cell {
   grid-column: 3/4;
   grid-row: 1/2;
-  background: red;
+
   border-radius: 0 15px 0 0;
   display: flex;
 }
@@ -65,6 +66,9 @@ export default {
   grid-column: 1/2;
   grid-row: 3/4;
   background: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .peersConect_cell {
   grid-column: 2/3;
@@ -80,7 +84,7 @@ export default {
 .validatorComment_cell {
   grid-column: 3/4;
   grid-row: 2/3;
-  background: red;
+
   display: flex;
 }
 .validCntr {
---
+++ launcher/src/components/UI/the-control/TheThermometer.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="thermoParent">
+    <div id="thermometer">
+      <div>
+        <div id="stem"></div>
+        <div id="marks">
+          <div id="line">
+            --<br />--<br />--<br />--<br />--<br />--<br />--<br />--<br />--<br />--<br />--<br />--<br />--<br />--<br />--
+          </div>
+        </div>
+        <div id="merc-stem"><div id="mercury"></div></div>
+        <div id="bulb">100</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.thermoParent {
+  width: 1%;
+}
+#thermometer {
+  width: 5%;
+}
+#far {
+  border: 4px solid black;
+  height: 400px;
+}
+
+#cel {
+  border: 4px solid black;
+  height: 400px;
+}
+
+#stem {
+  width: 20px;
+  height: 215px;
+  position: relative;
+  left: 21px;
+  z-index: 1;
+  border-radius: 50px 50px 0 0;
+  border-left: 4px solid black;
+  border-top: 4px solid black;
+  border-right: 4px solid black;
+  animation-name: stem;
+  animation-duration: 2s;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+}
+
+#marks {
+  width: 20px;
+  height: 90px;
+  position: relative;
+  bottom: 180px;
+  left: 36px;
+  z-index: 40;
+}
+
+#line {
+  line-height: 0.7em;
+}
+
+#merc-stem {
+  width: 10px;
+  height: 85px;
+  position: relative;
+  bottom: 175px;
+  left: 25px;
+}
+#mercury {
+  width: 20px;
+  height: 25px;
+  position: relative;
+  bottom: -80%;
+  z-index: 10;
+  animation-name: mercury;
+  animation-duration: 2s;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+}
+
+#bulb {
+  position: relative;
+  /*   overflow: hidden; */
+  bottom: 190px;
+  z-index: 20;
+  width: 60px;
+  height: 60px;
+  background-color: grey;
+  border-radius: 50px;
+  border: 4px solid black;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-weight: bold;
+}
+
+@media only screen and (max-width: 600px) {
+  #main {
+    height: 600px;
+    overflow: hidden;
+  }
+}
+
+@keyframes mercury {
+  from {
+    background-color: #dd2c00;
+    height: 190px;
+    bottom: 100%;
+  }
+  to {
+    background-color: #e4f30d;
+    height: 25px;
+    bottom: -80%;
+  }
+}
+</style>
