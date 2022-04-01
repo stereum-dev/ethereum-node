b2ced844ed73b32f0bb9d5cefa864d3cdc4810db
maxbehzadi82 <69126271+maxbehzadi82@users.noreply.github.com>
2022-04-01 17:37:16 +0200

    REMOVE: removed 4 unit tests

--- launcher/tests/unit/layers/BackgroundPage.spec.js
+++
@@ -1,7 +0,0 @@
-import { mount } from '@vue/test-utils'
-import BackgroundPage from "../../../src/components/layers/BackgroundPage";
-test('BackgroundPage contains img tag',()=>{
-
-    const wrapper = mount(BackgroundPage)
-    expect(wrapper.element.img)
-})
