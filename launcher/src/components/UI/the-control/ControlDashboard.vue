9b6cec3f482df9085b2504706c7d8cdf9362992c
mabasian <54101509+mabasian@users.noreply.github.com>
2022-04-01 13:41:01 +0200

    UPDATE: thermometers on the dashboard combonent (#140)
    * change to vue3
    
    without any errors
    
    * new Update
    
    it's a last dbug, with the better css
    
    * Update OneClick.vue
    
    page of component one click is created, with the style and template. scripts are not update
    
    * new update
    
    some new update with dbug
    
    * new update for node page
    
    node page added without content but in the route installed
    
    * make ui
    
    make a Trapezium on ui design and add this to the main js for use every wher
    
    * update for the Node page
    
    it's just simple page without any component. it has a small bug
    
    * nodeBg added
    
    the node page's back ground added to main.js. it has some css problems at he moment but it's ok. the routing at the moment is symple. without any conditions! 😏
    
    * update oneclick with v-for
    
    added v-for and data to the row but they are not inline block
    
    * update
    
    * update
    
    * node header
    
    a component added to header for the node page
    
    * node header js
    
    node header as a base component
    
    * header added
    
    the function is good but css has problem, positioning is not true
    
    * solve the css problems
    
    header with the logo fixed on node page
    
    * update
    
    update the css for header in the node page
    
    * installation option stracture
    
    one click changed
    
    * update installOption
    
    css solved
    
    * right side bar of node page component added
    
    * update
    
    * update
    
    * update
    
    * right side bar added to node with dummy style
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * FE Update
    
    * new plugin for node page
    
    new plugin made and connected tgo node page
    footerNode
    ServicePlugin
    theTrapezion
    
    * update
    
    * gridnode added
    
    add a grid page to be a cabinet of node page
    
    * update
    
    stracture of node page is ready
    
    * control update
    
    control panel
    control grid
    routing added
    
    * update
    
    * conflicts solved
    
    * update fixed lockjson
    
    * jest update
    
    * trapezium positions
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * nodedrag added
    
    * colors set to design
    
    * ADD:Service-plugin_style
    
    * update
    
    * ADD:arrow btn to service plugin
    
    * update
    
    * update
    
    * new pull from base
    
    * UPDATE: change some style at install buttons
    
    * ADD: router link to the header for the manage page
    
    * UPDATE: add service pligin to the base js to use everywhere without export
    
    * DELETE: one of the service plugin from ui
    
    * UPDATE: display none for the boxes on the header
    
    * delete: service plugin test deleted
    
    * UPDATE: Click installation layer updated
    
    * UPDATE: Click installation Insalliation Menu updaed
    
    * ADD: vuex added to project
    
    * in process: structure is not ready,some data added
    
    * vuex: some functions changed with vuex
    
    * vuex: done, strature fixed
    
    * UPDATE:vuex stractures standarded
    
    * ADD: flag module to language functions at store
    
    * UPDATE: store stractured for all the pages
    
    * ADD: sandclock added to control dashboard
    
    * ADD: BusyRam added to control panel
    
    * ADD: storage plugin to control
    
    * ADD: Counter
    
    * Update SandClock.vue
    
    * ADD: inputs side of sandclock
    
    * TEST: find the best chart for the control page
    
    * Revert "TEST: find the best chart for the control page"
    
    This reverts commit a54ed1c9b5be805bdb52e0686228f169b6c97a66.
    
    * ADD: Storage Counter with the right size position
    
    * ADD: the thempometer component to contrl pannel
    
    * UPDATE: the thompometer lines updated
    
    * REMOVE: test colors from control
    
    * ADD:thermometer right style for the first cell
    
    * ADD:Second thermometer in the right position
    
    * UPDATE: update thermometers on the control dashboard component

--- launcher/src/components/UI/the-control/ControlDashboard.vue
+++ launcher/src/components/UI/the-control/ControlDashboard.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="ctrlParent">
-    <!-- <sand-clock></sand-clock>
-    <busy-ram></busy-ram>
-    <storage-counter></storage-counter> -->
     <div class="machineName_cell">machineName_cell</div>
     <div class="nodeProcess_cell">nodeProcess_cell</div>
-    <div class="sandFull_cell"><sand-clock></sand-clock></div>
+    <div class="sandFull_cell">
+      <sand-clock></sand-clock>
+    </div>
     <div class="discSpace_cell">discSpace_cell</div>
 
     <div class="networkSync_cell">networkSync_cell</div>
 
-    <div class="cpuUsage_cell"><the-thermometer></the-thermometer></div>
+    <div class="cpuUsage_cell">
+      <the-thermometer></the-thermometer>
+    </div>
     <div class="peersConect_cell">peersConect_cell</div>
     <div class="networkUsage_cell">
       <the-thermometer class="netThermo"></the-thermometer>
@@ -20,6 +21,7 @@
     </div>
   </div>
 </template>
+
 <script>
 import TheThermometer from "./TheThermometer.vue";
 import SandClock from "./SandClock.vue";
@@ -29,6 +31,7 @@ export default {
   components: { SandClock, StorageCounter, TheThermometer },
 };
 </script>
+
 <style scoped>
 .ctrlParent {
   display: grid;
@@ -38,6 +41,7 @@ export default {
   grid-template-rows: 12% repeat(3, 29.5%);
   position: relative;
 }
+
 .machineName_cell {
   grid-column: 1/2;
   grid-row: 1/2;
@@ -45,39 +49,43 @@ export default {
   border-radius: 15px 0 0 0;
   background: blue;
 }
+
 .nodeProcess_cell {
   grid-column: 2/3;
   grid-row: 1/2;
 }
+
 .sandFull_cell {
   grid-column: 3/4;
   grid-row: 1/2;
-
   border-radius: 0 15px 0 0;
   display: flex;
 }
+
 .discSpace_cell {
   grid-column: 1/2;
   grid-row: 2/3;
-
   background: violet;
 }
+
 .networkSync_cell {
   grid-column: 2/3;
   grid-row: 2/3;
 }
+
 .cpuUsage_cell {
   grid-column: 1/2;
   grid-row: 3/4;
-
   display: flex;
   justify-content: center;
   align-items: center;
 }
+
 .peersConect_cell {
   grid-column: 2/3;
   grid-row: 3/4;
 }
+
 .networkUsage_cell {
   grid-column: 1/2;
   grid-row: 4/5;
@@ -86,12 +94,13 @@ export default {
   align-items: center;
   border-radius: 0 0 0 15px;
 }
+
 .validatorComment_cell {
   grid-column: 3/4;
   grid-row: 2/3;
-
   display: flex;
 }
+
 .netThermo {
   position: relative;
   top: 0rem;
