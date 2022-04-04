86f2bfe903934da1007bef842cccbf978126c33e
maxbehzadi82 <69126271+maxbehzadi82@users.noreply.github.com>
2022-04-04 10:46:51 +0200

    ADD: text summary to controlPanel

--- launcher/src/components/UI/the-control/ControlPanel.vue
+++ launcher/src/components/UI/the-control/ControlPanel.vue
@@ -36,7 +36,7 @@
               <span>{{ item.title }}</span>
             </div>
             <div class="general-description">
-              <p>{{ item.description }}</p>
+              <p>{{ item.summary }}</p>
             </div>
           </div>
         </div>
@@ -51,7 +51,7 @@
               <span>{{ item.title }}</span>
             </div>
             <div class="expert-description">
-              <p>{{ item.description }}</p>
+              <p>{{ item.summary }}</p>
             </div>
           </div>
         </div>
@@ -74,25 +74,30 @@ export default {
       controlPanelGeneralItems: [
         {
           id: 1,
-          title: "GETH RUNNING",
+          title: "PRUNNING",
+          summary:
+            "Initiates the prunning of your execution client to free up storage",
           description:
             "Geth as execution client collects massive amounts of data that can be deleted after a while. Run this to free up some storage space",
         },
         {
           id: 2,
           title: "OS UPDATES",
+          summary: "Updates your OS to the newest available version",
           description:
             "Operating System updates might repair security holes, can add new features to your devices and remove outdated ones. Keep your device turned on!",
         },
         {
           id: 3,
           title: "CONFIG EXPORT",
+          summary: "Export your configuration to import it on a new device",
           description:
             'Currently running "Configuration" will be exported and stored in "/tmp/exported-config" directory. It could be then used to setup on other/new device.',
         },
         {
           id: 4,
           title: "RESTART HOST",
+          summary: "Restarts the host of your node",
           description:
             "Sometimes there are things getting stuck or not responding anymore, a restart might help you resolve some issues (but not all). This will take a couple of minutes.",
         },
@@ -101,6 +106,7 @@ export default {
         {
           id: 1,
           title: "API BINDING",
+          summary: "Configure an IP you can access from the outside",
           description:
             "If you want to use your beacon client(s) or monitoring to be accessible outside of your server, configure on which IP the services should listen on.",
         },
@@ -241,12 +247,12 @@ export default {
   height: 20%;
 }
 .expert-title span {
-  font-size: 10px;
+  font-size: 0.6rem;
   font-weight: 800;
   color: rgb(240, 20, 20);
 }
 .general-title span {
-  font-size: 10px;
+  font-size: 0.6rem;
   font-weight: 800;
   color: rgb(82, 82, 82);
 }
@@ -257,9 +263,9 @@ export default {
 }
 .expert-description p,
 .general-description p {
-  font-size: 6px;
+  font-size: .55rem;
   font-weight: 800;
   text-align: center;
-  color: rgb(31, 31, 31);
+  color: rgb(40, 40, 40);
 }
 </style>
