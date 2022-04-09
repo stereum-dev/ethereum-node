a08af9f2b95c1f4c78429627c106b416e90cd340
maxbehzadi82 <69126271+maxbehzadi82@users.noreply.github.com>
2022-04-09 20:59:02 +0200

    REFACTOR:root styles fixed

--- launcher/src/App.vue
+++ launcher/src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <div id="app">
-    <!-- <div id="nav"><router-link to="/">Setup</router-link> |</div> -->
+  <div id="app-parent">
     <router-view />
   </div>
 </template>
@@ -13,8 +12,25 @@ export default {
   },
 };
 </script>
-<style scoped>
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  top: 0;
+  left: 0;
+}
+#app-parent {
   font-family: "noto sans";
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
 }
 </style>
