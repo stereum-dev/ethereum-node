a2ad8dc795ee8375a33c2b56660c7cec170cdc98
mabasian <54101509+mabasian@users.noreply.github.com>
2022-05-07 11:37:33 +0200

    ADD: machine name component to control page (#236)
    steel not ready to use data and it's just style

---
+++ launcher/src/components/UI/the-control/MachineName.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="MachineNameParent">
+    <div class="ubuntuIcon">
+      <img src="../../../../public/img/icon/Control/ubuntuIco.svg" />
+    </div>
+    <input type="text" class="machineNam" />
+  </div>
+</template>
+
+<style scoped>
+.MachineNameParent {
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+.machineNam {
+  background: #33393e;
+  color: #eee;
+  border: 2px solid #b7c2c2;
+  left: -5%;
+  position: relative;
+  z-index: 2;
+  width: 80%;
+}
+.ubuntuIcon {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid #b7c2c2;
+  border-radius: 18px;
+  z-index: 3;
+  width: 10%;
+}
+.ubuntuIcon img {
+  width: 100%;
+}
+</style>
