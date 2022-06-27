6009e11cbc9d301907f7c0b7592fcf9f711cb40d
NeoPlays <80448387+NeoPlays@users.noreply.github.com>
2022-06-27 19:16:37 +0200

    FIX: getting stuck on connecting animation (#457)
    * FIX: getting stuck on connecting animation
    
    * UPDATE: use more precise conditional

--- launcher/src/components/UI/plugin-installation/VerifyInstallation.vue
+++ launcher/src/components/UI/plugin-installation/VerifyInstallation.vue
@@ -89,7 +89,7 @@ export default {
       await ControlService.startOneClickServices()
 
       let services = await ControlService.getServices();
-        if (services && services.length > 0) {
+        if (services && Array.isArray(services) && services.length > 0) {
         services.forEach((service) => {
           let buffer = this.allServices.find((element) => element.service === service.service)
           if(buffer){
