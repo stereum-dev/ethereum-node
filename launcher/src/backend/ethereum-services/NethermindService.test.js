ee4dad4bc806f9a1e1ceed0fa3b3d5621b07e244
NeoPlays <80448387+NeoPlays@users.noreply.github.com>
2022-06-27 15:19:45 +0200

    ADD: Besu and Nethermind Tests, update other tests (#454)

---
+++ launcher/src/backend/ethereum-services/NethermindService.test.js
@@ -0,0 +1,68 @@
+import { NethermindService } from "./NethermindService";
+import { networks } from "./NodeService";
+import { ServicePort, servicePortProtocol } from "./ServicePort";
+import { ServiceVolume } from "./ServiceVolume";
+
+test('buildByUserInput', () => {
+    const ports = [
+        new ServicePort(null, 4040, 4040, servicePortProtocol.tcp)
+    ]
+
+    const nethermindService = NethermindService.buildByUserInput(networks.goerli, ports, '/opt/stereum/nethermind').buildConfiguration()
+
+    expect(nethermindService.service).toBe('NethermindService')
+    expect(nethermindService.id).toMatch(/.{8}-.{4}-.{4}-.{4}-.{12}/)
+    expect(typeof nethermindService.configVersion).toBe('number')
+    expect(nethermindService.image).toContain('nethermind/nethermind')
+    expect(nethermindService.command).toContain('--config=goerli')
+    expect(nethermindService.command).toContain('--datadir=/opt/app/data')
+    expect(nethermindService.entrypoint).toContain('./Nethermind.Runner')
+    expect(nethermindService.ports).toHaveLength(1)
+    expect(nethermindService.ports).toContain('0.0.0.0:4040:4040/tcp')
+    expect(nethermindService.volumes).toContain('/opt/stereum/nethermind-' + nethermindService.id + '/data:/opt/app/data')
+    expect(nethermindService.user).toBe('root')
+    expect(nethermindService.network).toBe(networks.goerli)    
+})
+
+test('buildExecutionClientHttpEndpointUrl', () => {
+    const nethermindHttpEndpoint = NethermindService.buildByUserInput(networks.goerli, [], '/opt/stereum/nethermind').buildExecutionClientHttpEndpointUrl()
+    expect(nethermindHttpEndpoint).toMatch(/http:\/\/stereum-.{36}:8545/)
+})
+
+test('buildExecutionClientWsEndpointUrl', () => {
+    const nethermindWsEndpoint = NethermindService.buildByUserInput(networks.goerli, [], '/opt/stereum/nethermind').buildExecutionClientWsEndpointUrl()
+    expect(nethermindWsEndpoint).toMatch(/ws:\/\/stereum-.{36}:8546/)
+})
+
+test('buildExecutionClientMetricsEndpoint', () => {
+    const nethermindMetricsEndpoint = NethermindService.buildByUserInput(networks.goerli, [], '/opt/stereum/nethermind').buildExecutionClientMetricsEndpoint()
+    expect(nethermindMetricsEndpoint).toMatch(/stereum-.{36}:6060/)
+})
+
+test('buildPrometheusJob', () => {
+    const nethermindPrometheusJob = NethermindService.buildByUserInput(networks.goerli, [], '/opt/stereum/nethermind').buildPrometheusJob()
+    expect(nethermindPrometheusJob).toMatch(/\n  - job_name: stereum-.{36}\n    static_configs:\n      - targets: \[stereum-.{36}:6060]/)
+})
+
+test('buildByConfiguration', () => {
+    const nethermind = NethermindService.buildByConfiguration({
+        id: '297',
+        service: 'NethermindService',
+        configVersion: 14,
+        image: 'nethermind:v3.4.1',
+        ports: ['0.0.0.0:1234:5678/tcp', '8.8.8.8:1234:5678/udp'],
+        volumes: ['/opt/stereum/foo:/opt/app/data']
+      })
+    
+      expect(nethermind.id).toBe('297')
+      expect(nethermind.service).toBe('NethermindService')
+      expect(nethermind.configVersion).toBe(14)
+      expect(nethermind.image).toBe('nethermind')
+      expect(nethermind.imageVersion).toBe('v3.4.1')
+      expect(nethermind.ports).toHaveLength(2)
+      expect(nethermind.ports[0].destinationPort).toBe('1234')
+      expect(nethermind.ports[1].servicePort).toBe('5678')
+    
+      expect(nethermind.volumes).toHaveLength(1)
+      expect(nethermind.volumes[0]).toBeDefined()
+})
