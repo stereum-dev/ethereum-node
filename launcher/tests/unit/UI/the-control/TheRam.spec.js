aea33d1002bdb81c65ddd5b2d67d498b9050f0be
mabasian <54101509+mabasian@users.noreply.github.com>
2022-07-03 21:08:38 +0200

    update: debug some errors

--- launcher/tests/unit/UI/the-control/TheRam.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from "@vue/test-utils";
-import TheRam from "../../../../src/components/UI/the-control/TheRam.vue";
-
-test("ram symbol exist", () => {
-  const wrapper = shallowMount(TheRam);
-  const img = wrapper.find("img");
-
-  expect(img.exists()).toBe(true);
-});
