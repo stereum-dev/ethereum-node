8e6b8d9b3d7c97f1ba35aa9494d18d28168951ab
maxbehzadi82 <69126271+maxbehzadi82@users.noreply.github.com>
2022-07-05 11:25:59 +0200

    FIX:Fixing issues & adding new features to the task manager (#496)
    * REFACTOR: server connection delete modal
    
    * FIX: fixed #479
    
    * REFACTOR: progressbar replaced by text
    
    * REFACTOR:welcome page button position changed
    
    * REMOVE: Progressbar replaced by text and icon
    
    * ADD: oneClick plugin tooltip
    
    * ADD: taskManager error terminal
    
    New pop-up added to display and copy errors
    
    * ADD:new plugin icons added
    
    * REFACTOR: subtasks tooltip

---
+++ launcher/src/components/UI/task-manager/ErrorTerminal.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="terminal-parent">
+    <div class="error-box" @click="$emit('closeTerminal')"></div>
+    <div class="error-modal">
+      <div class="title-box">
+        <span>{{ item.name }} :</span>
+      </div>
+      <div class="description">
+        <span>>> {{ item.action }}</span>
+      </div>
+      <div class="copy-icon" @click="$emit('copyError')">
+        <img src="/img/icon/service-icons/copy1.png" alt="icon" />
+        <span>copied!</span>
+      </div>
+      <div class="btn-box">
+        <span>Click outside to close</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["item"],
+};
+</script>
+<style scoped>
+.terminal-parent {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.error-box {
+  width: 100%;
+  height: 100%;
+  background-color: rgb(8, 8, 8);
+  opacity: 0.2;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 96;
+}
+.error-modal {
+  width: 45%;
+  height: 40%;
+  background-color: rgb(25, 25, 25);
+  box-shadow: 0px 1px 3px 1px rgb(19, 19, 19);
+  border-radius: 10px;
+  border: 3px solid rgb(173, 173, 173);
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  z-index: 101;
+}
+.error-modal .title-box {
+  width: 100%;
+  height: 20%;
+  border-radius: 9px 9px 0 0;
+  margin: 10px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+.title-box span {
+  width: 100%;
+  height: 50%;
+  color: #d7d7d7;
+  font-size: 0.7rem;
+  font-weight: 600;
+  text-transform: capitalize;
+  text-align: left;
+  margin-left: 10px;
+}
+.error-modal .description {
+  width: 96%;
+  height: 100%;
+  margin: 10px;
+  overflow: hidden;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+.error-modal .description span {
+  width: 100%;
+  height: 50%;
+  margin-left: 10px;
+  color: #a8a8a8;
+  font-size: 0.8rem;
+  font-weight: 500;
+  text-transform: capitalize;
+  text-align: left;
+}
+
+.error-modal .btn-box {
+  grid-column: 2/4;
+  grid-row: 3;
+  width: 100%;
+  height: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.btn-box span {
+  color: #979595;
+  font-size: 0.6rem;
+  font-weight: 600;
+  align-self: flex-end;
+}
+.error-modal .copy-icon {
+  width: 5%;
+  height: 10%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  right: 5px;
+  top: 52px;
+}
+.copy-icon img {
+  width: 20px;
+  height: 20px;
+}
+.copy-icon:active img {
+  transform: scale(0.95);
+}
+.error-modal .copy-icon:active span {
+  display: block;
+  z-index: 1005;
+}
+
+.copy-icon span {
+  display: none;
+  background-color: transparent;
+  color: rgb(96, 150, 120);
+  font-size: 0.5rem;
+  position: absolute;
+  top: -15px;
+  right: 0;
+}
+</style>
