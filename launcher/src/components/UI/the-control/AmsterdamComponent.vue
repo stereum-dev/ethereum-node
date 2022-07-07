3c969fadebaacc5811eda2eeb67eef06baa1659f
mabasian <54101509+mabasian@users.noreply.github.com>
2022-07-07 13:51:40 +0200

    ADD: node connections added to dashboard (#513)
    * ADD:node conection comp. UI added to control dashboard
    
    * ADD: node connection row added
    
    * button condition updated

--- launcher/src/components/UI/the-control/AmsterdamComponent.vue
+++ launcher/src/components/UI/the-control/AmsterdamComponent.vue
@@ -55,7 +55,7 @@
               </div>
             </div>
           </sync-info>
-          <img :src="activeIco" />
+          <img :src="errorIco" />
         </div>
       </div>
     </div>
---
+++ launcher/src/components/UI/the-control/NodeConnectionRow.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="rowParent">
+    <div class="title">
+      <span>{{ rowName }}</span>
+    </div>
+    <div class="btn" :class="{ active: isActive }" @click="toggle">
+      <span>{{ onoff }}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["rowName"],
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+  computed: {
+    onoff() {
+      return this.isActive ? "ON" : "OFF";
+    },
+  },
+
+  methods: {
+    toggle() {
+      this.isActive = this.isActive ? false : true;
+    },
+  },
+};
+</script>
+<style scoped>
+.rowParent {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50%;
+  border-radius: 10px;
+  margin: 5% 0;
+}
+.active {
+  background: greenyellow !important;
+  color: #000 !important;
+}
+.title {
+  display: flex;
+  width: 70%;
+  font-size: 48%;
+  justify-content: flex-start;
+  align-items: center;
+  margin: 0 4%;
+}
+.btn {
+  display: flex;
+  width: 30%;
+  font-size: 50%;
+  font-weight: bold;
+  padding: 0 2px;
+  border-radius: 5px;
+  cursor: pointer;
+  color: #eee;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  border: 1px solid #343434;
+  background: rgb(42, 42, 42);
+}
+</style>
