71b8249e2c0baa9829272093ef7f4900c92fe2b7
maxbehzadi82 <69126271+maxbehzadi82@users.noreply.github.com>
2022-07-15 10:30:40 +0200

    ADD:new updates table + icons & static data

---
+++ launcher/src/components/UI/the-node/UpdateTable.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tableParent">
+    <div class="tableContent">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.tableParent {
+  width: 100%;
+  height: 60%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 5px 3px;
+}
+.tableContent {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+</style>
