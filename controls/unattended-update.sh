0f72d6528cbff90aa03f355de822b0918ea24bad
stefa2k <54934211+stefa2k@users.noreply.github.com>
2022-07-18 17:46:10 +0200

    ADD: update-os & wiring with unattended update (#554)

--- controls/unattended-update.sh
+++ controls/unattended-update.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-if ansible-playbook -v genericPlaybook.yaml --extra-vars "stereum_role=update-stereum"; then
-  ansible-playbook -v finishUpdate.yaml
-  ansible-playbook -v genericPlaybook.yaml --extra-vars "stereum_role=update-services"
-fi
+ansible-playbook -v genericPlaybook.yaml --extra-vars "stereum_role=update-os"
 
 # EOF
