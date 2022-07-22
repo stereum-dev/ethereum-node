0569102559baa9f7455f4571161f60ac8186869a
NeoPlays <80448387+NeoPlays@users.noreply.github.com>
2022-07-22 09:16:56 +0200

    ADD: Simple Validator Account Stats (#575)

---
+++ launcher/src/store/theStaking.js
@@ -0,0 +1,10 @@
+import { defineStore } from "pinia";
+
+export const useStakingStore = defineStore("theStaking", {
+  state: () => {
+    return {
+      totalBalance: 0,
+    };
+  },
+  actions: {},
+});
