2b389f0fdf7974e7f4855dc8a2bc4886b1360487
maxbehzadi82 <69126271+maxbehzadi82@users.noreply.github.com>
2022-08-10 11:35:13 +0200

    ADD:service updates table & header

--- launcher/src/components/UI/node-header/UpdatePanel.vue
+++ launcher/src/components/UI/node-header/UpdatePanel.vue
@@ -43,7 +43,18 @@
             restarted</span
           >
         </div>
-        <div class="service-updateBox"></div>
+        <div class="service-updateBox">
+          <div class="availableTable">
+            <div class="tableHeader">
+              <div class="tableUpdateIcon">
+                <img src="/img/icon/header-icons/update-green.png" alt="icon" />
+              </div>
+              <span>AVAILABLE PLUG-IN UPDATES</span>
+            </div>
+            <div class="tableContent"></div>
+            <div class="btnBox"></div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -93,6 +104,10 @@ export default {};
 .serviceUpdates {
   width: 100%;
   height: 60%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 .stereumUpdates-title,
 .serviceUpdates-title {
@@ -125,10 +140,7 @@ export default {};
   justify-content: center;
   align-items: center;
 }
-.service-updateBox {
-  width: 100%;
-  height: 75%;
-}
+
 .stereum-updateBox .versionBox {
   width: 50%;
   height: 100%;
@@ -241,5 +253,62 @@ export default {};
   color: #c6c6c6;
   align-self: center;
   text-align: left;
+}
+.service-updateBox {
+  width: 100%;
+  height: 75%;
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  padding: 5px;
+}
+.service-updateBox .availableTable {
+  width: 100%;
+  height: 100%;
+  background-color: #334d4d;
+  border: 3px solid #434343;
+  border-radius: 6px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-self: center;
+}
+.service-updateBox .availableTable .tableHeader {
+  width: 100%;
+  height: 10%;
+  background-color: #335959;
+  border-bottom: 3px solid #434343;
+  border-radius: 3px;
+  padding: 2px;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-template-rows: 1fr;
+}
+.tableHeader .tableUpdateIcon {
+  grid-column: 2/3;
+  grid-row: 1;
+  width:27%;
+  height:100%;
+  background-color: #2a4940;
+  border-radius: 100%;
+  margin-right: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  justify-self: flex-end;
+}
+.tableHeader .tableUpdateIcon img {
+  width: 10px;
+  height: 10px;
+}
+.tableHeader span {
+  grid-column: 3/6;
+  grid-row: 1;
+  width: 100%;
+  font-size: 0.6rem;
+  font-weight: 600;
+  color: #c6c6c6;
+  text-transform: uppercase;
+  align-self: center;
 }
 </style>
