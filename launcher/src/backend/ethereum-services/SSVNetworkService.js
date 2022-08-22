899b9f3a06513fa4803f425d947de181b493c237
stefa2k <54934211+stefa2k@users.noreply.github.com>
2022-08-22 15:22:50 +0200

    FIX: remove ssv network contract of config file (#690)

--- launcher/src/backend/ethereum-services/SSVNetworkService.js
+++ launcher/src/backend/ethereum-services/SSVNetworkService.js
@@ -11,7 +11,6 @@ eth2:
   BeaconNodeAddr: "http://beacon:5052"
 eth1:
   ETH1Addr: ""
-  RegistryContractAddr: "0x687fb596F3892904F879118e2113e1EEe8746C2E"
 OperatorPrivateKey: ""
 global:
   LogLevel: "debug"
@@ -22,7 +21,6 @@ MetricsAPIPort: 15000
   BeaconNodeAddr: "${consensusClients.map(client => client.buildConsensusClientHttpEndpointUrl())[0]}"
 eth1:
   ETH1Addr: "${executionClients.map(client => client.buildExecutionClientWsEndpointUrl())[0]}"
-  RegistryContractAddr: "0x687fb596F3892904F879118e2113e1EEe8746C2E"
 OperatorPrivateKey: ""
 global:
   LogLevel: info
