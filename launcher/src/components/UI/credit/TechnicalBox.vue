f5575cba607d241e1f31b4c291c237c6310395b9
mabasian <54101509+mabasian@users.noreply.github.com>
2022-08-25 14:52:25 +0200

    UPDATE: fetch data with the github

--- launcher/src/components/UI/credit/TechnicalBox.vue
+++ launcher/src/components/UI/credit/TechnicalBox.vue
@@ -3,16 +3,44 @@
 </template>
 <script>
 export default {
-    methods: {
-        
+  data() {
+    return {
+      results: [],
+    };
+  },
+  created() {
+    this.github();
+  },
+  methods: {
+    github() {
+      fetch(
+        "https://api.github.com/repos/stereum-dev/ethereum-node/contributors"
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].login,
+              avatar: data[id].avatar_url,
+            });
+          }
+          this.results = results;
+          console.log(this.results);
+        });
     },
-}
+  },
+};
 </script>
 <style scoped>
 .technical-box_parent {
   display: flex;
   width: 95%;
   height: 80%;
-  background: #b19e30;
 }
 </style>
