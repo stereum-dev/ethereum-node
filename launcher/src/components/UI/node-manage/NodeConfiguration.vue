6ab264f84da4329946fb912b677d91e6b87ba49b
NeoPlays <80448387+NeoPlays@users.noreply.github.com>
2022-08-31 16:10:53 +0200

    FIX: no error msg on failed validator import (#722)
    * catch bad request responses, fixes #447 and #711
    
    * edit special char list for escaping fixes #716

--- launcher/src/components/UI/node-manage/NodeConfiguration.vue
+++ launcher/src/components/UI/node-manage/NodeConfiguration.vue
@@ -108,6 +108,7 @@ export default {
       this.removeIsConfirmed = true;
       this.destroyNode();
       this.removeAllPlugins();
+      this.$router.push("/");
     },
     removeAllPlugins() {
       if (this.removeIsConfirmed) {
