1a4059a109dfd4b90aa70f56680ef0c0eaeda02c
daverolo <107847185+daverolo@users.noreply.github.com>
2022-09-28 16:03:26 +0200

    REFACTOR: queued requests for control components to avoid stuttering

--- launcher/src/pages/TheControl.vue
+++ launcher/src/pages/TheControl.vue
@@ -40,63 +40,81 @@ export default {
     }),
   },
   mounted() {
-    this.polling = setInterval(this.refresh, 1000); //refresh services
+    this.polling = setInterval(this.refresh, 100); //refresh services
   },
   beforeUnmount() {
     clearInterval(this.polling);
   },
   methods: {
+    async requestQueued(){
+        this.request = Array.isArray(this.request) ? this.request : [];
+        const ARGUMENTS = Array.prototype.slice.call(arguments); // convert functon "arguments" to Array
+        const meth = ARGUMENTS.length ? ARGUMENTS.shift() : null;
+        const args = ARGUMENTS.length ? ARGUMENTS : null;
+        if(meth in this.request && this.request[meth]){
+          return;
+        }
+        this.request[meth] = true;
+        const csvc = ControlService;
+        const func = eval("async () => {return await csvc."+meth+"()}")
+        const resp = await func();
+        this.request[meth] = false;
+        return resp;
+    },
     async refresh() {
       try {
-        // @FRONTEND - getNodeStats returns an object with 3 keys (code/info/data)
-        // code      : 0 (number!) means success all other values (including null or undefined) means error.
-        // info      : a message about the last result.
-        // data      : additional data (if available) or empty string
-        // Eeach main element in the "data" response has also the same 3 keys to handle errors indivudually!
-        // For example "data.syncstatus.{code|info|data}" or "data.p2pstatus.{code|info|data}"
-        // At the moment the following data is provided:
-        // data.syncstatus   : can be used for wiring launcher/src/components/UI/the-control/SyncStatus.vue
-        // data.p2pstatus    : can be used for wiring launcher/src/components/UI/the-control/PeerToPeer.vue
-        // data.storagestatus: can be used for wiring launcher/src/components/UI/the-control/TheStorage.vue
-        const nodeStats = await ControlService.getNodeStats();
-
-        console.log("@FRONTEND: data for wiring controls", nodeStats);
-        if (nodeStats) {
-          try {
-            this.code = nodeStats.code;
-            this.valPeer = nodeStats.data.p2pstatus.data.valPeer;
-            this.numPeer = nodeStats.data.p2pstatus.data.numPeer;
-            this.syncstatus = nodeStats.data.syncstatus;
-            this.storagestatus = nodeStats.data.storagestatus.data;
-            this.consensusClient =
-              nodeStats.data.p2pstatus.data.details.consensus.client;
-            this.consensusNumPeer =
-              nodeStats.data.p2pstatus.data.details.consensus.numPeer;
-            this.consensusValPeer =
-              nodeStats.data.p2pstatus.data.details.consensus.valPeer;
-            this.executionClient =
-              nodeStats.data.p2pstatus.data.details.execution.client;
-            this.executionValPeer =
-              nodeStats.data.p2pstatus.data.details.execution.valPeer;
-            this.executionNumPeer =
-              nodeStats.data.p2pstatus.data.details.execution.numPeer;
-          } catch (e) {}
-        }
-
-        const response = await ControlService.getServerVitals();
-        if (response) {
-          this.ServerName = response.ServerName;
-          this.totalRam = response.totalRam;
-          this.usedRam = response.usedRam;
-          this.availDisk = response.availDisk;
-          this.totalDisk = response.totalDisk;
-          this.usedPerc = response.usedPerc;
-          this.cpu = response.cpu;
-          this.rx = response.rx;
-          this.tx = response.tx;
-          this.readValue = response.readValue;
-          this.writeValue = response.writeValue;
-        }
+        // Get Node Stats
+        this.requestQueued('getNodeStats').then((nodeStats) => {
+          // @FRONTEND - getNodeStats returns an object with 3 keys (code/info/data)
+          // code      : 0 (number!) means success all other values (including null or undefined) means error.
+          // info      : a message about the last result.
+          // data      : additional data (if available) or empty string
+          // On error, the "data" key may or may not holds additional error information.
+          // On success, each main element in the "data" key has the same 3 keys to handle errors individually!
+          // For example "data.syncstatus.{code|info|data}" or "data.p2pstatus.{code|info|data}"
+          // At the moment the following data is provided:
+          // data.syncstatus   : can be used for wiring launcher/src/components/UI/the-control/SyncStatus.vue
+          // data.p2pstatus    : can be used for wiring launcher/src/components/UI/the-control/PeerToPeer.vue
+          // data.storagestatus: can be used for wiring launcher/src/components/UI/the-control/TheStorage.vue
+          // console.log("@FRONTEND: data for wiring controls", nodeStats);
+          if (nodeStats) {
+            try {
+              this.code = nodeStats.code;
+              this.valPeer = nodeStats.data.p2pstatus.data.valPeer;
+              this.numPeer = nodeStats.data.p2pstatus.data.numPeer;
+              this.syncstatus = nodeStats.data.syncstatus;
+              this.storagestatus = nodeStats.data.storagestatus.data;
+              this.consensusClient =
+                nodeStats.data.p2pstatus.data.details.consensus.client;
+              this.consensusNumPeer =
+                nodeStats.data.p2pstatus.data.details.consensus.numPeer;
+              this.consensusValPeer =
+                nodeStats.data.p2pstatus.data.details.consensus.valPeer;
+              this.executionClient =
+                nodeStats.data.p2pstatus.data.details.execution.client;
+              this.executionValPeer =
+                nodeStats.data.p2pstatus.data.details.execution.valPeer;
+              this.executionNumPeer =
+                nodeStats.data.p2pstatus.data.details.execution.numPeer;
+            } catch (e) {}
+          }
+        });
+        // Get Server Vitals
+        this.requestQueued('getServerVitals').then((response) => {
+          if (response) {
+            this.ServerName = response.ServerName;
+            this.totalRam = response.totalRam;
+            this.usedRam = response.usedRam;
+            this.availDisk = response.availDisk;
+            this.totalDisk = response.totalDisk;
+            this.usedPerc = response.usedPerc;
+            this.cpu = response.cpu;
+            this.rx = response.rx;
+            this.tx = response.tx;
+            this.readValue = response.readValue;
+            this.writeValue = response.writeValue;
+          }
+        });
       } catch (err) {
         console.log("some other error occured", err);
       }
