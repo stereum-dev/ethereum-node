6c60e69e6dae57384e14b2ed60155f2361f5948b
mabasian <54101509+mabasian@users.noreply.github.com>
2022-12-06 16:51:19 +0100

    Fix #563  - UI is ready

--- launcher/src/components/UI/node-header/NotifModal.vue
+++ launcher/src/components/UI/node-header/NotifModal.vue
@@ -2,7 +2,7 @@
   <div class="notif-modal-parent">
     <div class="modal-opacity" @click="$emit('closeMe')"></div>
     <div class="notif-modal-content">
-      <div class="content">
+      <div class="content" v-if="qrPage">
         <div class="title-box">
           <span>{{ $t("notifModal.notification") }}</span>
         </div>
@@ -11,18 +11,111 @@
             {{ $t("notifModal.notifText") }}
           </p>
         </div>
-        <div class="messageContent">
-          <img
-            src="../../../../public/img/icon/header-icons/monitor2.png"
-            alt="icon"
-          />
+        <div class="messageContent" @click="qrViewer">
+          <img :src="banner" alt="icon" />
         </div>
         <span class="close">{{ $t("notifModal.cancelText") }}</span>
+      </div>
+      <div class="qrPage_content" v-if="!qrPage">
+        <div class="banner" @click="qrViewer"><img :src="banner" /></div>
+        <div class="qrContent">
+          <div class="qrCode-boxes">
+            <span
+              >1- VISIT THE APPSTORE OR OUR WEBSITE & DOWNLOAD "STEREUM NODE
+              MONITOR"
+            </span>
+            <span>2. OPEN "STEREUM MONITOR" ON YOUR SMARTPHONE </span>
+            <span>3. SCAN THE QR CODE AT: </span>
+            <span>4. SEND A TEST NOTIFICATION</span>
+          </div>
+          <div class="qrCode-boxes">
+            <div class="qrCode-place-holder">
+              <comming-soon></comming-soon>
+              <div class="qrCode">
+                <img :src="qrCode" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      qrPage: true,
+      banner: "/img/icon/header-icons/monitor2.png",
+      //BACKEND
+      //qrCode is a dummy data
+      qrCode: "/img/icon/header-icons/dummyQR.png",
+    };
+  },
+  methods: {
+    qrViewer() {
+      this.qrPage = !this.qrPage;
+    },
+  },
+};
+</script>
 <style scoped>
+.qrCode {
+  width: 70%;
+  height: 70%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.qrCode-place-holder {
+  background: #1d1d1d;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  border: 1px solid grey;
+  border-radius: 10px;
+  box-shadow: 1px 1px 5px 1px #001717;
+  position: relative;
+}
+.qrCode-boxes {
+  width: 40%;
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+  color: #c3c3c3;
+  font-size: 90%;
+  font-weight: 600;
+  justify-content: space-around;
+}
+
+.qrContent {
+  height: 70%;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.qrPage_content {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.banner {
+  width: 100%;
+  height: 30%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.banner img {
+  width: 60%;
+  margin-left: 2%;
+}
 .notif-modal-parent {
   width: 100%;
   height: 100%;
@@ -88,7 +181,6 @@
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #2e553b;
 }
 .title-box span {
   font-size: 1.6rem;
@@ -104,6 +196,7 @@
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
+  cursor: pointer;
 }
 .messageContent img {
   width: 80%;
