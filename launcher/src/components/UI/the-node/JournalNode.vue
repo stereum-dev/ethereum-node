2b5db3ef37f6de5f5436ff69d0e492035ad04b6c
maxbehzadi82 <69126271+maxbehzadi82@users.noreply.github.com>
2022-12-09 13:33:29 +0100

    ADD:shutdown node btn

--- launcher/src/components/UI/the-node/JournalNode.vue
+++ launcher/src/components/UI/the-node/JournalNode.vue
@@ -10,7 +10,7 @@
         </div>
       </div>
     </div>
-    <div class="config-bg">
+    <div class="configBtn">
       <div class="edit-btn">
         <router-link to="/manage">
           <span>{{ $t("journalnode.edit") }}</span>
@@ -20,6 +20,13 @@
           />
         </router-link>
       </div>
+      <div class="state-btn">
+        <span>Trun node off</span>
+        <img
+          src="../../../../public/img/icon/node-journal-icons/power2.png"
+          alt="icon"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -27,10 +34,8 @@
 import ControlService from "@/store/ControlService";
 import UpdateTable from "./UpdateTable.vue";
 import { mapState } from "pinia";
-import { mapWritableState } from "pinia";
-import { useServices } from "@/store/services.js";
 import { useControlStore } from "../../../store/theControl";
-import { useNodeHeader } from "../../../store/nodeHeader";
+
 export default {
   components: { UpdateTable },
   data() {
@@ -60,18 +65,6 @@ export default {
   grid-template-columns: 1fr;
   justify-content: center;
   align-items: center;
-}
-.config-bg {
-  grid-column: 1;
-  grid-row: 3/10;
-  width: 95%;
-  height: 96%;
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: repeat(9, 1fr);
-  background-color: #606060;
-  border-radius: 10px;
-  margin: 0 auto;
 }
 
 .server {
@@ -172,62 +165,24 @@ export default {
   text-overflow: clip;
   align-self: center;
 }
-
-.config-btns {
+.configBtn {
   grid-column: 1;
-  grid-row: 2/10;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-  margin-top: 5px;
-}
-
-.config-btns .config-update {
-  grid-column: 1;
-  grid-row: 2/3;
-  width: 90%;
-  height: 32px;
-  background-color: #292929;
-  font-size: 0.7rem;
-  font-weight: 700;
-  color: rgb(196, 196, 196);
-  border: 1px solid #787878;
-  margin-top: 5px;
-  border-radius: 8px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  cursor: pointer;
-  box-shadow: 0 1px 3px 1px #2c2c2c;
-}
-
-.config-btns .config-update span {
-  width: 50%;
-  font-size: 0.8rem;
-  font-weight: 700;
-  color: rgb(196, 196, 196);
-}
-.config-btns .config-update img {
-  width: 20px;
-  height: 17px;
-  margin-right: 10px;
-}
-
-.config-btns .config-update:hover {
-  background-color: #2c2c2c;
-}
-
-.config-btns .config-update:active {
-  box-shadow: none;
-  transform: scale(0.99);
+  grid-row: 3/10;
+  width: 95%;
+  height: 96%;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(8, 1fr);
+  background-color: #606060;
+  border-radius: 10px;
+  margin: 0 auto;
 }
 
 .edit-btn {
   grid-column: 1;
   grid-row: 1/2;
   width: 95%;
-  height: 95%;
+  height: 80%;
   margin-top: 4px;
   display: flex;
   justify-content: center;
@@ -278,136 +233,29 @@ export default {
   text-decoration: none;
   color: #4eb051;
 }
-
-.delete-btn img {
-  width: 24px;
-  height: 24px;
-  margin-right: 5px;
-}
-.btn-icon {
-  width: 21px;
-  height: 21px;
-  border-radius: 5px;
-  margin-right: 5px;
-}
-.btn-icon img {
-  width: 21px;
-  height: 21px;
-}
-
-.config-row {
-  grid-column: 1/7;
-  grid-row: 2;
+.state-btn {
+  grid-column: 1;
+  grid-row: 2/3;
   width: 95%;
-  height: 30px;
-  border: 2px solid rgb(155, 212, 236);
-  border-radius: 10px;
+  height: 80%;
+  padding: 0 10px;
+  margin-top: 4px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 3px;
-  margin: 0 auto;
-}
-
-.config-row .row-content {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  width: 100%;
-  height: 25px;
-}
-.config-row .row-content span {
-  width: 80%;
+  justify-self: center;
+  background-color: #242529;
   font-size: 0.7rem;
-  font-weight: 700;
-}
-.testnet-icon {
-  width: 20%;
-  min-width: 23px;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-}
-.testnet-icon img {
-  width: 23px;
-  height: 23px;
-}
-.remove-modal-parent {
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 97;
-}
-.tableRow {
-  width: 99%;
-  height: 25px;
-  background-color: rgb(36, 35, 35);
+  font-weight: 800;
+  text-transform: uppercase;
+  color: rgb(220, 10, 3);
   border: 1px solid #787878;
-  border-radius: 5px;
-  padding: 3px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-.serviceName {
-  width: 40%;
-  height: 25px;
-  font-size: 0.7rem;
-  font-weight: 600;
-  color: #a5a5a5;
-  margin-left: 3px;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: clip;
-  text-transform: capitalize;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-}
-.version {
-  width: 30%;
-  height: 25px;
-  font-size: 0.7rem;
-  font-weight: 600;
-  color: #c6ad51;
-  margin-left: 3px;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: clip;
-  text-transform: capitalize;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-}
-.downloadIcon {
-  width: 15%;
-  height: 25px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.downloadIcon img {
-  width: 15px;
-  height: 18px;
+  border-radius: 8px;
   cursor: pointer;
+  box-shadow: 0 1px 3px 1px #2c2c2c;
 }
-.disabledDownloadIcon {
-  width: 15%;
-  height: 25px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.disabledDownloadIcon img {
-  width: 15px;
-  height: 18px;
-}
-.downloadIcon img:hover {
-  transform: scale(1.1);
-}
-.downloadIcon img:active {
-  transform: scale(1);
+.state-btn img {
+  width: 10%;
+  margin-right: 8px;
 }
 </style>
