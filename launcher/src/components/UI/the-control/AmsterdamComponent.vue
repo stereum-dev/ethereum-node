d322916d05aece4615d09091f14a1bc0b9832281
mabasian <54101509+mabasian@users.noreply.github.com>
2023-01-04 00:06:23 +0100

    REFACTOR: squares are created

--- launcher/src/components/UI/the-control/AmsterdamComponent.vue
+++ launcher/src/components/UI/the-control/AmsterdamComponent.vue
@@ -7,22 +7,22 @@
       <span>{{ $t("controlPage.node") }}</span>
     </div>
     <div class="docBox">
-      <div class="upSide">
-        <div class="epochBox">
-          <div class="ttlEpoch"><span>EPOCH</span></div>
-          <div class="epochText">
-            <span>{{ counter }} #</span>
-          </div>
-          <div class="downTtl"><span class="downTtl">CONFIRMED</span></div>
-        </div>
-      </div>
-      <div class="downSide">
-        <div class="fahrradIco">
-          <img src="../../../../public/img/icon/control/amsterdam.png" />
-        </div>
-        <div class="dateBox">
-          <span>{{ days }} days</span>
-          <span>{{ date }}</span>
+      <div
+        class="containerSquare"
+        v-for="item in items"
+        :key="item"
+        :data-tooltip="item"
+      >
+        <div
+          v-for="(n, index) in 32"
+          :key="index"
+          class="square"
+          :style="{
+            background: index === item * 2 ? 'red' : 'green',
+            '!important': index === item * 2,
+          }"
+        >
+          {{ item * 32 * n }}
         </div>
       </div>
     </div>
@@ -43,6 +43,170 @@ export default {
       testnetIcon: "/img/icon/control/testnetIconControl.png",
       days: null,
       date: "",
+      items: [
+        [
+          {
+            id: 1,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 2,
+            color: 0,
+            column: 2,
+          },
+          {
+            id: 3,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 4,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 5,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 6,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 7,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 8,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 9,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 10,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 11,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 12,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 13,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 14,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 15,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 16,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 17,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 18,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 19,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 20,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 21,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 22,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 23,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 24,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 25,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 26,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 27,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 28,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 29,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 30,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 31,
+            color: 0,
+            column: 1,
+          },
+          {
+            id: 32,
+            color: 0,
+            column: 1,
+          },
+        ],
+      ],
     };
   },
 
@@ -61,6 +225,37 @@ export default {
 };
 </script>
 <style scoped>
+* {
+  box-sizing: border-box;
+}
+[data-tooltip] {
+  position: relative;
+  cursor: default;
+}
+[data-tooltip]::after {
+  position: absolute;
+  width: max-content;
+  left: calc(50%-25%);
+
+  text-align: center;
+  content: attr(data-tooltip);
+  color: #eee;
+  background: black;
+  border-radius: 5px;
+  font-size: 70%;
+  padding: 2% 3%;
+  border: 1px solid #929292;
+  text-transform: uppercase;
+  visibility: hidden;
+  opacity: 0;
+  transform: translateY(20%);
+  transition: opacity 0.3s transform 0.2s;
+}
+[data-tooltip]:hover::after {
+  opacity: 1;
+  visibility: visible;
+  transform: rotateY(0);
+}
 .serviceName {
   display: flex;
   justify-content: center;
@@ -136,99 +331,22 @@ export default {
   height: 100%;
   display: flex;
   flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-end;
 }
-.upSide {
-  height: 50%;
-  width: 100%;
+.containerSquare {
   display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-}
-.epochBox {
+  flex-wrap: nowrap;
   width: 95%;
-  height: 98%;
-  display: flex;
-  background: #33393e;
-  flex-direction: column;
-  border-radius: 5px;
-  justify-content: center;
+  height: 20%;
+  justify-content: space-between;
   align-items: center;
-}
-.ttlEpoch {
-  width: 90%;
-  display: flex;
-  height: 22%;
-  font-size: 50%;
-  margin-bottom: 2%;
-  font-weight: bold;
-}
-.epochText {
-  width: 90%;
-  height: 50%;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  border: 1px solid #c1c1c1;
-  background: #000;
-  font-size: 80%;
-  padding-right: 2%;
-  border-radius: 3px;
-}
-.downTtl {
-  width: 90%;
-  height: 22%;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  font-size: 15%;
-  margin-top: 1%;
-}
-.downSide {
-  width: 100%;
-  height: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.fahrradIco {
-  height: 85%;
-  width: 20%;
-  background: #33393e;
-  border: 1px solid #191b1d;
-  border-radius: 5px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.fahrradIco img {
-  width: 80%;
-}
-.dateBox {
-  width: 40%;
-  height: 80%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  font-size: 45%;
-  background: #33393e;
-  border-radius: 5px;
-  border: 1px solid #191b1d;
 }
 
-.circleArrow {
-  width: 20%;
-  height: 80%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-left: 5%;
-  background: #33393e;
-  border-radius: 5px;
-  border: 1px solid #191b1d;
-}
-.circleArrow img {
-  width: 60%;
+.square {
+  width: 3px;
+  height: 3px;
+  background-color: green;
+  color: transparent;
 }
 </style>
