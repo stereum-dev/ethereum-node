75e02bf67996c27ff03d83eba17f8df6f4ce787c
mabasian <54101509+mabasian@users.noreply.github.com>
2023-01-05 10:02:56 +0100

    REFACTOR: squares are created (#991)
    * REFACTOR: squares are created
    
    * REFACTOR: the Node Widget refactored

--- launcher/src/components/UI/the-control/AmsterdamComponent.vue
+++ launcher/src/components/UI/the-control/AmsterdamComponent.vue
@@ -7,23 +7,18 @@
       <span>{{ $t("controlPage.node") }}</span>
     </div>
     <div class="docBox">
-      <div class="upSide">
-        <div class="epochBox">
-          <div class="ttlEpoch"><span>EPOCH</span></div>
-          <div class="epochText">
-            <span>{{ counter }} #</span>
-          </div>
-          <div class="downTtl"><span class="downTtl">CONFIRMED</span></div>
-        </div>
-      </div>
-      <div class="downSide">
-        <div class="fahrradIco">
-          <img src="../../../../public/img/icon/control/amsterdam.png" />
-        </div>
-        <div class="dateBox">
-          <span>{{ days }} days</span>
-          <span>{{ date }}</span>
-        </div>
+      <div
+        class="line-squares"
+        v-for="(row, index) in rows"
+        :key="index"
+        :data-tooltip="index"
+      >
+        <div
+          class="square"
+          v-for="square in row"
+          :key="square.id"
+          :style="{ backgroundColor: square.color }"
+        ></div>
       </div>
     </div>
   </div>
@@ -43,6 +38,178 @@ export default {
       testnetIcon: "/img/icon/control/testnetIconControl.png",
       days: null,
       date: "",
+      rows: [
+        [
+          { id: 1, color: "#eee" },
+          { id: 2, color: "#eee" },
+          { id: 3, color: "#eee" },
+          { id: 4, color: "#eee" },
+          { id: 5, color: "#eee" },
+          { id: 6, color: "#eee" },
+          { id: 7, color: "#eee" },
+          { id: 8, color: "#eee" },
+          { id: 9, color: "#eee" },
+          { id: 10, color: "#eee" },
+          { id: 11, color: "#eee" },
+          { id: 12, color: "#eee" },
+          { id: 13, color: "#eee" },
+          { id: 14, color: "#eee" },
+          { id: 15, color: "#eee" },
+          { id: 16, color: "#eee" },
+          { id: 17, color: "#eee" },
+          { id: 18, color: "#eee" },
+          { id: 19, color: "#eee" },
+          { id: 20, color: "#eee" },
+          { id: 21, color: "#eee" },
+          { id: 22, color: "#eee" },
+          { id: 23, color: "#eee" },
+          { id: 24, color: "#eee" },
+          { id: 25, color: "#eee" },
+          { id: 26, color: "#eee" },
+          { id: 27, color: "#eee" },
+          { id: 28, color: "#eee" },
+          { id: 29, color: "#eee" },
+          { id: 30, color: "#eee" },
+          { id: 31, color: "#eee" },
+          { id: 32, color: "#eee" },
+        ],
+        [
+          { id: 33, color: "#eee" },
+          { id: 34, color: "#eee" },
+          { id: 35, color: "#eee" },
+          { id: 36, color: "#eee" },
+          { id: 37, color: "#eee" },
+          { id: 38, color: "#eee" },
+          { id: 39, color: "#eee" },
+          { id: 40, color: "#eee" },
+          { id: 41, color: "#eee" },
+          { id: 42, color: "#eee" },
+          { id: 43, color: "#eee" },
+          { id: 44, color: "#eee" },
+          { id: 45, color: "#eee" },
+          { id: 46, color: "#eee" },
+          { id: 47, color: "#eee" },
+          { id: 48, color: "#eee" },
+          { id: 49, color: "#eee" },
+          { id: 50, color: "#eee" },
+          { id: 51, color: "#eee" },
+          { id: 52, color: "#eee" },
+          { id: 53, color: "#eee" },
+          { id: 54, color: "#eee" },
+          { id: 55, color: "#eee" },
+          { id: 56, color: "#eee" },
+          { id: 57, color: "#eee" },
+          { id: 58, color: "#eee" },
+          { id: 59, color: "#eee" },
+          { id: 60, color: "#eee" },
+          { id: 61, color: "#eee" },
+          { id: 62, color: "#eee" },
+          { id: 63, color: "#eee" },
+          { id: 64, color: "#eee" },
+        ],
+        [
+          { id: 65, color: "#eee" },
+          { id: 66, color: "#eee" },
+          { id: 67, color: "#eee" },
+          { id: 68, color: "#eee" },
+          { id: 69, color: "#eee" },
+          { id: 70, color: "#eee" },
+          { id: 71, color: "#eee" },
+          { id: 72, color: "#eee" },
+          { id: 73, color: "#eee" },
+          { id: 74, color: "#eee" },
+          { id: 75, color: "#eee" },
+          { id: 76, color: "#eee" },
+          { id: 77, color: "#eee" },
+          { id: 78, color: "#eee" },
+          { id: 79, color: "#eee" },
+          { id: 80, color: "#eee" },
+          { id: 81, color: "#eee" },
+          { id: 82, color: "#eee" },
+          { id: 83, color: "#eee" },
+          { id: 84, color: "#eee" },
+          { id: 85, color: "#eee" },
+          { id: 86, color: "#eee" },
+          { id: 87, color: "#eee" },
+          { id: 88, color: "#eee" },
+          { id: 89, color: "#eee" },
+          { id: 90, color: "#eee" },
+          { id: 91, color: "#eee" },
+          { id: 92, color: "#eee" },
+          { id: 93, color: "#eee" },
+          { id: 94, color: "#eee" },
+          { id: 95, color: "#eee" },
+          { id: 96, color: "#eee" },
+        ],
+        [
+          { id: 97, color: "#eee" },
+          { id: 98, color: "#eee" },
+          { id: 99, color: "#eee" },
+          { id: 100, color: "#eee" },
+          { id: 101, color: "#eee" },
+          { id: 102, color: "#eee" },
+          { id: 103, color: "#eee" },
+          { id: 104, color: "#eee" },
+          { id: 105, color: "#eee" },
+          { id: 106, color: "#eee" },
+          { id: 107, color: "#eee" },
+          { id: 108, color: "#eee" },
+          { id: 109, color: "#eee" },
+          { id: 110, color: "#eee" },
+          { id: 111, color: "#eee" },
+          { id: 112, color: "#eee" },
+          { id: 113, color: "#eee" },
+          { id: 114, color: "#eee" },
+          { id: 115, color: "#eee" },
+          { id: 116, color: "#eee" },
+          { id: 117, color: "#eee" },
+          { id: 118, color: "#eee" },
+          { id: 119, color: "#eee" },
+          { id: 120, color: "#eee" },
+          { id: 121, color: "#eee" },
+          { id: 122, color: "#eee" },
+          { id: 123, color: "#eee" },
+          { id: 124, color: "#eee" },
+          { id: 125, color: "#eee" },
+          { id: 126, color: "#eee" },
+          { id: 127, color: "#eee" },
+          { id: 128, color: "#eee" },
+        ],
+        [
+          { id: 129, color: "#eee" },
+          { id: 130, color: "#eee" },
+          { id: 131, color: "#eee" },
+          { id: 132, color: "#eee" },
+          { id: 133, color: "#eee" },
+          { id: 134, color: "#eee" },
+          { id: 135, color: "#eee" },
+          { id: 136, color: "#eee" },
+          { id: 137, color: "#eee" },
+          { id: 138, color: "#eee" },
+          { id: 139, color: "#eee" },
+          { id: 140, color: "#eee" },
+          { id: 141, color: "#eee" },
+          { id: 142, color: "#eee" },
+          { id: 143, color: "#eee" },
+          { id: 144, color: "#eee" },
+          { id: 145, color: "#eee" },
+          { id: 146, color: "#eee" },
+          { id: 147, color: "#eee" },
+          { id: 148, color: "#eee" },
+          { id: 149, color: "#eee" },
+          { id: 150, color: "#eee" },
+          { id: 151, color: "#eee" },
+          { id: 152, color: "#eee" },
+          { id: 153, color: "#eee" },
+          { id: 154, color: "#eee" },
+          { id: 155, color: "#eee" },
+          { id: 156, color: "#eee" },
+          { id: 157, color: "#eee" },
+          { id: 158, color: "#eee" },
+          { id: 159, color: "#eee" },
+          { id: 160, color: "#eee" },
+        ],
+      ],
     };
   },
 
@@ -61,6 +228,38 @@ export default {
 };
 </script>
 <style scoped>
+* {
+  box-sizing: border-box;
+}
+
+[data-tooltip] {
+  position: relative;
+  cursor: default;
+}
+[data-tooltip]::after {
+  position: absolute;
+  width: max-content;
+  left: calc(50%-25%);
+
+  text-align: center;
+  content: attr(data-tooltip);
+  color: #eee;
+  background: black;
+  border-radius: 5px;
+  font-size: 70%;
+  padding: 2% 3%;
+  border: 1px solid #929292;
+  text-transform: uppercase;
+  visibility: hidden;
+  opacity: 0;
+  transform: translateY(20%);
+  transition: opacity 0.3s transform 0.2s;
+}
+[data-tooltip]:hover::after {
+  opacity: 1;
+  visibility: visible;
+  transform: rotateY(0);
+}
 .serviceName {
   display: flex;
   justify-content: center;
@@ -136,99 +335,20 @@ export default {
   height: 100%;
   display: flex;
   flex-direction: column;
-}
-.upSide {
-  height: 50%;
-  width: 100%;
-  display: flex;
   justify-content: center;
-  align-items: center;
-  flex-direction: column;
+  align-items: flex-start;
+  border-radius: 0 10px 10px 0;
 }
-.epochBox {
-  width: 95%;
-  height: 98%;
+.line-squares {
+  width: 97%;
+  height: 18%;
   display: flex;
-  background: #33393e;
-  flex-direction: column;
-  border-radius: 5px;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 }
-.ttlEpoch {
-  width: 90%;
-  display: flex;
-  height: 22%;
-  font-size: 50%;
-  margin-bottom: 2%;
-  font-weight: bold;
-}
-.epochText {
-  width: 90%;
-  height: 50%;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  border: 1px solid #c1c1c1;
-  background: #000;
-  font-size: 80%;
-  padding-right: 2%;
-  border-radius: 3px;
-}
-.downTtl {
-  width: 90%;
-  height: 22%;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  font-size: 15%;
-  margin-top: 1%;
-}
-.downSide {
-  width: 100%;
-  height: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.fahrradIco {
-  height: 85%;
-  width: 20%;
-  background: #33393e;
-  border: 1px solid #191b1d;
-  border-radius: 5px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.fahrradIco img {
-  width: 80%;
-}
-.dateBox {
-  width: 40%;
-  height: 80%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  font-size: 45%;
-  background: #33393e;
-  border-radius: 5px;
-  border: 1px solid #191b1d;
-}
-
-.circleArrow {
-  width: 20%;
-  height: 80%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-left: 5%;
-  background: #33393e;
-  border-radius: 5px;
-  border: 1px solid #191b1d;
-}
-.circleArrow img {
-  width: 60%;
+.square {
+  background: #4dfff3;
+  width: 1.9%;
+  height: 30%;
 }
 </style>
