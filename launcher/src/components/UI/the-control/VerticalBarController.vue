d1c7211b27ef9914de6a671be49983e3a7e48f25
mabasian <54101509+mabasian@users.noreply.github.com>
2023-03-14 09:55:34 +0100

    Update VerticalBarController.vue

--- launcher/src/components/UI/the-control/VerticalBarController.vue
+++ launcher/src/components/UI/the-control/VerticalBarController.vue
@@ -16,16 +16,33 @@ export default {
   data() {
     return {
       inputValue: [],
+      arrayWith18Ones: Array(10).fill(0),
     };
   },
   computed: {
     // every 6 sec update
     //it's defult and fix stracture!
-    arrayWith18Ones() {
-      return Array(10).fill(0);
-    },
+    // arrayWith18Ones() {
+    //   return Array(10).fill(0);
+    // },
+  },
+  created() {
+    setInterval(() => {
+      this.barGenerator();
+    }, 6000);
   },
   methods: {
+    barGenerator() {
+      const newBars = [];
+      // newBars is just a sample to show the functionallity and it has to change with the right data
+      // Generate 6 new random numbers between 1 and 4
+      for (let i = 0; i < 10; i++) {
+        newBars.push(Math.floor(Math.random() * 4) + 1);
+      }
+
+      // Replace the old array with the new one
+      this.arrayWith18Ones = newBars;
+    },
     getColor(number) {
       switch (number) {
         case 0:
@@ -33,9 +50,11 @@ export default {
         case 1:
           return "#74FA65";
         case 2:
-          return "#EB5353";
+          return "#FFD924";
         case 3:
-          return "#0013de";
+          return "#FA831B";
+        case 4:
+          return "#EB5353";
       }
     },
   },
