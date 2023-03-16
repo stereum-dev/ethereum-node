2a7f442304ab26a6fbfa941cdfbd4cf70f3c6aa6
NeoPlays <80448387+NeoPlays@users.noreply.github.com>
2023-03-16 05:02:20 +0100

    ADD: ValidatorEjector, package updates, small fixes (#1134)
    * ADD: ValidatorEjector, package updates, small fixes
    
    * small adjustment
    
    * update electron

--- launcher/src/components/UI/node-manage/ModifyPanel.vue
+++ launcher/src/components/UI/node-manage/ModifyPanel.vue
@@ -141,7 +141,7 @@ export default {
       options: [],
       checkedRelays: [],
       availableBlocks: [],
-      noPortServices: ["FlashbotsMevBoostService", "PrometheusNodeExporterService", "NotificationService", "Web3SignerService"],
+      noPortServices: ["FlashbotsMevBoostService", "PrometheusNodeExporterService", "NotificationService", "Web3SignerService", "ValidatorEjectorService"],
     };
   },
   computed: {
