9c6425a1cb1e4d5ba19e7ba8439b7c033101865b
maxbehzadi82 <69126271+MaxTheGeeek@users.noreply.github.com>
2023-04-18 20:42:22 +0200

    ADD:Tabbar for state routing

---
+++ launcher/src/components/UI/the-staking/SyncCommitte.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="sync_box">
+    <div class="syncCommitte">
+      <div class="totalBox">
+        <div class="totalTitle">
+          <span>total</span>
+        </div>
+        <div class="totalValue">
+          <span>0/0</span>
+        </div>
+      </div>
+      <div class="statusBox">
+        <div class="statusTitle">
+          <span>STATUS</span>
+        </div>
+        <div v-if="blockIsOnline" class="statusValue">
+          <span>Assigned Slot </span>
+          <span>4989008</span>
+        </div>
+        <div v-else class="statusValue">
+          <span>Not Assigned</span>
+          <span>---</span>
+        </div>
+        <div v-if="blockIsOnline" class="nextBlock">
+          <p>Next Block Proposal in: <span>44 sec</span></p>
+        </div>
+        <div v-else class="nextBlock">
+          <p>Next Block Proposal in: <span>?? sec</span></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blockIsOnline: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.sync_box {
+  grid-column: 1/7;
+  grid-row: 6/10;
+  width: 100%;
+  height: 100%;
+  margin-top: 5px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.syncCommitte {
+  width: 70%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.committeBox {
+  width: 100%;
+  height: 20%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.committeIcon {
+  width: 10%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.committeIcon img {
+  width: 20px;
+}
+.committeIcon span {
+  color: #efefef;
+  font-size: 0.5rem;
+  font-weight: 600;
+  text-align: center;
+}
+.committeTitle {
+  width: 70%;
+  height: 100%;
+  padding: 0 3px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  background: #318d1f;
+  border: 1px solid #9e9e9e;
+  border-radius: 3px;
+}
+.committeTitle span {
+  color: #fff;
+  font-size: 0.5rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  overflow: hidden;
+}
+.statusBox {
+  width: 100%;
+  height: 20%;
+  margin-top: 5px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.statusTitle,
+.startEpoch_title,
+.endEpoch_title {
+  width: 50%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding-left: 30px;
+}
+.statusTitle span,
+.startEpoch_title span,
+.endEpoch_title span {
+  color: #efefef;
+  font-size: 0.5rem;
+  font-weight: 600;
+  text-align: center;
+  text-transform: capitalize;
+}
+.statusValue {
+  width: 50%;
+  height: 100%;
+  padding: 0 3px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.statusValue span {
+  color: #4cab39;
+  font-size: 0.5rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  overflow: hidden;
+}
+
+.startEpoch,
+.endEpoch {
+  width: 100%;
+  height: 20%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.startEpoch_value,
+.endEpoch_value {
+  width: 50%;
+  height: 95%;
+  padding: 3px 3px 0 3px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #171717;
+  border: 1px solid #9e9e9e;
+  border-radius: 3px;
+}
+.startEpoch_value span,
+.endEpoch_value span {
+  color: #efd96bdf;
+  font-size: 0.6rem;
+  font-weight: 600;
+  text-align: center;
+  overflow: hidden;
+}
+</style>
---
+++ launcher/src/components/UI/the-staking/TabBar.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="tabbar">
+    <div
+      v-for="(item, index) in tabs"
+      :key="index"
+      :class="['tabbar-item', { active: index === activeIndex }]"
+      class="tab"
+      @click="getItem(item, index)"
+    >
+      <img :src="item.imgPath" alt="icon" @mousedown.prevent />
+      <div class="title">
+        <span> {{ optionsTitle }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    tabs: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      activeIndex: 0, // set the default active index to the first item
+      optionsTitle: "ATTESTATION",
+    };
+  },
+  mounted() {},
+  methods: {
+    getItem(item, index) {
+      this.activeIndex = index;
+      this.optionsTitle = item.title;
+      this.$emit("getTitle", this.optionsTitle);
+    },
+  },
+};
+</script>
+<style scoped>
+.tabbar {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  width: 100%;
+  height: 40px;
+}
+.tabbar .tab {
+  padding-bottom: 5px;
+}
+.tabbar-item {
+  flex: 1;
+  height: 100%;
+  border-radius: 10px;
+  padding-top: 3px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  background-color: #076462a9;
+  border: 1px solid #1e6e6da9;
+  font-size: 14px;
+  color: #666;
+  cursor: pointer;
+}
+.tabbar-item img {
+  width: 20px;
+  height: 20px;
+  margin-top: 2px;
+}
+
+.tabbar-item.active {
+  color: #333;
+  background-color: #237e7d;
+  box-shadow: 1px 1px 5px 1px rgb(43, 43, 43);
+  transition-duration: 200ms;
+}
+.tabbar-item.active .title {
+  color: #fff;
+}
+.tabbar-item .title {
+  width: 97.2%;
+  height: 30%;
+  min-height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #237e7d;
+  border-radius: 0 0 10px 10px;
+  position: absolute;
+  bottom: 3px;
+  left: 3px;
+}
+.title span {
+  color: #fff;
+  font-size: 0.7rem;
+  font-weight: 600;
+}
+.disabled {
+  background-color: #050505;
+  opacity: 0.5;
+  cursor: not-allowed;
+}
+</style>
