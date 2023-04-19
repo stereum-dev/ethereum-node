ea6e1c980d3218f153d2be8ce57e42ae9c37f191
maxbehzadi82 <69126271+MaxTheGeeek@users.noreply.github.com>
2023-04-19 14:23:07 +0200

    ADD: Dynamic comp for state & tabbar

--- launcher/src/components/UI/the-staking/ValidatorState.vue
+++ launcher/src/components/UI/the-staking/ValidatorState.vue
@@ -5,28 +5,7 @@
     </div>
     <div class="stateBox">
       <div class="stateInnerBox">
-        <div class="dropDown_parent">
-          <div class="title">
-            <span>VALIDATOR</span>
-          </div>
-          <div class="dropDown_box">
-            <div class="dropDown" @click="toggleDropDown">
-              <div v-if="!selectedValidator.key" class="options">Choose a validator</div>
-              <div v-else class="options">
-                {{ selectedValidator.key }}
-              </div>
-
-              <div class="dropDown_icon">
-                <img :class="{ rotate: dropDownIsOpen }" src="/img/icon/arrows/arrow-down.png" alt="Arrow Icon" />
-              </div>
-            </div>
-            <div v-if="dropDownIsOpen" class="valueBox" @mouseleave="dropDownIsOpen = false">
-              <div v-for="(key, index) in keys" :key="index" class="options_value" @click="chooseValidator(key)">
-                <span>{{ `${key.key.substring(0, 10)}...${key.key.substring(key.key.length - 15)}` }}</span>
-              </div>
-            </div>
-          </div>
-        </div>
+        <StateDropdown :keys="keys" />
         <div class="indexParent">
           <div class="indexBox">
             <div class="index">
@@ -56,51 +35,69 @@
         </div>
         <div class="componentParent">
           <div class="dynamicComponent">
-            <component :is="SyncCommitte" />
+            <div
+              v-for="(item, index) in optionsType"
+              v-show="item.title === currentComponent"
+              :key="index"
+              class="outterBox"
+            >
+              <component :is="item.comp" v-bind="item" />
+            </div>
           </div>
           <div class="predicitionIcon">
             <img src="/img/icon/the-staking/predicition-icon.png" alt="Icon" />
           </div>
         </div>
-
-        <div v-if="SyncCommitteIsActive" class="syncParent">
-          <SyncCommitte />
-          <div class="description">
-            <p>New Sync Committee in <span>256</span> Epochs</p>
-          </div>
-        </div>
-        <div class="historyParent"></div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import StateDropdown from "./StateDropdown.vue";
 import TotalBalance from "./TotalBalance.vue";
 import TabBar from "./TabBar.vue";
 import SyncCommitte from "./SyncCommitte.vue";
+import BlockProduction from "./BlockProduction.vue";
+import TheAttestation from "./TheAttestation.vue";
 import { mapWritableState } from "pinia";
 import { useStakingStore } from "@/store/theStaking";
 
 export default {
   components: {
     TotalBalance,
+    StateDropdown,
     TabBar,
     SyncCommitte,
+    TheAttestation,
+    BlockProduction,
   },
   data() {
     return {
+      optionsType: [
+        {
+          title: "ATTESTATION",
+          comp: TheAttestation,
+        },
+        {
+          title: "SYNC COMMITTEE",
+          comp: SyncCommitte,
+        },
+        {
+          title: "BLOCK PRODUCTION",
+          comp: BlockProduction,
+        },
+      ],
       tabs: [
         { id: 1, title: "ATTESTATION", imgPath: "/img/icon/the-staking/eye.png", display: false },
         { id: 2, title: "SYNC COMMITTEE", imgPath: "/img/icon/the-staking/comitte.png", display: false },
         { id: 3, title: "BLOCK PRODUCTION", imgPath: "/img/icon/the-staking/cube.png", display: false },
-        // { id: 4, title: "", imgPath: "", display: false },
-        // { id: 5, title: "", imgPath: "", display: false },
       ],
       selectedValidator: {},
-      dropDownIsOpen: false,
+
       maxCharacters: 30,
       withdrawalAddress: "0x12345gbfdbf097df9gb7s9dfg7b9sdfg7b67890",
+      currentComponent: "ATTESTATION",
     };
   },
   computed: {
@@ -108,22 +105,20 @@ export default {
       keys: "keys",
     }),
   },
+
+  mounted() {
+    this.getActiveComponent("ATTESTATION");
+  },
   methods: {
     toggleDropDown() {
       this.dropDownIsOpen = !this.dropDownIsOpen;
     },
+    getActiveComponent(item) {
+      this.currentComponent = item;
+    },
     chooseValidator(key) {
       this.selectedValidator = key;
       this.dropDownIsOpen = false;
-    },
-    getActiveComponent(item) {
-      this.tabs.forEach((tab) => {
-        if (tab.title === item) {
-          tab.display = true;
-        } else {
-          tab.display = false;
-        }
-      });
     },
   },
 };
@@ -169,124 +164,9 @@ export default {
   grid-template-columns: repeat(6, 1fr);
   grid-template-rows: repeat(10, 1fr);
 }
-.dropDown_parent {
-  grid-column: 1/7;
-  grid-row: 1/2;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 5px 0 5px;
-}
-.dropDown_parent .title {
-  width: 30%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.dropDown_parent .title span {
-  color: #fff;
-  font-size: 0.6rem;
-  font-weight: 700;
-}
-.dropDown_parent .dropDown_box {
-  width: 70%;
-  height: 100%;
-  padding: 2px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  z-index: 1;
-}
-.dropDown_box .dropDown {
-  width: 100%;
-  height: 100%;
-  border: 1px solid rgb(238, 238, 238);
-  border-radius: 2px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  cursor: pointer;
-}
-.dropDown_box .dropDown .options {
-  width: 90%;
-  height: 100%;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  padding: 2px;
-  color: #e7e7e7;
-  font-size: 0.7rem;
-  font-weight: 600;
-  text-align: left;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-}
-.dropDown_box .valueBox {
-  width: 94%;
-  height: 85%;
-  padding: 5px 2px;
-  max-height: 180px;
-  background-color: #1258a2;
-  border-radius: 4px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-  transition-duration: 300ms;
-  overflow-x: hidden;
-  overflow-y: auto;
-  position: absolute;
-  top: 14%;
-  left: 3%;
-}
 
-.valueBox::-webkit-scrollbar-thumb {
-  background: #efefef;
-  border-radius: 5px;
-}
 
-.options_value {
-  width: 100%;
-  height: 30px;
-  border-bottom: 1px solid #3b3f42;
-  padding: 2px 4px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-}
-.options_value:hover {
-  transition-duration: 300ms;
-  background-color: #e4e4e4;
-}
-.options_value:hover span {
-  color: #2d2d2d;
-}
 
-.options_value span {
-  width: 100%;
-  color: #cdcdcd;
-  font-size: 0.85rem;
-  font-weight: 500;
-  text-align: center;
-}
-.dropDown_box .dropDown .dropDown_icon {
-  width: 10%;
-  height: 100%;
-  background-color: #0a7ae2;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.dropDown_box .dropDown .dropDown_icon img {
-  width: 70%;
-  height: 50%;
-}
-.rotate {
-  transform: rotate(180deg) !important;
-}
 
 .indexParent {
   grid-column: 1/7;
@@ -420,7 +300,7 @@ export default {
   align-items: flex-start;
 }
 .componentParent .dynamicComponent {
-  width: 58%;
+  width: 70%;
   height: 100%;
   padding: 0 5px;
   display: flex;
@@ -428,17 +308,23 @@ export default {
   justify-content: space-between;
   align-items: flex-start;
 }
+.dynamicComponent .outterBox {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
 .componentParent .predicitionIcon {
-  width: 32%;
+  width: 30%;
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 10px 0;
+  padding: 5px 0;
 }
 .componentParent .predicitionIcon img {
   width: 100%;
-  height: 100%;
 }
 
 .blockBox .cubeIcon span {
