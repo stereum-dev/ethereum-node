22428a054eac5fe916a742481a8d6e4b20bee09c
mabasian <54101509+mabasian@users.noreply.github.com>
2023-06-05 13:33:10 +0200

    algorithm error handling changing

--- launcher/src/components/UI/the-control/ControlAlert.vue
+++ launcher/src/components/UI/the-control/ControlAlert.vue
@@ -190,13 +190,21 @@ export default {
   methods: {
     async readService() {
       const validators = this.installedServices.filter((i) => i.category === "validator");
+
       if (validators && validators.length > 0 && validators[0].config) {
         const addresses = [];
+
         for (const validator of validators) {
+          if (validator.name === "ssv.network" || validator.name === "Obol Charon") {
+            continue;
+          }
+
           if (!validator.yaml) validator.yaml = await ControlService.getServiceYAML(validator.config.serviceID);
-          const pattern =
-            validator.expertOptions[validator.expertOptions.findIndex((o) => o.title === "Default Fee Recipient")]
-              .pattern;
+          const patternIndex = validator.expertOptions.findIndex((o) => o.title === "Default Fee Recipient");
+          if (patternIndex === -1) {
+            continue;
+          }
+          const pattern = validator.expertOptions[patternIndex].pattern;
           const match = [...validator.yaml.match(new RegExp(pattern))][2];
           if (match) {
             const address = match;
@@ -212,9 +220,9 @@ export default {
           (validator) => validator.address === "0x0000000000000000000000000000000000000000"
         );
         this.notSetAddresses = notSetAddresses;
-      } else {
-        console.error("Invalid item or missing config property.");
-      }
+      } // else {
+      //   console.error("Invalid item or missing config property.");
+      // }
     },
     closeNotification() {
       this.notification = false;
