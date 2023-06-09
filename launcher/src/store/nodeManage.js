7bf865fd86b2b6d1345fadeea0c81a8abbcea1a1
mabasian <54101509+mabasian@users.noreply.github.com>
2023-06-09 14:39:00 +0200

    change the networks arange (#1269)

--- launcher/src/store/nodeManage.js
+++ launcher/src/store/nodeManage.js
@@ -172,7 +172,7 @@ export const useNodeManage = defineStore("nodeManage", {
           icon: "/img/icon/click-installation/mainnet-icon.png",
           currencyIcon: "/img/icon/control/mainnet-currency-symbol.png",
           dataEndpoint: "https://mainnet.beaconcha.in/api/v1",
-          support: ["staking", "mev boost"]
+          support: ["staking", "mev boost"],
         },
         {
           id: 2,
@@ -181,25 +181,26 @@ export const useNodeManage = defineStore("nodeManage", {
           icon: "/img/icon/click-installation/testnet-icon.png",
           currencyIcon: "/img/icon/control/goETH_Currency_Symbol.png",
           dataEndpoint: "https://goerli.beaconcha.in/api/v1",
-          support: ["staking", "ssv.network", "mev boost", "obol"]
+          support: ["staking", "ssv.network", "mev boost", "obol"],
         },
+
         {
           id: 3,
-          name: "Gnosis Mainnet",
-          network: "gnosis",
-          icon: "/img/icon/click-installation/gnosis_mainnet_icon.png",
-          currencyIcon: "/img/icon/control/gno_currency_symbol.png",
-          dataEndpoint: "https://beacon.gnosischain.com/api/v1",
-          support: ["staking"]
-        },
-        {
-          id: 4,
           name: "Sepolia Testnet",
           network: "sepolia",
           icon: "/img/icon/click-installation/testnet-icon.png",
           currencyIcon: "/img/icon/control/goETH_Currency_Symbol.png",
           dataEndpoint: "https://sepolia.beaconcha.in/api/v1",
-          support: ["staking", "mev boost"]
+          support: ["staking", "mev boost"],
+        },
+        {
+          id: 4,
+          name: "Gnosis Mainnet",
+          network: "gnosis",
+          icon: "/img/icon/click-installation/gnosis_mainnet_icon.png",
+          currencyIcon: "/img/icon/control/gno_currency_symbol.png",
+          dataEndpoint: "https://beacon.gnosischain.com/api/v1",
+          support: ["staking"],
         },
       ],
       currentNetwork: {},
