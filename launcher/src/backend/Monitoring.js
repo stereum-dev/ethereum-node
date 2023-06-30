1bfe473a9df3c780c75f4b2c111d26e41b8ddca3
NeoPlays <80448387+NeoPlays@users.noreply.github.com>
2023-06-30 14:00:28 +0200

    FIX: Lint Test Name (#1334)
    * FIX: Lint Test Name
    
    * FIX: no-changed-when

--- launcher/src/backend/Monitoring.js
+++ launcher/src/backend/Monitoring.js
@@ -42,7 +42,7 @@ export class Monitoring {
     this.globalMonitoringCache = { ...globalMonitoringCache };
     try {
       fs.unlinkSync(this.serviceInfosCacheFile);
-    } catch (e) {}
+    } catch (e) { }
   }
 
   // Jobs to handle on login
@@ -230,7 +230,7 @@ export class Monitoring {
         }
         //console.log('REQUIRE fresh cache ' + hash, dnow);
       }
-    } catch (e) {}
+    } catch (e) { }
     if (await this.checkStereumInstallation()) {
       var serviceConfigs = await this.serviceManagerProm.readServiceConfigurations();
       const serviceStates = await this.nodeConnectionProm.listServices();
@@ -408,11 +408,11 @@ export class Monitoring {
     var query =
       rpc_method.trim().indexOf("{") < 0
         ? JSON.stringify({
-            jsonrpc: "2.0",
-            method: rpc_method.trim(),
-            params: rpc_params,
-            id: 1,
-          })
+          jsonrpc: "2.0",
+          method: rpc_method.trim(),
+          params: rpc_params,
+          id: 1,
+        })
         : rpc_method;
 
     // Define default response
@@ -904,7 +904,6 @@ export class Monitoring {
           data: "",
         };
       }
-      const apikey = await this.validatorAccountManager.getApiToken(client);
       const resultKeymanagerAPI = await this.validatorAccountManager.keymanagerAPI(client, "GET", "/eth/v1/keystores");
       const result = JSON.parse(resultKeymanagerAPI.stdout);
       if (!result.data) result.data = [];
@@ -1219,12 +1218,12 @@ export class Monitoring {
             labels.forEach(function (label, index) {
               try {
                 results[label] = xx.filter((s) => s.metric.__name__ == labels[index])[0].value[1];
-              } catch (e) {}
+              } catch (e) { }
             });
             try {
               frstVal = results[labels[1]];
               scndVal = results[labels[0]];
-            } catch (e) {}
+            } catch (e) { }
           }
           // Set chain head block for this client from RPC server (if available)
           if (
@@ -1241,7 +1240,7 @@ export class Monitoring {
                 typeof chain_head_block === "string" && chain_head_block.startsWith("0x")
                   ? parseInt(chain_head_block, 16)
                   : 0;
-            } catch (e) {}
+            } catch (e) { }
             let stay_on_hold_till_first_block = false; // true = enabled | false = disabled
             if (stay_on_hold_till_first_block && !chain_head_block) {
               // stay on hold until EC has responded the first block by RPC
@@ -1544,7 +1543,7 @@ export class Monitoring {
                     .value.pop()
                 );
                 details[clientType]["numPeerBy"]["fields"].push(item);
-              } catch (e) {}
+              } catch (e) { }
             });
           }
 
@@ -1915,7 +1914,7 @@ export class Monitoring {
         await this.nodeConnection.closeTunnels(openTunnels);
         this.rpcTunnel = {};
       }
-    } catch (e) {}
+    } catch (e) { }
 
     // Respond success with fresh RPC status data
     const freshrpcstatus = await this.getRpcStatus();
@@ -2098,7 +2097,7 @@ export class Monitoring {
         await this.nodeConnection.closeTunnels(openTunnels);
         this.wsTunnel = {};
       }
-    } catch (e) {}
+    } catch (e) { }
 
     // Respond success with fresh WS status data
     const freshwsstatus = await this.getWsStatus();
@@ -2247,7 +2246,7 @@ export class Monitoring {
     try {
       let r = wsResult.stdout.trim().split("\n");
       statuscode = r.length > 0 ? parseInt(r.pop()) : statuscode;
-    } catch (e) {}
+    } catch (e) { }
 
     // Respond true if websocket is available, false otherwise
     if (!wsResult.stdout.toLowerCase().includes("sec-websocket") && statuscode != 200) {
@@ -2354,7 +2353,7 @@ export class Monitoring {
         await this.nodeConnection.closeTunnels(openTunnels);
         this.beaconTunnel = {};
       }
-    } catch (e) {}
+    } catch (e) { }
 
     // Respond success with fresh BEACON status data
     const freshbeaconstatus = await this.getBeaconStatus();
@@ -2479,8 +2478,8 @@ export class Monitoring {
     const addr_type = Array.isArray(addr)
       ? "arr"
       : typeof addr === "string" && ["public", "local"].includes(addr)
-      ? "str"
-      : "invalid";
+        ? "str"
+        : "invalid";
     addr = addr_type == "str" ? addr.toLowerCase().trim() : addr;
     if (addr_type == "invalid") {
       return {
@@ -2568,7 +2567,7 @@ export class Monitoring {
     for (let i = 0; i < serviceInfos.length; i++) {
       const hashDependencies =
         serviceInfos[i].config.dependencies.consensusClients.length ||
-        serviceInfos[i].config.dependencies.executionClients.length
+          serviceInfos[i].config.dependencies.executionClients.length
           ? "yes"
           : "no";
       easyInfos.push({
