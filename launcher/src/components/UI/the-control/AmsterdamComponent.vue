6c137bda0b519506b0bf27daa56a4c2ed484d1d3
mabasian <54101509+mabasian@users.noreply.github.com>
2023-08-05 09:34:51 +0200

    Tiny css (#1385)

--- launcher/src/components/UI/the-control/AmsterdamComponent.vue
+++ launcher/src/components/UI/the-control/AmsterdamComponent.vue
@@ -324,7 +324,7 @@ export default {
 .square {
   width: 23%;
   height: 40%;
-  margin: 0 0.5%;
+  margin: 0 2.5%;
 
   background: #94deff;
 }
@@ -357,21 +357,21 @@ export default {
 @keyframes square-anim {
   0% {
     height: 40%;
-    background-color: rgb(111, 163, 240);
+    background-color: #336666;
   }
   20% {
     height: 40%;
   }
   40% {
     height: 80%;
-    background-color: rgb(111, 200, 240);
+    background-color: #478e8e;
   }
   80% {
     height: 40%;
   }
   100% {
     height: 40%;
-    background-color: rgb(111, 163, 240);
+    background-color: #336666;
   }
 }
 /*end of the test*/
