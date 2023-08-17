bf93c9d3196e6a585c6d5968ec49a376afe5a304
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2023-08-17 15:33:57 +0200

    ADD: service clients to node page

--- launcher/src/components/UI/node-page/components/service/ServiceBody.vue
+++ launcher/src/components/UI/node-page/components/service/ServiceBody.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="scrollbar scrollbar-rounded-* scrollbar-thumb-teal-800 scrollbar-track-transparent w-full h-full max-h-[430px] rounded-md border border-gray-500 overflow-y-auto bg-[#151618] relative p-2"
+    class="scrollbar scrollbar-rounded-* scrollbar-thumb-teal-800 scrollbar-track-transparent w-full h-full max-h-[425px] rounded-md border border-gray-500 overflow-y-auto bg-[#151618] relative pt-1"
   >
-    <div ref="service" class="col-start-1 col-span-1 space-y-2 py-2">
-      <div v-for="item in getServices" :key="item" class="max-h-[85px] grid grid-cols-3 py-2">
+    <div ref="service" class="col-start-1 col-span-1 space-y-2 px-1 pt-2 mt-1">
+      <div v-for="item in getServices" :key="item" class="max-h-[80px] grid grid-cols-3 pt-2">
         <ServiceLayout :client="item" />
         <ServiceButtons :client="item" @open-expert="openExpertWindow" @open-log="openLogsPage" />
         <ExpertWindow
