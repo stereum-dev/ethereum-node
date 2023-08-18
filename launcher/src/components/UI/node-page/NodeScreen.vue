b928d9a2458d4ab1975e79d49be7bb82f56b1c21
Erfan Rahimizadeh <35860676+erfan7r6a@users.noreply.github.com>
2023-08-18 07:36:18 -0400

    ADD: Alerting and tutorial node page

--- launcher/src/components/UI/node-page/NodeScreen.vue
+++ launcher/src/components/UI/node-page/NodeScreen.vue
@@ -11,28 +11,73 @@
       <div class="col-start-18 col-end-22">
         <ServiceSection />
       </div>
-      <div class="col-start-22 col-end-25 bg-violet-400">alerting</div>
+      <div class="col-start-22 col-end-25">
+        <NodeAlert v-if="infoAlarm"/>
+        <div
+          class="info-button"
+          @click="infoAlarm = !infoAlarm"
+          @mouseenter="cursorLocation = infoAlarm ? `${chckTutorial}` : `${returnStatus}`"
+          @mouseleave="cursorLocation = ''"
+        >
+          <img src="/img/icon/round-icon.png" alt="information" />
+        </div>
+        <NodeTutorial v-if="!infoAlarm" @show-modal="openTutorialModalHandler" />
+      </div>
     </div>
     <!-- End Node main layout -->
   </base-layout>
 </template>
 <script>
+import NodeAlert from "../the-node/NodeAlert.vue";
 import NodeSidebar from "./sections/NodeSidebar.vue";
 import NodeSection from "./sections/NodeSection.vue";
 import ServiceSection from "./sections/ServiceSection.vue";
+import NodeTutorial from "../the-node/NodeTutorial.vue";
+// import NodeAlerting from "./sections/NodeAlerting.vue";
 export default {
   name: "NodeScreen",
   components: {
     NodeSidebar,
     NodeSection,
     ServiceSection,
+    NodeAlert,
+    NodeTutorial,
   },
   props: {},
   data() {
-    return {};
+    return {
+      infoAlarm: true,
+      cursorLocation: "",
+      chckTutorial: "/img/icon/round-icon.png",
+      returnStatus: "/img/icon/round-icon.png",
+    };
   },
   computed: {},
   mounted() {},
   methods: {},
 };
 </script>
+<style scoped>
+.info-button {
+  width: 98%;
+  height: 7%;
+  background: #264744;
+  border-radius: 20px;
+  box-shadow: 0 1px 3px 0px #1c1f22;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  margin-top: 3px;
+  margin-bottom: 15px;
+}
+.info-button:hover {
+  background: rgb(43, 84, 81);
+}
+.info-button:active {
+  background: rgba(43, 84, 81, 0.5);
+}
+.info-button img {
+  max-width: 19%;
+}
+</style>
