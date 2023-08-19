d08887992191fe1db6685496130a8e967a49a644
mabasian <54101509+mabasian@users.noreply.github.com>
2023-08-19 11:40:52 +0200

    add server access managment comp.

---
+++ launcher/src/components/UI/node-header/SecurityButton.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="securBtn-parent">
+    <div class="securBtn-box" @click="test">
+      <img :src="stereumStatus ? '/img/icon/LOGO.png' : '/img/icon/statusOff.png'" class="main-header__brand" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "pinia";
+import { useFooter } from "@/store/theFooter";
+export default {
+  data() {
+    return {
+      test: false,
+    };
+  },
+  computed: {
+    ...mapState(useFooter, {
+      stereumStatus: "stereumStatus",
+    }),
+  },
+  methods: {
+    testM() {
+      this.test = !this.test;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.securBtn-parent {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 8%;
+  height: 15%;
+  z-index: 50;
+  cursor: pointer;
+}
+.securBtn-box {
+  width: 100%;
+  z-index: 51;
+}
+.securBtn-box img {
+  width: 100%;
+  z-index: 51;
+}
+.securBtn-box img:active {
+  transform: scale(0.98);
+}
+.test-page {
+  width: 100vw;
+  height: 100vh;
+  background-color: #472630;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 49;
+  opacity: 0.5;
+  display: none;
+}
+</style>
---
+++ launcher/src/components/UI/node-header/ServerAccessManagement.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dashboard-parent"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.dashboard-parent {
+  width: 100vw;
+  height: 95vh;
+  background-color: #472630;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 49;
+  opacity: 0.5;
+  border-radius: 10px;
+  border: 5px solid #c1c1c1;
+}
+</style>
