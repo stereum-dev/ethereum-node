e59fc2652103c94a9e2e76e44345c1112eb7be80
mabasian <54101509+mabasian@users.noreply.github.com>
2023-08-21 12:40:59 +0200

    update the server access management dash
    the main dashboard is done

--- launcher/src/components/UI/node-header/ServerAccessManagement.vue
+++ launcher/src/components/UI/node-header/ServerAccessManagement.vue
@@ -23,6 +23,45 @@
         <div class="management-info_box_row" />
       </div>
     </div>
+    <div class="login-info-box">
+      <div class="login-info-box_info-part">
+        <!-- <div class="helf-part">
+          <div class="half-part_row">
+            <span>LOGIN TYPE</span><span>{{ !selectedConnection.useAuthKey ? "SSH KEY" : "PASSWORD and SSH" }}</span>
+          </div>
+        </div> -->
+        <div class="helf-part left">
+          <div class="half-part_row">
+            <span>PASSWORD</span>
+            <div class="btn-part"><div class="login-info-btn">CHANGE PASSWORD</div></div>
+          </div>
+          <div class="half-part_row">
+            <span>SSH Key</span>
+            <div class="btn-part">
+              <div class="login-info-btn">create a new key</div>
+              <div class="login-info-btn">add an existing key</div>
+            </div>
+          </div>
+        </div>
+        <!-- <div class="helf-part left">
+          <div class="half-part_row">
+            <span>CONVERT TO SSH</span>
+            <div class="btn-part">
+              <div class="login-info-btn">create a new key</div>
+              <div class="login-info-btn">add an existing key</div>
+            </div>
+          </div>
+        </div> -->
+      </div>
+      <div class="key-info-part">
+        <div v-for="(key, index) in keys" :key="index" class="key-row">
+          <div class="name">{{ key }}</div>
+          <div class="btn-box">
+            <KeyRowBtn @delete-key="confirmDelete(key)" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,10 +69,13 @@
 import { mapState } from "pinia";
 import { useControlStore } from "@/store/theControl";
 import ControlService from "@/store/ControlService";
+import KeyRowBtn from "./KeyRowBtn.vue";
 export default {
+  components: { KeyRowBtn },
   data() {
     return {
       selectedConnection: {},
+      keys: ["key 1", "key 2", "key 3"],
     };
   },
   computed: {
@@ -41,6 +83,9 @@ export default {
       ServerName: "ServerName",
       ipAddress: "ipAddress",
     }),
+    passSSHRow() {
+      return !this.selectedConnection.useAuthKey ? "pass" : "ssh";
+    },
   },
   mounted() {
     this.loadStoredConnections();
@@ -50,6 +95,10 @@ export default {
       const savedConnections = await ControlService.readConfig();
       let selectedConnection = savedConnections.savedConnections.find((item) => item.host === this.ipAddress);
       this.selectedConnection = selectedConnection;
+      console.log(this.selectedConnection);
+    },
+    confirmDelete(key) {
+      console.log(key);
     },
   },
 };
@@ -86,7 +135,7 @@ export default {
 }
 .management-info {
   width: 95%;
-  height: 30%;
+  height: 25%;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -106,7 +155,6 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
-
   padding: 0 2%;
   color: #eee;
 }
@@ -115,5 +163,117 @@ export default {
 }
 .management-info_box_row > span:nth-child(1) {
   font-size: 150%;
+}
+.login-info-box {
+  width: 95%;
+  height: 58%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+}
+.login-info-box_info-part {
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  height: 25%;
+}
+
+.helf-part {
+  width: 50%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+}
+
+.half-part_row {
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 2%;
+  color: #eee;
+}
+.pass {
+  height: 50%;
+}
+.ssh {
+  height: 100%;
+}
+.half-part_row span {
+  width: 50%;
+}
+.half-part_row > span:nth-child(1) {
+  font-size: 150%;
+}
+
+.btn-part {
+  width: 50%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.login-info-btn {
+  width: 48.5%;
+  height: 60%;
+  color: #eee;
+  font-size: 60%;
+  font-weight: 600;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  border-radius: 15px;
+  background: #35a83580;
+  text-transform: uppercase;
+  box-shadow: 1px 1px 5px 1px rgb(45, 44, 44);
+}
+.login-info-btn:hover {
+  background: #35a835;
+}
+.login-info-btn:active {
+  background: #35a83580;
+  border: none;
+  box-shadow: none;
+  transform: scale(0.98);
+}
+.key-info-part {
+  width: 100%;
+  height: 75%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  background: #23272a;
+  border: 1px solid grey;
+  box-shadow: 1px 1px 6px 1px #001717;
+  border-radius: 10px;
+}
+.key-row {
+  width: 95%;
+  height: 12%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 2%;
+  color: #eee;
+  background: #383838;
+  margin-top: 1%;
+  border-radius: 5px;
+  border: 1px solid #39393d;
+  box-shadow: 1px 1px 6px 1px #1d2525;
+}
+.btn-box {
+  width: 7%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
 }
 </style>
