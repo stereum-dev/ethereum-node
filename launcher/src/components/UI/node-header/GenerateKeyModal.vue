22083314b70adbd45f6bf1ccb9c28fa58a67c9cb
mabasian <54101509+mabasian@users.noreply.github.com>
2023-08-23 10:23:43 +0200

    fix the generate key modal
    frontend is done

--- launcher/src/components/UI/node-header/GenerateKeyModal.vue
+++ launcher/src/components/UI/node-header/GenerateKeyModal.vue
@@ -5,8 +5,13 @@
       <div class="generate-key-modal_container">
         <div class="generate-key-modal_rows">
           <label for="keyType">key type</label>
-          <div class="key-type-selection">
-            <span>{{ pickPath }}</span>
+          <div class="key-type-selection" @click="keyTypeDropdown = !keyTypeDropdown">
+            <span>{{ keyType }}</span>
+          </div>
+          <div v-if="keyTypeDropdown" class="dropdown">
+            <div v-for="key in keyTypeCollection" :key="key" class="dropdown-row" @click="keyPicker(key)">
+              <span>{{ key }}</span>
+            </div>
           </div>
         </div>
         <div class="generate-key-modal_rows">
@@ -16,24 +21,23 @@
           <label for="sshPass">ssh password</label
           ><input type="password" placeholder="DEFINE AN OPTIONAL SSH PASSWORD" />
         </div>
-        <div class="generate-key-modal_rows"><label for="unlockExpert">unlock expert options</label></div>
         <div class="generate-key-modal_rows">
-          <label for="bitAmount">bit amount</label><input type="text" placeholder="SPECIFY SSH BIT AMOUNT" />
+          <label for="unlockExpert">unlock expert options</label>
+          <div class="toggle" :style="{ backgroundColor: expert ? '#0BB910' : '#808080' }" @click="expertUnlock">
+            <div class="toggle-circle" :class="{ animate: expert }" />
+          </div>
+        </div>
+        <div class="generate-key-modal_rows">
+          <label for="bitAmount">bit amount</label
+          ><input type="text" placeholder="SPECIFY SSH BIT AMOUNT" :disabled="!expert" />
         </div>
         <div class="generate-key-modal_rows">
           <label for="specifyCypher">specify cypher</label>
-          <input
-            ref="input4"
-            v-model="pickPath"
-            type="text"
-            class="test"
-            placeholder="USE A CUSTOM CYPER"
-            @click="console.log('test', $refs.input4)"
-            @focus="handleFocus"
-          />
+          <input type="text" placeholder="USE A CUSTOM CYPER" :disabled="!expert" />
         </div>
       </div>
-      <button>click</button>
+      <div class="generate-btn">generate</div>
+      <span class="click-close">click out site to close</span>
     </div>
     <div class="bg-modal" @click="generateModalShow = false" />
   </div>
@@ -45,7 +49,10 @@ import { useControlStore } from "@/store/theControl";
 export default {
   data() {
     return {
-      pickPath: "",
+      keyType: "RSA",
+      keyTypeDropdown: false,
+      keyTypeCollection: ["RSA", "EDCSA", "ED25519"],
+      expert: false,
     };
   },
   computed: {
@@ -56,6 +63,13 @@ export default {
   methods: {
     handleFocus() {
       console.log("Input focused!");
+    },
+    expertUnlock() {
+      this.expert = !this.expert;
+    },
+    keyPicker(key) {
+      this.keyType = key;
+      this.keyTypeDropdown = false;
     },
   },
 };
@@ -119,7 +133,6 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  border: 1px dotted yellow;
 }
 .generate-key-modal_rows label {
   color: #eee;
@@ -151,6 +164,83 @@ export default {
   color: #eee;
   font-size: 100%;
   font-weight: 600;
+  text-transform: uppercase;
+}
+.dropdown {
+  width: 55%;
+  background: #1258a2;
+  color: #d5d5d5;
+  height: 33%;
+  position: absolute;
+  top: 26%;
+  left: 38%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.dropdown-row {
+  width: 100%;
+  height: 33%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding-left: 2%;
+  cursor: pointer;
+}
+.dropdown-row:hover {
+  background: #d5d5d5;
+  color: #1258a2;
+}
+.toggle {
+  width: 10%;
+  height: 60%;
+
+  border-radius: 20px;
+  box-shadow: inset 0 2px 5px rgba(0, 0, 0, 0.6);
+  display: flex;
+  align-items: center;
+  padding: 0 0.8%;
+}
+.toggle-circle {
+  width: 50%;
+  height: 85%;
+  background: #eee;
+  border-radius: 50%;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.6);
+  transition: transform 0.3s ease;
+}
+.animate {
+  transform: translateX(95%);
+}
+.generate-btn {
+  width: 30%;
+  height: 10%;
+  background: #0bb910;
+  color: #d5d5d5;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  font-size: 100%;
+  font-weight: 600;
+  text-transform: uppercase;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.6);
+}
+.generate-btn:hover {
+  background: rgba(53, 168, 53, 0.5);
+}
+.generate-btn:active {
+  background: rgba(53, 168, 53, 0.8);
+  box-shadow: none;
+  transform: scale(0.98);
+}
+.click-close {
+  color: red;
+  justify-self: flex-end;
+  margin-top: 3.2%;
+  font-size: 70%;
   text-transform: uppercase;
 }
 </style>
