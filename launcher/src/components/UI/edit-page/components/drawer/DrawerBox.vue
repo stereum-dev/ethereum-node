b8751e8a4cc4f6c04c539536f878874cf51d5139
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2023-08-25 15:44:16 +0200

    ADD: edit drawer

--- launcher/src/components/UI/edit-page/components/drawer/DrawerBox.vue
+++ launcher/src/components/UI/edit-page/components/drawer/DrawerBox.vue
@@ -1,6 +1,37 @@
+import { useServices } from '@/store/services';
 <template>
-  <aside
-    class="flex flex-col bg-gradient-to-b from-10% from-[#264744] to-[#325d5a] w-full h-full px-4 py-8 overflow-y-auto border-r rtl:border-r-0 rtl:border-l border-gray-700"
-  ></aside>
+  <div
+    class="flex flex-col w-full h-full px-2 py-4 overflow-y-auto overflow-x-hidden border-l border-gray-500 rounded-l-xl bg-[#2e5151]"
+  >
+    <div
+      class="w-full h-full flex flex-col items-center space-y-6 bg-[#151618] border border-gray-500 rounded-md overflow-y-auto relative py-3"
+    >
+      <transition-group>
+        <div v-for="service in store.allServices" :key="service.serviceID" ref="el" class="w-full relative">
+          <img
+            :src="service.sIcon"
+            alt="Client Icon"
+            class="w-14 cursor-grab mx-auto"
+            @mouseenter="service.displayTooltip = true"
+            @mouseleave="service.displayTooltip = false"
+          />
+          <div
+            v-if="service.displayTooltip"
+            class="absolute -top-[20px] left-1 h-5 bg-slate-100 border border-yellow-600 text-xs font-semibold rounded-sm flex justify-center items-center px-1 justify-self-center truncate overflow-x-clip"
+          >
+            <span>{{ service.name }}</span>
+          </div>
+        </div>
+      </transition-group>
+    </div>
+  </div>
 </template>
 
+<script setup>
+import { useServices } from "@/store/services";
+
+import { ref } from "vue";
+
+const store = useServices();
+const el = ref(null);
+</script>
--- launcher/src/components/UI/edit-page/sections/DrawerSection.vue
+++ launcher/src/components/UI/edit-page/sections/DrawerSection.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="w-40 h-full flex flex-col justify-between items-center">
+  <div
+    class="w-28  h-full flex flex-col justify-between items-center fixed right-0 top-0"
+    @mouseleave="store.isDrawerOpen = false"
+  >
     <DrawerBox />
   </div>
 </template>
 <script setup>
 import DrawerBox from "../components/drawer/DrawerBox";
-import { ref } from "vue";
+import { useNodeManage } from "@/store/nodeManage";
 
+const store = useNodeManage(); 
 
-const isOpen = ref(false);
-
-const toggle = () => {
-  isOpen.value = !isOpen.value;
-};
 </script>
