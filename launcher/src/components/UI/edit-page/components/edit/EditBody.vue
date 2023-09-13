5826ea368bc4d978d65ef934ad2f5f9655333b6c
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2023-09-13 14:25:05 +0200

    ADD: drop zone

--- launcher/src/components/UI/edit-page/components/edit/EditBody.vue
+++ launcher/src/components/UI/edit-page/components/edit/EditBody.vue
@@ -2,56 +2,137 @@
   <div class="w-full h-full flex flex-col justify-between items-center">
     <EditHeader />
     <div
-      class="w-full h-full max-h-[430px] rounded-md border overflow-hidden border-gray-500 mt-1 bg-[#151618] relative"
+      class="w-full h-full max-h-[430px] rounded-md border overflow-hidden mt-1 bg-[#151618] relative"
+      :class="isOverDropZone ? 'border-dashed border-2 border-blue-500 ' : 'border-gray-500'"
     >
       <div
-        id="clientTitle"
-        class="absolute top-0 w-full mx-auto flex justify-evenly items-center h-6 bg-[#264744] border border-gray-950 rounded-t-sm text-gray-200 text-sm"
+        class="absolute top-0 w-full mx-auto grid grid-cols-3 h-6 bg-[#4f585f] border border-gray-950 rounded-t-[5px] text-gray-200 text-sm"
       >
-        <span>Validator </span>
-        <span>Consensus Clients</span>
-        <span>Execution Clients</span>
+        <span class="col-start-1 justify-self-center">Validator </span>
+        <span class="col-start-2 justify-self-center">Consensus Clients</span>
+        <span class="col-start-3 justify-self-center">Execution Clients</span>
       </div>
       <div
-        class="w-full h-full grid grid-cols-3 pb-2 pt-7 scrollbar scrollbar-rounded-* scrollbar-thumb-teal-800 scrollbar-track-transparent overflow-y-auto"
+        ref="dropZoneRef"
+        class="w-full h-full max-h-[428px] grid grid-cols-3 pt-5"
+        :class="{
+          'scrollbar scrollbar-rounded-* scrollbar-thumb-teal-800 scrollbar-track-transparent overflow-y-auto':
+            scrollBox,
+        }"
+        @drop="onDrop($event)"
+        @dragover.prevent="isOverDropZone = true"
+        @dragleave.prevent="isOverDropZone = false"
       >
-        <ValidatorClients
-          @open-expert="openExpertWindow"
-          @open-log="openLogsPage"
-          @hide-modal="clickOutside"
-          @state-handler="stateHandler"
-          @restart-handler="restartService"
-        />
-
-        <ConsensusClients
-          @open-expert="openExpertWindow"
-          @open-log="openLogsPage"
-          @hide-modal="clickOutside"
-          @state-handler="stateHandler"
-          @restart-handler="restartService"
-        />
-        <ExecutionClients
-          @open-expert="openExpertWindow"
-          @open-log="openLogsPage"
-          @hide-modal="clickOutside"
-          @state-handler="stateHandler"
-          @restart-handler="restartService"
-        />
-
-        <slot name="drop"></slot>
+        <span
+          v-if="isOverDropZone"
+          class="col-start-2 col-span-1 self-center justify-self-center flex justify-center items-center text-xl text-blue-400"
+          >+</span
+        >
+        <ValidatorClients v-if="!isOverDropZone" @delete-service="removeInstalledService" />
+        <ConsensusClients v-if="!isOverDropZone" @delete-service="removeInstalledService" />
+        <ExecutionClients v-if="!isOverDropZone" @delete-service="removeInstalledService" />
       </div>
     </div>
   </div>
 </template>
+
 <script setup>
 import EditHeader from "./EditHeader.vue";
 import ExecutionClients from "./ExecutionClients.vue";
 import ConsensusClients from "./ConsensusClients.vue";
 import ValidatorClients from "./ValidatorClients.vue";
+import { computed, watch, ref } from "vue";
+import { useNodeManage } from "@/store/nodeManage";
+import { useServices } from "@/store/services";
+
+const emit = defineEmits(["onDrop"]);
+
+//Pinia stores
+const manageStore = useNodeManage();
+const serviceStore = useServices();
+
+// refs
+const isOverDropZone = ref(false);
+const scrollBox = ref(false);
+
+// computed & watchers properties
+const displayDropZone = computed(() => {
+  let dropClass;
+  if (isOverDropZone.value) {
+    dropClass = "border-dashed border-4 border-gray-500 z-50";
+  } else {
+    dropClass = "border-0";
+  }
+  return dropClass;
+});
+
+watch(serviceStore.installedServices, () => {
+  const validators = serviceStore.installedServices.filter((service) => service.category === "validator");
+
+  const consensus = serviceStore.installedServices.filter((service) => service.category === "consensus");
+
+  const execution = serviceStore.installedServices.filter((service) => service.category === "execution");
+
+  if (validators.length > 3 || consensus.length > 3 || execution.length > 3) {
+    scrollBox.value = true;
+  } else {
+    scrollBox.value = false;
+  }
+});
+
+// methods
+
+const onDrop = (event) => {
+  isOverDropZone.value = false;
+  emit("onDrop", event);
+};
+
+const removeInstalledService = (item) => {
+  console.log(item);
+  manageStore.confirmChanges.push({
+    content: "DELETE",
+    contentIcon: "/img/icon/manage-node-icons/trash.png",
+    service: manageStore.currentNetwork,
+  });
+};
+
+function openExpertWindow(client) {
+  // Implement your logic to open the expert window here
+}
+
+function openLogsPage(client) {
+  // Implement your logic to open the logs page here
+}
+
+function clickOutside(client) {
+  // Implement your logic for clicking outside the client here
+}
+
+function stateHandler(client) {
+  // Implement your state handling logic here
+}
+
+function restartService(client) {
+  // Implement your restart service logic here
+}
 </script>
 
 <style scoped>
 ::-webkit-scrollbar {
   width: 3px;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.slide-enter {
+  transform: scaleY(0);
+  transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-leave-to {
+  transform: scaleY(0);
+  transition: opacity 0.5s ease-in-out;
 }
 </style>
