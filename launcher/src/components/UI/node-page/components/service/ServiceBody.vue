86497d1f8f1f26fa975b0cdaf9f10e4752703987
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2023-09-13 14:24:34 +0200

    ADD: box title

--- launcher/src/components/UI/node-page/components/service/ServiceBody.vue
+++ launcher/src/components/UI/node-page/components/service/ServiceBody.vue
@@ -1,12 +1,20 @@
 <template>
   <div
-    class="scrollbar scrollbar-rounded-* hover:scrollbar-thumb-teal-800 scrollbar-track-transparent w-full h-full max-h-[430px] rounded-md border border-gray-500 overflow-y-auto bg-[#151618] relative pt-1 pl-1 hover:scroll-auto"
+    class="w-full h-[430px] rounded-md border border-gray-500 bg-[#151618] relative hover:scroll-auto overflow-y-auto"
   >
-    <div ref="service" class="flex flex-col space-y-4 items-center pt-2">
+    <div
+      class="absolute inset-x-0 w-full mx-auto flex justify-center items-center h-6 bg-[#4f585f] border border-gray-950 rounded-t-[5px] text-gray-200 text-sm z-10"
+    >
+      <span>Services </span>
+    </div>
+    <div
+      ref="service"
+      class="h-full flex flex-col space-y-4 items-center pt-2 overflow-y-auto scrollbar scrollbar-rounded-* hover:scrollbar-thumb-teal-800 scrollbar-track-transparent"
+    >
       <div
         v-for="item in getServices"
         :key="item"
-        class="max-h-[80px] max-w-[160px] grid grid-cols-2 py-2 rounded-md border border-gray-700 bg-[#212629] shadow-md pr-1"
+        class="max-h-[70px] max-w-[160px] grid grid-cols-2 py-2 rounded-md border border-gray-700 bg-[#212629] shadow-md mx-auto mt-8"
       >
         <ServiceLayout :client="item" />
         <ServiceButtons :client="item" />
