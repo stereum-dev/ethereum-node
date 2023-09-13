424ceee1cb1067ec477a12c16b4e559faa98b252
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2023-09-13 14:23:52 +0200

    ADD: connected mevboost

--- launcher/src/components/UI/edit-page/components/edit/ClientLayout.vue
+++ launcher/src/components/UI/edit-page/components/edit/ClientLayout.vue
@@ -2,31 +2,49 @@ import { computed } from 'vue';
 <template>
   <div
     class="grid-col-1 col-span-1 relative w-full h-full flex justify-center items-center box-border"
+    :class="{ 'opacity-25': client.displayPluginMenu }"
     @pointerdown.prevent.stop
     @mousedown.prevent.stop
   >
     <div class="flex flex-col justify-center items-center gap-2">
       <span class="text-xs text-gray-200 font-semibold">{{ client.name }}</span>
       <img class="w-14" :src="client.sIcon" alt="icon" />
-      <p class="text-[10px] text-gray-200">
-        ID:<span class="text-[10px] text-yellow-200">{{ serviceId }}</span>
+      <p class="text-[10px] text-gray-400">
+        ID:<span class="text-[12px] text-gray-200 ml-1">{{ serviceId }}</span>
       </p>
+    </div>
+
+    <div
+      v-if="getConnectedMevboost.config.serviceID === client.config.serviceID"
+      class="h-1/3 flex justify-evenly items-center absolute -start-2 -top-5"
+    >
+      <img class="w-6" src="/img/icon/plugin-icons/Other/mev-sIcon.png" alt="icon" />
     </div>
   </div>
 </template>
 
 <script setup>
 import { computed } from "vue";
+import { useServices } from "@/store/services";
 const { client } = defineProps({
   client: {
     type: Object,
     required: true,
   },
 });
+const serviceStore = useServices();
 
 const serviceId = computed(() => formattedServiceID(client.config.serviceID));
 
 const formattedServiceID = (item) => {
   return item.slice(0, 6) + "..." + item.slice(-6);
 };
+
+const getConnectedMevboost = computed(() => {
+  let connectedMevboost;
+  connectedMevboost = serviceStore.installedServices
+    .filter((e) => e.category == "consensus")
+    .find((e) => e.config.dependencies.mevboost[0]);
+  return connectedMevboost;
+});
 </script>
