93b9cccbf4be0193495729653c10f249a024814f
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2023-09-22 02:55:12 +0200

    FIX: emit

--- launcher/src/components/UI/node-page/components/service/ServiceButtons.vue
+++ launcher/src/components/UI/node-page/components/service/ServiceButtons.vue
@@ -33,7 +33,7 @@
     </button>
     <button
       class="col-span-1 p-1 transition-colors duration-200 bg-gray-900 hover:bg-gray-600 rounded-md"
-      @click="$emit('openExpertMode', props.client)"
+      @click="expertMode(props.client)"
     >
       <img src="/img/icon/plugin-menu-icons/setting2.png" alt="icon" class="w-8 active:scale-95" />
     </button>
@@ -58,4 +58,10 @@ const props = defineProps({
     required: true,
   },
 });
+
+const emit = defineEmits(["expertMode"]);
+
+const expertMode = (item) => {
+  emit("expertMode", item);
+};
 </script>
