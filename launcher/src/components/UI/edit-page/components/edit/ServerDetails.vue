1b7f03adf6e8af4fd1adbaf183aac7583874c4e6
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2023-10-03 17:26:45 +0200

    REFACTOR: server details

--- launcher/src/components/UI/edit-page/components/edit/ServerDetails.vue
+++ launcher/src/components/UI/edit-page/components/edit/ServerDetails.vue
@@ -2,12 +2,15 @@
   <div
     class="col-start-1 col-span-3 flex flex-col justify-between items-center border border-gray-600 rounded-md bg-[#151618]"
   >
-    <div class="w-full flex justify-start items-center px-2 h-[25px]">
-      <span class="text-xs text-gray-100 overflow-hidden whitespace-pre ml-5">Server :</span>
-      <span class="text-sm ml-1 text-yellow-500 overflow-hidden whitespace-pre">{{ props.serverName }}</span>
+    <div class="w-full flex justify-center items-center px-2 h-[25px]">
+      <span class="text-md font-semibold ml-1 text-yellow-500 overflow-hidden whitespace-pre text-center">{{
+        props.serverName
+      }}</span>
     </div>
-    <div class="w-full flex justify-start items-center px-2 h-[25px]">
-      <span class="text-xs text-left text-gray-100 overflow-hidden whitespace-pre ml-[5px]">Server IP :</span>
+    <div class="w-full flex justify-center items-center px-2 h-[25px]">
+      <span v-if="props.serverIp" class="text-xs text-left text-gray-100 overflow-hidden whitespace-pre ml-[5px]"
+        >IP :</span
+      >
       <span class="text-sm pl-2 text-yellow-500 overflow-hidden whitespace-pre">{{ props.serverIp }}</span>
     </div>
   </div>
