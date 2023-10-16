8ca1153bb587bbf091ed657bda29bf0d7981d846
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2023-10-16 17:32:16 +0200

    ADD: footer with tooltips & status

--- launcher/src/components/base/BaseLayout.vue
+++ launcher/src/components/base/BaseLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col justify-between box-border items-center w-screen h-screen border-2 border-slate-500 rounded-lg z-50"
+    class="flex flex-col justify-between box-border items-center w-screen h-screen border-2 border-slate-500 rounded-lg z-30"
   >
     <div
       class="w-full rounded-t-lg h-16 bg-gradient-to-b from-10% from-[#264744] via-[#325d5a] vie-10% to-[#264744] to-95% border-b border-[#1c3634]"
@@ -10,19 +10,104 @@
         @pointerdown.prevent.stop
         @mousedown.prevent.stop
       >
-        <img src="/img/icon/LOGO.png" class="rounded-tl-lg" />
+        <SecurityButton
+          :tooltip="tooltip"
+          @mouse-enter="mouseEnter"
+          @access-switch="accessSwitch"
+          @mouse-leave="mouseLeave"
+        />
       </div>
+
       <MainNavbar />
     </div>
     <div class="flex justify-center items-center w-full h-full bg-[#33393E]">
       <slot></slot>
     </div>
     <div class="w-full h-[30px] rounded-b-lg bg-[#33393E]" @pointerdown.prevent.stop @mousedown.prevent.stop>
+      <TheFooter />
       <TaskManager />
     </div>
   </div>
 </template>
-<script setup>
+<script>
 import MainNavbar from "../UI/node-header/MainNavbar.vue";
 import TaskManager from "../UI/task-manager/TaskManager.vue";
+import TheFooter from "../layers/TheFooter.vue";
+import SecurityButton from "../UI/node-header/SecurityButton.vue";
+import { mapWritableState } from "pinia";
+import { useNodeHeader } from "@/store/nodeHeader";
+import { useFooter } from "@/store/theFooter";
+
+export default {
+  components: {
+    MainNavbar,
+    TaskManager,
+    TheFooter,
+    SecurityButton,
+  },
+  data() {
+    return {
+      tooltip: false,
+      serverAccMange: this.$t("serverManagement.serverAccMange"),
+    };
+  },
+  computed: {
+    ...mapWritableState(useFooter, {
+      stereumStatus: "stereumStatus",
+      cursorLocation: "cursorLocation",
+    }),
+    ...mapWritableState(useNodeHeader, {
+      serverAccessManagement: "serverAccessManagement",
+    }),
+  },
+  methods: {
+    mouseEnter() {
+      this.tooltip = true;
+      this.cursorLocation = this.serverAccMange;
+    },
+
+    mouseLeave() {
+      this.tooltip = false;
+      this.cursorLocation = "";
+    },
+
+    accessSwitch() {
+      console.log("accessSwitch");
+      this.serverAccessManagement = !this.serverAccessManagement;
+    },
+  },
+};
 </script>
+<style scoped>
+.server-access-tooltip {
+  width: 220%;
+  height: 50%;
+  background-color: #1e2429;
+  border: 1px solid #b4b4b4;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  top: 5%;
+  left: 15%;
+  border-radius: 10px;
+  color: #eee;
+  font-size: 65%;
+  font-weight: 600;
+  text-transform: uppercase;
+}
+.securBtn-parent {
+  cursor: pointer;
+}
+.securBtn-box {
+  width: 100%;
+  z-index: 51;
+}
+.securBtn-box img {
+  width: 100%;
+  z-index: 51;
+}
+.securBtn-box img:active {
+  transform: scale(0.98);
+}
+</style>
