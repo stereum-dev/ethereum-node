80016926187c923c1106910aa0a332a26d2a8b3f
daverolo <107847185+daverolo@users.noreply.github.com>
2023-10-19 16:29:49 +0200

    FIX: os check in new welcome component

--- launcher/src/components/UI/welcome-page/components/WelcomeFooter.vue
+++ launcher/src/components/UI/welcome-page/components/WelcomeFooter.vue
@@ -33,9 +33,8 @@ const t = i18n.global.t;
 
 const supportMessage = t("installitionMenu.osSupported");
 
-const active = ref(false);
+const active = ref(true);
 const isSupported = ref(false);
-const running = ref(false);
 let message = ref(null);
 
 const os = computed(() => {
@@ -45,31 +44,10 @@ const os = computed(() => {
 // Lifecycle Hooks
 
 onMounted(() => {
-  osCheck();
   checkOsRequirements();
 });
 
 //Methods
-const osCheck = () => {
-  active.value = true;
-  setTimeout(() => {
-    if (os.value.includes("Win")) {
-      message.value = "Windows";
-      isSupported.value = true;
-    } else if (os.value.includes("Mac")) {
-      message.value = "MacOS";
-      isSupported.value = true;
-    } else if (os.value.includes("Linux")) {
-      message.value = "Linux";
-      isSupported.value = true;
-    } else {
-      message.value = "Not Supported";
-      isSupported.value = false;
-    }
-    active.value = false;
-  }, 5000);
-};
-
 const display = async (osResponse, suResponse) => {
   const osData = await osResponse;
   const suData = await suResponse;
@@ -106,7 +84,7 @@ const display = async (osResponse, suResponse) => {
   } else {
     message.value = "UNSUPPORTED OS";
   }
-  running.value = false;
+  active.value = false;
 };
 const checkOsRequirements = async () => {
   const osResponse = await ControlService.checkOS();
