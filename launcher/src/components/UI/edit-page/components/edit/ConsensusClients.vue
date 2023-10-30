537baf55a28b0cf90d10cf93674d3d0317e3d409
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2023-10-30 16:42:52 +0100

    Update ConsensusClients.vue
    CHANGE unused to comment

--- launcher/src/components/UI/edit-page/components/edit/ConsensusClients.vue
+++ launcher/src/components/UI/edit-page/components/edit/ConsensusClients.vue
@@ -42,8 +42,8 @@ const emit = defineEmits(["deleteService", "switchClient", "modifyService", "inf
 //Refs
 
 const manageStore = useNodeManage();
-const isMouseOverClient = ref(false);
-const isMousePassedClient = ref(false);
+//const isMouseOverClient = ref(false);
+//const isMousePassedClient = ref(false);
 
 // computed & watchers properties
 const getConsensus = computed(() => {
