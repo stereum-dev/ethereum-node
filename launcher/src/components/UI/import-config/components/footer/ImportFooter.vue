70f7232dc465045f4d869f0b5a400e47b6f2b236
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2023-10-31 16:19:46 +0100

    FIX: config process

---
+++ launcher/src/components/UI/import-config/components/footer/ImportFooter.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="w-full h-full col-start-1 col-span-full row-start-11 row-span-2 grid grid-cols-12 gird-rows-2 px-4"
+    @mousedown.prevent
+  >
+    <div
+      class="col-start-1 col-span-2 row-start-1 row-span-2 justify-self-end w-[120px] h-12 bg-[#264744] hover:bg-[#447a75] rounded-full py-2 px-4 flex justify-center items-center hover:scale-110 hover:shadow-lg hover:shadow-[#1e2a29] transition-all duration-300 ease-in-out active:scale-100 active:shadow-none"
+      @click="goBack"
+    >
+      <span class="text-gray-200 text-xl font-semibold text-center uppercase">Back</span>
+    </div>
+    <div
+      class="col-start-11 col-span-1 row-start-1 row-span-2 w-[120px] h-12 bg-[#264744] hover:bg-[#447a75] rounded-full py-2 px-4 flex justify-center items-center hover:scale-110 hover:shadow-lg hover:shadow-[#1e2a29] transition-all duration-300 ease-in-out active:scale-100 active:shadow-none cursor-pointer"
+      :class="!installStore.isConfigButtonEnbabled ? 'pointer-events-none opacity-50' : ''"
+      @click="goToNext"
+    >
+      <span class="text-gray-200 text-xl font-semibold text-center uppercase">Next</span>
+    </div>
+  </div>
+</template>
+<script setup>
+import { useClickInstall } from "@/store/clickInstallation";
+import { computed } from "vue";
+import { useRouter } from "vue-router";
+
+const emit = defineEmits(["runInstaller"]);
+const router = useRouter();
+const installStore = useClickInstall();
+
+const fullPath = computed(() => {
+  return router.currentRoute.value.fullPath;
+});
+
+const goToNext = () => {
+  if (fullPath.value === "/config/upload") {
+    router.push("/config/list");
+  } else if (fullPath.value === "/config/list") {
+    router.push("/config/sync");
+  } else if (fullPath.value === "/config/sync") {
+    router.push("/config/verify");
+  } else if (fullPath.value === "/config/verify") {
+    emit("runInstaller");
+  }
+};
+
+const goBack = () => {
+  router.back();
+};
+</script>
---
+++ launcher/src/components/UI/import-config/components/header/ImportHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="col-start-5 col-end-21 row-start-1 row-span-2 grid grid-cols-6 grid-rows-2 p-1 items-end gap-2">
+    <div class="h-14 col-start-1 col-end-4 row-start-1 row-span-2 flex justify-center bg-[#1E2429] rounded-md p-1">
+      <img
+        v-if="currentNetwork"
+        class="w-[50px] h-[50px] col-start-1 col-span-2 row-start-1 row-span-2 z-10 self-center"
+        :src="currentNetwork?.icon"
+        alt="Preset Icon"
+      />
+      <div class="w-full h-full flex justify-center items-center ml-2">
+        <span class="w-full text-[22px] font-bold text-gray-400 uppercase tracking-wider">
+          {{ currentNetwork?.name }}</span
+        >
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { computed } from "vue";
+import { useClickInstall } from "@/store/clickInstallation";
+
+const installStore = useClickInstall();
+
+const currentNetwork = computed(() => {
+  return installStore.configNetwork;
+});
+</script>
