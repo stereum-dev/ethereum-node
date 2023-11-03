c567a54899058ba1b473d92c96c796b40f6efb4d
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2023-11-03 11:43:29 +0100

    ADD: group lauout

---
+++ launcher/src/components/UI/staking-page/components/list/rows/GroupRow.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="w-full h-9 bg-[#336666] rounded-full grid grid-cols-12 p-1">
+    <div class="col-start-1 col-end-4 self-center overflow-hidden flex justify-start items-center">
+      <img class="w-7 h-7" src="/img/icon/the-staking/newfolder-icon.png" alt="Folder Icon" />
+      <span class="text-center text-xs text-gray-300 ml-1 overflow-hidden"> Group Name</span>
+    </div>
+    <div
+      class="col-start-4 col-end-6 self-center text-center text-xs text-gray-300 overflow-hidden flex justify-center items-center"
+    >
+      <span class="">Contains 2 keys</span>
+    </div>
+    <div
+      class="col-start-7 col-end-8 self-center text-center text-xs text-gray-300 overflow-hidden flex justify-center items-center"
+    >
+      <span>Active</span>
+    </div>
+    <div
+      class="col-start-8 col-end-10 self-center text-center text-xs text-gray-300 overflow-hidden flex justify-center items-center"
+    >
+      <span>64.035485665</span>
+    </div>
+    <div class="col-start-10 col-span-full bg-[#151618] rounded-full grid grid-cols-4 items-center">
+      <div class="col-start-1 col-span-1 w-4 h-4 bg-teal-800 rounded-md justify-self-center"></div>
+      <div class="col-start-2 col-span-1 w-4 h-4 bg-teal-800 rounded-md justify-self-center"></div>
+      <div class="col-start-3 col-span-1 w-4 h-4 bg-teal-800 rounded-md justify-self-center"></div>
+      <div class="col-start-4 col-span-1 w-4 h-4 bg-teal-800 rounded-md justify-self-center"></div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
