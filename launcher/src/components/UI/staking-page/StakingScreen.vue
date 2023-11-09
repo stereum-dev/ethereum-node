be6504f1ead3b4fd755c5f8d14e2b8f8fcce217f
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2023-11-09 10:11:31 +0100

    CHANGE: height

--- launcher/src/components/UI/staking-page/StakingScreen.vue
+++ launcher/src/components/UI/staking-page/StakingScreen.vue
@@ -1,9 +1,9 @@
 <template>
   <base-layout>
-    <div class="w-full h-full grid grid-cols-24 grid-rows-12 relative p-1 gap-x-1">
+    <div class="w-full h-full max-h-full grid grid-cols-24 grid-rows-12 p-1">
       <SidebarSection />
       <ListSection />
-      <ManagementSection />
+      <!-- <ManagementSection /> -->
     </div>
   </base-layout>
 </template>
