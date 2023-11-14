c1336af25fa0b187e2ff35090e4807898b049022
gbayasgalan <73820006+gbayasgalan@users.noreply.github.com>
2023-11-14 21:06:01 +0100

    validator-exit API  (#1563)
    * ADD: validator-exit API for single & multiple keys
    
    * add the exit all keys to the selection option
    
    * fix the withdraw all validators
    
    * update exit chain data from pinia in the display validator
    
    * update exit validator
    
    password for exit validator removed
    
    * remove exit validator
    
    exit validator component removed from display validator
    
    * fix the confirm exit validator for both types
    
    withdraw the multi and single validator key is posible
    
    * fix the exit chan modal result
    
    ---------
    
    Co-authored-by: mabasian <54101509+mabasian@users.noreply.github.com>

--- launcher/src/backend/Monitoring.js
+++ launcher/src/backend/Monitoring.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-empty, no-prototype-builtins */
 import { ServiceManager } from "./ServiceManager";
 import { ValidatorAccountManager } from "./ValidatorAccountManager";
+import { StringUtils } from "./StringUtils.js";
+import { SSHService } from "./SSHService.js";
 import * as QRCode from "qrcode";
 import * as log from "electron-log";
 import * as crypto from "crypto";
@@ -43,7 +45,7 @@ export class Monitoring {
     this.globalMonitoringCache = { ...globalMonitoringCache };
     try {
       fs.unlinkSync(this.serviceInfosCacheFile);
-    } catch (e) { }
+    } catch (e) {}
   }
 
   // Jobs to handle on login
@@ -232,7 +234,7 @@ export class Monitoring {
         }
         //console.log('REQUIRE fresh cache ' + hash, dnow);
       }
-    } catch (e) { }
+    } catch (e) {}
     if (await this.checkStereumInstallation()) {
       var serviceConfigs = await this.serviceManagerProm.readServiceConfigurations();
       const serviceStates = await this.nodeConnectionProm.listServices();
@@ -424,11 +426,11 @@ export class Monitoring {
     var query =
       rpc_method.trim().indexOf("{") < 0
         ? JSON.stringify({
-          jsonrpc: "2.0",
-          method: rpc_method.trim(),
-          params: rpc_params,
-          id: 1,
-        })
+            jsonrpc: "2.0",
+            method: rpc_method.trim(),
+            params: rpc_params,
+            id: 1,
+          })
         : rpc_method;
 
     // Define default response
@@ -1238,12 +1240,12 @@ export class Monitoring {
             labels.forEach(function (label, index) {
               try {
                 results[label] = xx.filter((s) => s.metric.__name__ == labels[index])[0].value[1];
-              } catch (e) { }
+              } catch (e) {}
             });
             try {
               frstVal = results[labels[1]];
               scndVal = results[labels[0]];
-            } catch (e) { }
+            } catch (e) {}
           }
           // Set chain head block for this client from RPC server (if available)
           if (
@@ -1260,7 +1262,7 @@ export class Monitoring {
                 typeof chain_head_block === "string" && chain_head_block.startsWith("0x")
                   ? parseInt(chain_head_block, 16)
                   : 0;
-            } catch (e) { }
+            } catch (e) {}
             let stay_on_hold_till_first_block = false; // true = enabled | false = disabled
             if (stay_on_hold_till_first_block && !chain_head_block) {
               // stay on hold until EC has responded the first block by RPC
@@ -1569,7 +1571,7 @@ export class Monitoring {
                     .value.pop()
                 );
                 details[clientType]["numPeerBy"]["fields"].push(item);
-              } catch (e) { }
+              } catch (e) {}
             });
           }
 
@@ -1940,7 +1942,7 @@ export class Monitoring {
         await this.nodeConnection.closeTunnels(openTunnels);
         this.rpcTunnel = {};
       }
-    } catch (e) { }
+    } catch (e) {}
 
     // Respond success with fresh RPC status data
     const freshrpcstatus = await this.getRpcStatus();
@@ -2124,7 +2126,7 @@ export class Monitoring {
         await this.nodeConnection.closeTunnels(openTunnels);
         this.wsTunnel = {};
       }
-    } catch (e) { }
+    } catch (e) {}
 
     // Respond success with fresh WS status data
     const freshwsstatus = await this.getWsStatus();
@@ -2273,7 +2275,7 @@ export class Monitoring {
     try {
       let r = wsResult.stdout.trim().split("\n");
       statuscode = r.length > 0 ? parseInt(r.pop()) : statuscode;
-    } catch (e) { }
+    } catch (e) {}
 
     // Respond true if websocket is available, false otherwise
     if (!wsResult.stdout.toLowerCase().includes("sec-websocket") && statuscode != 200) {
@@ -2380,7 +2382,7 @@ export class Monitoring {
         await this.nodeConnection.closeTunnels(openTunnels);
         this.beaconTunnel = {};
       }
-    } catch (e) { }
+    } catch (e) {}
 
     // Respond success with fresh BEACON status data
     const freshbeaconstatus = await this.getBeaconStatus();
@@ -2505,8 +2507,8 @@ export class Monitoring {
     const addr_type = Array.isArray(addr)
       ? "arr"
       : typeof addr === "string" && ["public", "local"].includes(addr)
-        ? "str"
-        : "invalid";
+      ? "str"
+      : "invalid";
     addr = addr_type == "str" ? addr.toLowerCase().trim() : addr;
     if (addr_type == "invalid") {
       return {
@@ -2594,7 +2596,7 @@ export class Monitoring {
     for (let i = 0; i < serviceInfos.length; i++) {
       const hashDependencies =
         serviceInfos[i].config.dependencies.consensusClients.length ||
-          serviceInfos[i].config.dependencies.executionClients.length
+        serviceInfos[i].config.dependencies.executionClients.length
           ? "yes"
           : "no";
       easyInfos.push({
@@ -3136,6 +3138,65 @@ rm -rf diskoutput
           preJustifiedEpochStatus: [],
           finalizedEpochStatus: [],
         });
+      }
+    } catch (error) {
+      console.log("Error occured to get Beacon node status: ", error);
+      return {
+        info: "Error occured to get Beacon node status: ",
+        data: error,
+      };
+    }
+  }
+
+  async exitValidatorAccount(pubkey, password, serviceID) {
+    const beaconStatus = await this.getBeaconStatus();
+    try {
+      if (beaconStatus.code === 0) {
+        const beaconAPIPort = beaconStatus.data[0].beacon.destinationPort;
+        const serviceId = beaconStatus.data[0].sid;
+        if (!Array.isArray(pubkey)) {
+          pubkey = [pubkey];
+        }
+        const results = [];
+        for (let i = 0; i < pubkey.length; i++) {
+          const ref = StringUtils.createRandomString(); // Create a random string to identify the task
+          this.nodeConnection.taskManager.otherTasksHandler(ref, `Exit Account ${pubkey[i].substring(0, 6)}..`);
+          try {
+            const result = await this.validatorAccountManager.getExitValidatorMessage(pubkey[i], password, serviceID);
+            if (SSHService.checkExecError(result) && result.stderr) throw SSHService.extractExecError(result);
+            log.info(result);
+
+            const exitMsg = JSON.stringify(JSON.parse(result.stdout).data);
+            const exitCommand = `docker run --rm --network=stereum curlimages/curl curl 'http://stereum-${serviceId}:${beaconAPIPort}/eth/v1/beacon/pool/voluntary_exits' -H 'accept: */*' -H 'Content-Type: application/json' -d '${exitMsg}'`;
+            const runExitCommand = await this.nodeConnection.sshService.exec(exitCommand);
+            if (SSHService.checkExecError(runExitCommand) && runExitCommand.stderr)
+              throw SSHService.extractExecError(runExitCommand);
+            log.info(runExitCommand);
+
+            // if (!runExitCommand.stdout.includes("validator_index")) { // find out "successful msg" and put instead of <validator_index> !!!!
+            //   throw "Undexpected Error: " + runExitCommand.stdout;
+            // }
+
+            // Push successful task
+            this.nodeConnection.taskManager.otherTasksHandler(ref, `Exiting Account`, true, runExitCommand.stdout);
+            this.nodeConnection.taskManager.otherTasksHandler(ref);
+
+            // add pubkey into the runExitCommands' result;
+            runExitCommand["pubkey"] = `${pubkey[i]}`;
+            results.push(runExitCommand);
+          } catch (error) {
+            this.nodeConnection.taskManager.otherTasksHandler(
+              ref,
+              `Exiting Account Failed`,
+              false,
+              `Exiting Account Failed ${pubkey[i]} Failed:\n` + error
+            );
+            this.nodeConnection.taskManager.otherTasksHandler(ref);
+            log.error("Exiting signed voluntary account Failed:\n", error);
+            return error;
+          }
+        }
+        return results;
       }
     } catch (error) {
       console.log("Error occured to get Beacon node status: ", error);
