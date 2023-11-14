e71c0fd481fe2440dd956450060ecf67b4d32290
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2023-11-14 11:35:21 +0100

    FIX: switch client dropdown

--- launcher/src/components/UI/edit-page/components/modals/SwitchContent.vue
+++ launcher/src/components/UI/edit-page/components/modals/SwitchContent.vue
@@ -101,7 +101,7 @@ const switchService = (service) => {
 }
 
 .slide-fade-leave-active {
-  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
 }
 
 .slide-fade-enter-from,
