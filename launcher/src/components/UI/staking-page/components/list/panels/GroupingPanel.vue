d2fa49468a62eded15cf0c8e4320c91802c6dae1
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2023-11-15 13:55:35 +0100

    ADD: grouping panel & functions

---
+++ launcher/src/components/UI/staking-page/components/list/panels/GroupingPanel.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    class="w-full h-10 bg-[#2e3335] rounded-md flex justify-center items-center cursor-pointer border border-gray-600 p-1 shadow-md shadow-[#171D22]"
+  >
+    <div class="w-full h-full grid grid-cols-12 grid-rows-1">
+      <img
+        class="w-6 h-6 self-center col-start-1 col-span-1 justify-self-center"
+        src="/img/icon/the-staking/group.png"
+        alt="Group Icon"
+      />
+
+      <input
+        id="input1"
+        v-model="stakingStore.groupName"
+        class="col-start-2 col-end-11 w-full bg-[#171D22] border border-[#283139] px-4 py-1 rounded outline-none text-xs text-gray-400"
+        type="text"
+        autofocus
+        placeholder="Pick a name for your group..."
+      />
+
+      <div class="col-start-11 col-span-1 flex justify-center items-center p-1">
+        <div
+          class="w-7 h-7 rounded-md bg-[#171D22] p-1 flex justify-center items-center hover:scale-110 border border-[#171D22] active:scale-100 hover:shadow-md hover:shadow-[#101214] hover:border-[#3f4851] active:shadow-none transition-all duration-150"
+          @click="cancelGrouping"
+        >
+          <img class="w-5 h-5" src="/img/icon/the-staking/close.png" alt="" />
+        </div>
+      </div>
+      <div class="col-start-12 col-span-1 flex justify-center items-center p-1">
+        <div
+          class="w-7 h-7 rounded-md bg-[#171D22] p-1 flex justify-center items-center hover:scale-110 border border-[#171D22] active:scale-100 hover:shadow-md hover:shadow-[#101214] hover:border-[#3f4851] active:shadow-none transition-all duration-150"
+          @click="confirmGrouping"
+        >
+          <img class="w-4 h-4" src="/img/icon/the-staking/check.png" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { useStakingStore } from "@/store/theStaking";
+
+const emit = defineEmits(["confirmGrouping"]);
+
+const stakingStore = useStakingStore();
+
+const cancelGrouping = () => {
+  stakingStore.groupName = "";
+  stakingStore.isGroupingPanelActive = false;
+  stakingStore.isInsertPanelActive = true;
+};
+
+const confirmGrouping = () => {
+  emit("confirmGrouping", stakingStore.groupName);
+};
+</script>
--- launcher/src/components/UI/staking-page/components/list/panels/RemovePanel.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div
-    class="w-full h-8 bg-[#17A2B8] hover:bg-[#2c8692] rounded-full flex justify-center items-center cursor-pointer shadow-md shadow-gray-800"
-  >
-    <span class="text-md font-semibold text-gray-200">Grafiti</span>
-  </div>
-</template>
