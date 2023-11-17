890183d27908856c9c29ce9c8e21b5dc86042a7b
mabasian <54101509+mabasian@users.noreply.github.com>
2023-11-17 13:00:20 +0300

    add dump logs to the nuke modal
    nukeModal.vue in the edit page

--- launcher/src/components/UI/edit-page/components/modals/NukeModal.vue
+++ launcher/src/components/UI/edit-page/components/modals/NukeModal.vue
@@ -9,10 +9,10 @@
         <span class="warning">{{ $t("nukeModal.nukeQ") }} </span>
       </div>
       <div class="remove-btn">
-        <div class="save-config-btn">
-          <img src="/img/icon/manage-node-icons/log_export.png" alt="" /><span>dump logs</span>
+        <div :class="['save-config-btn', loadingDumpClass]" @click="dumpLogs">
+          <img :src="loadingIconsClass" alt="" :class="loadingBtnClass" /><span>dump logs</span>
         </div>
-        <div class="yes-box" @click="removeItems">
+        <div :class="['yes-box', loadingDumpClass]" @click="removeItems">
           <span>{{ $t("nukeModal.nukeTitle") }}</span>
         </div>
       </div>
@@ -62,7 +62,20 @@ export default {
       loginBtn: true,
       nukeNode: "url('./img/icon/arrows/NukeNode_Final.gif')",
       autoScroll: true,
+      loadingDump: false,
     };
+  },
+
+  computed: {
+    loadingDumpClass() {
+      return this.loadingDump ? "deactivate" : "";
+    },
+    loadingBtnClass() {
+      return this.loadingDump ? "animate-spin" : "";
+    },
+    loadingIconsClass() {
+      return this.loadingDump ? "/img/icon/arrows/loading.png" : "/img/icon/manage-node-icons/log_export.png";
+    },
   },
   mounted() {
     // @frontend: uncomment the block below to use nuke dummy data to style the content
@@ -81,6 +94,14 @@ export default {
     }, 100);
   },
   methods: {
+    //dummy method to simulate the nuke process
+    dumpLogs() {
+      this.loadingDump = true;
+      setTimeout(() => {
+        this.loadingDump = false;
+      }, 4000);
+    },
+    //end dummy method
     disableAutoScroll() {
       this.autoScroll = false;
     },
@@ -112,6 +133,12 @@ export default {
 };
 </script>
 <style scoped>
+.deactivate {
+  opacity: 0.5;
+  box-shadow: none;
+  pointer-events: none;
+  cursor: not-allowed;
+}
 .item {
   width: 100%;
   display: flex;
