49364a003d7e44dfe0c867d66ac8dc85ce6f006e
mabasian <54101509+mabasian@users.noreply.github.com>
2023-12-01 15:15:41 +0100

    fix the block style

--- launcher/src/components/UI/one-click/components/plugin/InstallationPath.vue
+++ launcher/src/components/UI/one-click/components/plugin/InstallationPath.vue
@@ -16,25 +16,25 @@
         />
       </div>
     </div>
-  <div
-    class="w-full col-start-1 col-span-full row-start-3 row-span-2 border rounded-md border-gray-600 mx-auto bg-[#336666]"
-  >
-    <div class="col-start-1 col-span-full row-start-1 row-span-1 flex justify-center items-center p-1">
+    <div
+      class="w-full col-start-1 col-span-full row-start-3 row-span-2 border rounded-md border-gray-600 mx-auto bg-[#336666]"
+    >
+      <div class="col-start-1 col-span-full row-start-1 row-span-1 flex justify-center items-center p-1">
         <span class="text-sm text-gray-300 font-semibold">INSTALLATION OPTIONS</span>
-    </div>
-    <div class="h-8 col-start-1 col-span-full row-start-1 row-span-1 flex justify-center items-center p-1">
-      <div class="">
-        <input
-          id="InstallOption"
-          v-model="clickStore.startServicesAfterInstall"
-          type="checkbox"
-          name="Start up client after installation?"
-          class="h-5 w-5 rounded-md border-gray-200 bg-white shadow-sm"
-        />
-        <span class="text-sm text-gray-300 font-semibold">Start up client after installation?</span>
+      </div>
+      <div class="h-8 col-start-1 col-span-full row-start-1 row-span-1 flex justify-center items-center p-1">
+        <div class="checkpoint-part">
+          <input
+            id="InstallOption"
+            v-model="clickStore.startServicesAfterInstall"
+            type="checkbox"
+            name="Start up client after installation?"
+            class="h-5 w-5 rounded-md border-gray-200 bg-white shadow-sm"
+          />
+          <span class="text-sm text-gray-300 font-semibold">Start up client after installation?</span>
+        </div>
       </div>
     </div>
-  </div>
     <div
       v-if="clickStore.selectedPreset.name === 'stereum on arm'"
       class="w-full col-start-1 col-span-full row-start-5 row-span-2 border rounded-md border-gray-600 mx-auto bg-[#336666]"
@@ -43,7 +43,7 @@
         <span class="text-sm text-gray-300 font-semibold">{{ $t("pluginName.monitor") }}</span>
       </div>
       <div class="h-8 col-start-1 col-span-full row-start-2 row-span-1 flex justify-center items-center p-1">
-        <div class="">
+        <div class="checkpoint-part">
           <label for="MarketingAccept" class="flex gap-2 cursor-pointer">
             <input
               id="MarketingAccept"
@@ -77,3 +77,12 @@ onMounted(() => {
   clickStore.startServicesAfterInstall = true;
 });
 </script>
+<style scoped>
+.checkpoint-part {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 0.5rem;
+  width: 90%;
+}
+</style>
