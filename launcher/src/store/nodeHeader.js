6f1f1d20d75e742944d8555d814e050af4e6b14b
mabasian <54101509+mabasian@users.noreply.github.com>
2023-12-22 15:30:48 +0100

    REDO: SSV modal  (#1589)
    * update the fetching
    
    * fix the new api for the testers
    
    * Revert "fix the new api for the testers"
    
    This reverts commit ab01562bba614a5ac3a60f186204e4ba992a178a.
    
    * Revert "update the fetching"
    
    This reverts commit b5a052c880f412cd597f73d00b5e0c6ad9702836.
    
    * UPDATE: new SSV modal structured
    
    * UPDATE: password controller to generate key
    
    * UPDATE: last step of the generate encrypt key
    
    * ADD: confirm box plugin to use in the modals
    
    * UPDATE: delete additional css
    
    * ADD: import box plugin to service modals
    
    * FIX: all confirm boxes added
    
    * ADD: password box added to service modal
    
    * UPDATE: the Algorithm  of steps
    
    * Fix: two steps of ssv modal
    
    just one step left
    
    * UPDATE: the second row is done
    
    * fixthe first step of third row
    
    * fix the activate the rows in empty input condition
    
    * add the new ssv loader gif
    
    * Fix the last step from first row
    
    * Fix the SSV modal

--- launcher/src/store/nodeHeader.js
+++ launcher/src/store/nodeHeader.js
@@ -2,6 +2,8 @@ import { defineStore } from "pinia";
 export const useNodeHeader = defineStore("nodeHeader", {
   state: () => {
     return {
+      importBoxModel: "",
+      passwordBoxModel: "",
       selectedValidatorFromNodeAlert: {},
       openModalFromNodeAlert: false,
       depositFile: false,
