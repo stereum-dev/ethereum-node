e6a88b89dd0e29b110e607f28fd6e4c7cd386451
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2023-12-27 22:46:21 +0100

    ADD: log section layers

--- launcher/src/components/UI/node-page/sections/LogsSection.vue
+++ launcher/src/components/UI/node-page/sections/LogsSection.vue
@@ -1,21 +1,28 @@
 <template>
-  <div></div>
+  <div class="w-full h-full col-start-1 col-span-full items-center grid grid-cols-24 grid-rows-12 absolute">
+    <LogsHeader :client="client" />
+    <LogsBody :client="client" />
+    <LogsFooter :client="client" />
+  </div>
 </template>
 
 //
 <script setup>
+import LogsHeader from "../components/logs/LogHeader.vue";
+import LogsBody from "../components/logs/LogBody.vue";
+import LogsFooter from "../components/logs/LogFooter.vue";
 // import ControlDialog from "../../the-control/ControlDialog.vue";
 // import { useNodeStore } from "@/store/theNode";
 // import { useServices } from "@/store/services";
 // import { saveAs } from "file-saver";
 // import { ref } from "vue";
 
-// const props = defineProps({
-//   item: {
-//     type: Object,
-//     default: null,
-//   },
-// });
+const props = defineProps({
+  client: {
+    type: Object,
+    default: null,
+  },
+});
 
 // const nodeStore = useNodeStore();
 // const serviceStore = useServices();
