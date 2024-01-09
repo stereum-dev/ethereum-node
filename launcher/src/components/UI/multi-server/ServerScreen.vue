0bf9419e54b3857d2424c90cf6b457df7bc234d0
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2024-01-09 16:35:03 +0100

    ADD: some avatars

--- launcher/src/components/UI/multi-server/ServerScreen.vue
+++ launcher/src/components/UI/multi-server/ServerScreen.vue
@@ -4,11 +4,126 @@ import ServerHeader from './components/ServerHeader.vue';
     class="w-full h-full absolute inset-0 grid grid-cols-24 grid-rows-7 bg-gray-700 z-10 p-2 rounded-md divide-y-2 divide-gray-500"
   >
     <ServerHeader />
-    <ServerBody />
+    <ServerBody @pick-server="setServerConnection" @change-password="changePassword" />
   </div>
 </template>
 
 <script setup>
 import ServerHeader from "./components/ServerHeader.vue";
 import ServerBody from "./components/ServerBody.vue";
+import { ref, onMounted, computed } from "vue";
+import ControlService from "@/store/ControlService";
+import { useControlStore } from "@/store/theControl";
+import { useServers } from "@/store/servers";
+
+const controlStore = useControlStore();
+const serverStore = useServers();
+
+const selectedConnection = ref({});
+const keys = ref([]);
+const confirmIndexDelete = ref([]);
+const chngPassword = ref(false);
+const newPass = ref("");
+const comparePass = ref([]);
+const keyLocation = ref("");
+const acceptedPass = ref("");
+const passChk = ref(false);
+
+const passSSHRow = computed(() => (!selectedConnection.value.useAuthKey ? "pass" : "ssh"));
+const controlPass = computed(() => (passChk.value ? "denyPass" : ""));
+
+onMounted(async () => {
+  await loadStoredConnections();
+  await readSSHKeyFile();
+});
+
+const setServerConnection = (server) => {
+  serverStore.servers.forEach((s) => (s.isConnected = false));
+  server.isConnected = true;
+};
+
+const readSSHKeyFile = async () => {
+  keys.value = await ControlService.readSSHKeyFile();
+};
+
+const formatKey = (key) => {
+  let keyArray = key.split(" ");
+  return `${keyArray[0]} ... ${keyArray.pop()}`;
+};
+
+const loadStoredConnections = async () => {
+  const savedConnections = await ControlService.readConfig();
+  selectedConnection.value = savedConnections.savedConnections.find((item) => item.host === controlStore.ipAddress);
+};
+
+const addKey = (newKeys) => {
+  keys.value = newKeys;
+};
+
+const confirmDelete = async (key) => {
+  await ControlService.writeSSHKeyFile(keys.value.filter((item) => item !== key));
+  await readSSHKeyFile();
+  confirmIndexDelete.value = [];
+  confirmIndex.value = null;
+};
+
+const confirmKeyIndex = (index) => {
+  confirmIndexDelete.value[index] = true;
+  confirmIndex.value = index;
+};
+
+const cancelKeyIndex = (index) => {
+  confirmIndexDelete.value[index] = false;
+  confirmIndex.value = null;
+};
+
+const generateModal = () => {
+  controlStore.generateModalShow = !controlStore.generateModalShow;
+};
+
+const changePassword = (newPassword) => {
+  console.log(newPassword);
+  serverStore.newPassword = "";
+};
+
+const acceptChangePass = async () => {
+  if (newPass.value === "") {
+    passmessage.value = t("serverManagement.passmessage");
+  } else {
+    if (comparePass.value.length < 1) {
+      comparePass.value.push(newPass.value);
+      newPass.value = "";
+      passmessage.value = t("serverManagement.confirmPass");
+      passChk.value = false;
+    } else {
+      const passwordEqual = comparePass.value.every((item) => item === newPass.value);
+      if (passwordEqual) {
+        acceptedPass.value = newPass.value;
+        chngPassword.value = false;
+        newPass.value = "";
+        comparePass.value = [];
+        passChk.value = false;
+        await ControlService.changePassword(acceptedPass.value);
+      } else {
+        newPass.value = "";
+        passmessage.value = "The passwords do not match";
+        comparePass.value = [];
+        passChk.value = true;
+      }
+    }
+  }
+};
+
+const previewFiles = async (event) => {
+  const Path = event.target.files[0].path;
+  let pathString = new String(Path);
+  let result = pathString.toString();
+  keyLocation.value = result;
+  await ControlService.AddExistingSSHKey(keyLocation.value);
+  await readSSHKeyFile();
+};
+
+const openUploadHandler = () => {
+  document.querySelector('input[type="file"]').click();
+};
 </script>
--- launcher/src/components/UI/multi-server/components/ServerBody.vue
+++ launcher/src/components/UI/multi-server/components/ServerBody.vue
@@ -2,12 +2,12 @@
   <div
     class="w-full h-full col-start-1 col-span-full row-start-2 row-span-full grid grid-cols-24 grid-rows-12 p-2 divide-x-2 divide-gray-500"
   >
-    <div class="col-start-1 col-end-11 row-start-1 row-span-full p-1 grid grid-cols-12 grid-rows-12">
+    <div class="col-start-1 col-end-11 row-start-1 row-span-full p-1 pl-0 grid grid-cols-12 grid-rows-12">
       <LoginBox v-if="serverStore.isServerLoginActive" />
-      <ManagementBox v-if="serverStore.isServerManagementActive" />
+      <ManagementBox v-if="serverStore.isServerManagementActive" @change-password="changePassword" />
     </div>
     <div class="col-start-11 col-span-full row-start-1 row-span-full p-1 grid grid-cols-12 grid-rows-12">
-      <SavedServers @pick-server="selectServerToConnect" @server-login="openLoginForm" />
+      <SavedServers @pick-server="pickServer" @server-login="openLoginForm" />
     </div>
   </div>
 </template>
@@ -18,14 +18,20 @@ import SavedServers from "./saved-servers/SavedServers.vue";
 import LoginBox from "./login-form/LoginBox.vue";
 import { useServers } from "@/store/servers";
 
+const emit = defineEmits(["pickServer", "serverLogin", "changePassword"]);
+
 const serverStore = useServers();
 
-const selectServerToConnect = (server) => {
-  serverStore.selectedServerToConnect = server;
+const pickServer = (server) => {
+  emit("pickServer", server);
 };
 
 const openLoginForm = () => {
   serverStore.isServerLoginActive = true;
   serverStore.isServerManagementActive = false;
+};
+
+const changePassword = (newPassword) => {
+  emit("changePassword", newPassword);
 };
 </script>
