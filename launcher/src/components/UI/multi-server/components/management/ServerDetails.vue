e6d05cb1dd2f571c0c1cb84b6d75990fee1486ba
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2024-01-09 16:32:34 +0100

    ADD: server details layout

--- launcher/src/components/UI/multi-server/components/management/ServerDetails.vue
+++ launcher/src/components/UI/multi-server/components/management/ServerDetails.vue
@@ -1,3 +1,79 @@
 <template>
-  <div>Server Details</div>
+  <div
+    class="w-full h-full col-start-1 col-span-full row-start-1 row-end-6 bg-slate-800 rounded-md grid grid-cols-2 grid-rows-4 p-2 relative"
+  >
+    <div class="col-start-1 col-span-1 row-start-1 row-span-1 flex flex-col justify-start items-start">
+      <p class="text-xs text-gray-300 font-semibold uppercase">Server Name</p>
+      <span>{{ serverName }}</span>
+    </div>
+    <div class="col-start-2 col-span-1 row-start-1 row-span-1 flex flex-col justify-start items-start">
+      <p class="text-xs text-gray-300 font-semibold uppercase">Machine Name</p>
+      <span>{{ serverName }}</span>
+    </div>
+    <div class="col-start-1 col-span-1 row-start-2 row-span-1 flex flex-col justify-start items-start">
+      <p class="text-xs text-gray-300 font-semibold uppercase">IP or HOSTNAME</p>
+      <span>{{ serverIp }}</span>
+    </div>
+    <div class="col-start-2 col-span-1 row-start-2 row-span-1 flex flex-col justify-start items-start">
+      <p class="text-xs text-gray-300 font-semibold uppercase">PORT</p>
+      <span>{{ serverName }}</span>
+    </div>
+    <div class="col-start-2 col-span-1 row-start-3 row-span-1 flex flex-col justify-start items-start">
+      <p class="text-xs text-gray-300 font-semibold uppercase">USERNAME</p>
+      <span>{{ serverName }}</span>
+    </div>
+    <div class="col-start-1 col-span-1 row-start-3 row-span-1 flex justify-start items-start relative space-x-1">
+      <img
+        class="w-6 h-6 rounded-full border border-gray-300"
+        :src="selectedAvatar"
+        alt="Avatar"
+        @click="avatarModal"
+      />
+      <p class="text-xs text-gray-300 font-semibold uppercase pt-1">Choose Avatar</p>
+      <AvatarModal @pick-avatar="pickSererAvatar" />
+    </div>
+
+    <ChangePassword @change-password="changePassword" />
+  </div>
 </template>
+<script setup>
+import ChangePassword from "./ChangePassword.vue";
+import AvatarModal from "../modals/AvatarModal.vue";
+import { computed } from "vue";
+import { useControlStore } from "@/store/theControl";
+import { useServers } from "@/store/servers";
+
+const emit = defineEmits(["changePassword"]);
+
+const controlStore = useControlStore();
+const serverStore = useServers();
+
+const serverName = computed(() => {
+  return controlStore.serverName;
+});
+
+const serverIp = computed(() => {
+  return controlStore.ipAddress;
+});
+
+const selectedAvatar = computed(() => {
+  if (serverStore.selectedAvatar?.img) {
+    return serverStore.selectedAvatar?.img;
+  } else {
+    return "/avatar/astronaut.png";
+  }
+});
+
+const changePassword = (newPassword) => {
+  emit("changePassword", newPassword);
+};
+
+const avatarModal = () => {
+  serverStore.isAvatarBoxActive = !serverStore.isAvatarBoxActive;
+};
+
+const pickSererAvatar = (avatar) => {
+  serverStore.selectedAvatar = avatar;
+  serverStore.isAvatarBoxActive = false;
+};
+</script>
--- launcher/src/store/servers.js
+++ launcher/src/store/servers.js
@@ -7,13 +7,65 @@ export const useServers = defineStore("servers", {
       isServerLoginActive: false,
       isServerManagementActive: true,
       selectedServerToConnect: null,
-
+      selectedAvatar: null,
+      newPassword: "",
+      avatars: [
+        {
+          name: "Centos",
+          img: "/avatar/centos.png",
+        },
+        {
+          name: "Debian",
+          img: "/avatar/debian.png",
+        },
+        {
+          name: "Ubuntu",
+          img: "/avatar/ubuntu.png",
+        },
+        {
+          name: "Linux",
+          img: "/avatar/linux.png",
+        },
+        {
+          name: "Fedora",
+          img: "/avatar/fedora.png",
+        },
+        {
+          name: "Lion",
+          img: "/avatar/lion.png",
+        },
+        {
+          name: "Panda",
+          img: "/avatar/panda.png",
+        },
+        {
+          name: "Dog",
+          img: "/avatar/dog.png",
+        },
+        {
+          name: "Dog-1",
+          img: "/avatar/dog-1.png",
+        },
+        {
+          name: "Dog-2",
+          img: "/avatar/dog-2.png",
+        },
+        {
+          name: "Footprint",
+          img: "/avatar/footprint.png",
+        },
+        {
+          name: "Dog-2",
+          img: "/avatar/dog-2.png",
+        },
+      ],
       servers: [
         {
           name: "Stereum Server",
           ip: "115.35.01.65",
           port: "25565",
           isConnected: false,
+          isSelected: false,
           icon: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
         },
         {
@@ -21,41 +73,7 @@ export const useServers = defineStore("servers", {
           ip: "115.35.01.65",
           port: "25565",
           isConnected: false,
-          icon: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-        },
-        {
-          name: "Google Server",
-          ip: "115.35.01.65",
-          port: "25565",
-          isConnected: false,
-          icon: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-        },
-        {
-          name: "AWS Server",
-          ip: "101.1.321.65",
-          port: "25565",
-          isConnected: false,
-          icon: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-        },
-        {
-          name: "Digital Ocean Server",
-          ip: "201.1.321.65",
-          port: "25565",
-          isConnected: false,
-          icon: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-        },
-        {
-          name: "Linode Server",
-          ip: "301.1.321.65",
-          port: "25565",
-          isConnected: false,
-          icon: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-        },
-        {
-          name: "Vultr Server",
-          ip: "401.1.321.65",
-          port: "25565",
-          isConnected: false,
+          isSelected: false,
           icon: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
         },
       ],
@@ -75,6 +93,14 @@ export const useServers = defineStore("servers", {
       this.servers.forEach((server) => {
         if (server.name === this.selectedServerToConnect.name) {
           server.isConnected = false;
+        }
+      });
+    },
+
+    setNewPassword(server) {
+      this.servers.forEach((s) => {
+        if (s.name === server.name) {
+          s.password = this.newPassword;
         }
       });
     },
