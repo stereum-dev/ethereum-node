7374d4d7de995a10729e322355c016e8dc42956f
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2024-01-18 17:52:21 +0100

    FIX: logs current filter

--- launcher/src/components/UI/node-page/components/logs/LogBody.vue
+++ launcher/src/components/UI/node-page/components/logs/LogBody.vue
@@ -2,7 +2,7 @@
   <div
     class="w-full h-full col-start-1 col-span-full row-start-2 row-end-12 grid grid-cols-24 grid-rows-12 bg-[#2d3035]"
   >
-    <LogSidebar />
+    <LogSidebar :client="props.client" />
     <div
       class="w-full h-full col-start-2 col-span-full row-start-1 row-span-full overflow-x-hidden overflow-y-auto max-h-[409px] p-1 flex flex-col justify-start items-start space-y-1 bg-[#202225]"
     >
@@ -34,6 +34,13 @@
 import LogSidebar from "./LogSidebar";
 import { useNodeStore } from "@/store/theNode";
 import { computed, ref } from "vue";
+
+const props = defineProps({
+  client: {
+    type: Object,
+    default: null,
+  },
+});
 
 const nodeStore = useNodeStore();
 const hoveredLogIndex = ref(null);
