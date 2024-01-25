c3f5117664d417d91bf7e1414c851b8534e95f23
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2024-01-25 11:40:04 +0100

    REMOVE: hydration config (#1647)
    * FIX: rcancel importing on pass panel
    
    * FIX: hydration warning
    
    * FIX: typo & change anim size
    
    * REMOVE : hydration config
    
    * REVERT: config loader

--- launcher/vue.config.js
+++ launcher/vue.config.js
@@ -48,15 +48,7 @@ module.exports = {
   },
 
   chainWebpack: (config) => {
-    config.resolve.alias.set("vue-i18n", "vue-i18n/dist/vue-i18n.esm-bundler.js");
-    config.module.rule("vue").use("vue-loader").loader("vue-loader");
-    config.module.rule("css").use("css-loader").loader("css-loader");
-    config.plugin("define").tap((definitions) => {
-      Object.assign(definitions[0], {
-        __VUE_PROD_DEVTOOLS__: "false",
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: "false",
-      });
-      return definitions;
-    });
+    config.resolve.alias.set("vue-i18n", "vue-i18n/dist/vue-i18n.cjs.js");
+    config.module.rule("vue").use("vue-loader", "css-loader");
   },
 };
