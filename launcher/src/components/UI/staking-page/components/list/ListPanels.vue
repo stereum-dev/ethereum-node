ac9f3947a9352310ece2a1faa60432b454131385
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2024-02-02 12:39:06 +0100

    FIX: remove group btn

--- launcher/src/components/UI/staking-page/components/list/ListPanels.vue
+++ launcher/src/components/UI/staking-page/components/list/ListPanels.vue
@@ -177,9 +177,9 @@ const groupingPanel = () => {
   stakingStore.isGroupingAllowed = true;
 };
 const removeGroup = () => {
-  stakingStore.isGroupListActive = false;
-  stakingStore.setActivePanel("insert");
   if (stakingStore.currentGroup) {
+    stakingStore.isGroupListActive = false;
+    stakingStore.setActivePanel("insert");
     emit("removeGroup", stakingStore.currentGroup);
   }
 };
