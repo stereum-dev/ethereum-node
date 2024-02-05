b18502ded9fdb128157fc51a7abd8ef2e7ab4bee
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2024-02-05 16:03:35 +0100

    ADD: menu box to header

--- launcher/src/components/UI/server-management/components/ServerHeader.vue
+++ launcher/src/components/UI/server-management/components/ServerHeader.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="col-start-1 col-span-full row-start-1 row-span-1 flex justify-center items-center">
-
-    <span class="text-5xl text-gray-200 font-bold">SERVER MANAGEMENT</span>
-
+  <div
+    class="col-start-1 col-span-full row-start-1 row-span-1 grid grid-cols-24 items-center"
+  >
+    <span class="col-start-4 col-end-14 text-3xl text-gray-200 font-bold"
+      >SERVER MANAGEMENT</span
+    >
+    <div
+      class="col-start-14 col-span-full w-full h-full grid grid-cols-12 grid-rows-5 items-center pr-2 pl-1"
+    >
+      <div
+        class="col-start-1 col-span-full row-start-2 row-end-5 h-full bg-[#1b1b1d] rounded-md"
+      >
+        Menu
+      </div>
+    </div>
   </div>
 </template>
