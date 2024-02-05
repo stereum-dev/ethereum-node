2ebb0d13e0d5db14675507dc28df1000e71b4321
mabasian <54101509+mabasian@users.noreply.github.com>
2024-02-05 12:47:17 +0100

    ADD service select button to new p2p net

--- launcher/src/components/UI/the-control/NewPeerToPeer.vue
+++ launcher/src/components/UI/the-control/NewPeerToPeer.vue
@@ -6,11 +6,40 @@
       </div>
       <span>PEER TO PEER NETWORK</span>
     </div>
+    <div class="np2p-rowsbox">
+      <div class="service-rox"></div>
+    </div>
+    <div class="service-selction">
+      <div
+        class="service-icon"
+        :style="{ background: selectedService == consensus ? '#94DEFF' : '' }"
+        @click="selectService(consensus)"
+      >
+        <img :src="controlStore.currentConsensusIcon" alt="" />
+      </div>
+      <div
+        class="service-icon"
+        :style="{ background: selectedService == execution ? '#94DEFF' : '' }"
+        @click="selectService(execution)"
+      >
+        <img :src="controlStore.currentExecutionIcon" alt="" />
+      </div>
+    </div>
   </div>
 </template>
+<script setup>
+import { useControlStore } from "@/store/theControl";
+import { ref } from "vue";
 
-<script>
-export default {};
+const controlStore = useControlStore();
+const selectedService = ref("consensus");
+const consensus = ref("consensus");
+const execution = ref("execution");
+
+const selectService = (service) => {
+  selectedService.value = service;
+  console.log(selectedService.value);
+};
 </script>
 
 <style scoped>
@@ -49,5 +78,61 @@ export default {};
   font-size: 50%;
   font-weight: bold;
   color: #c1c1c1;
+}
+.np2p-rowsbox {
+  width: 50%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+}
+.service-rox {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 95%;
+  height: 25%;
+  margin: 1.2px;
+  font-size: 70%;
+  font-weight: 600;
+  border: 1px solid #c1c1c1;
+  border-radius: 10px;
+  padding: 1% 2%;
+}
+.service-selction {
+  width: 20%;
+  height: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+}
+.service-icon {
+  width: 90%;
+  height: 40%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #232222;
+  border-radius: 10px;
+  margin: 1.2px;
+  border: 1px solid #232222;
+  box-shadow: 1px 1px 5px 1px #171717;
+  cursor: pointer;
+}
+.service-icon img {
+  width: 1.5rem;
+  height: 1.5rem;
+}
+.service-icon:hover {
+  background: #313131;
+  border: 1px solid #c1c1c1;
+  box-shadow: none;
+}
+.service-icon:active {
+  background: #313131;
+  border: none;
+  box-shadow: inset 1px 1px 5px 1px #171717;
 }
 </style>
