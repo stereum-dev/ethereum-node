da6af7f0f3be068f216386368478d1cb60ab1e02
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2024-02-05 12:26:46 +0100

    REMOVE: old login page

--- launcher/src/components/UI/multi-server/components/ServerHeader.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="col-start-1 col-span-full row-start-1 row-span-1 flex justify-center items-center">
-
-    <span class="text-5xl text-gray-200 font-bold">SERVER MANAGEMENT</span>
-
-  </div>
-</template>
