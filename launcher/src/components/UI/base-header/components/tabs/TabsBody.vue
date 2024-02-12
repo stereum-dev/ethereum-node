c3175272319cc101176d1421e32e2d72e431f35d
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2024-02-12 22:34:20 +0100

    ADD: pages tabs

---
+++ launcher/src/components/UI/base-header/components/tabs/TabsBody.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="col-start-3 col-end-12 grid grid-cols-3 items-center gap-x-1">
+    <SingleTab v-for="tab in tabs" :key="tab.page" :tab="tab" />
+  </div>
+</template>
+
+<script setup>
+import SingleTab from "./SingleTab.vue";
+import { ref } from "vue";
+
+const tabs = ref([
+  { page: "Node", path: "/node", relativePath: "/edit" },
+  { page: "Control", path: "/control" },
+  { page: "Staking", path: "/staking" },
+]);
+</script>
