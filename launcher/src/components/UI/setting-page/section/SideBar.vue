c9ea50ddabe10c8fad268d5962ea155061f30647
mabasian <54101509+mabasian@users.noreply.github.com>
2024-02-22 16:39:00 +0100

    Add title to sidebar

--- launcher/src/components/UI/setting-page/section/SideBar.vue
+++ launcher/src/components/UI/setting-page/section/SideBar.vue
@@ -3,8 +3,20 @@
     class="flex flex-col items-start justify-start h-full w-full bg-[#33393E]"
     style="border-right: 1px solid #707070"
   >
+    <div
+      class="title mt-3 bg-[#336666] w-11/12 h-12 pr-4 font-semibold text-xl uppercase box-border justify-end flex items-center"
+    >
+      settings
+    </div>
     <slot></slot>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.title {
+  color: #eee;
+  border: 1px solid #343434;
+  box-shadow: 5px 1px 10px 1px rgb(36, 36, 36);
+  border-radius: 0 10px 10px 0;
+}
+</style>
