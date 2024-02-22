3dd25df9e5b65d11b901d12c406824774eda003d
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2024-02-22 18:17:17 +0100

    ADD: disabling btns during process

--- launcher/src/components/UI/server-management/components/server-update/UpdateServer.vue
+++ launcher/src/components/UI/server-management/components/server-update/UpdateServer.vue
@@ -2,26 +2,35 @@
   <div
     class="w-full h-full col-start-1 col-span-full row-start-1 row-span-full bg-[#1b1b1d] rounded-md grid grid-cols-12 grid-rows-12 p-2 pt-0 gap-y-2"
   >
-    <div class="w-full h-full col-start-1 col-span-full row-start-1 row-span-1 flex justify-start items-center py-1">
+    <div
+      class="w-full h-full col-start-1 col-span-full row-start-1 row-span-1 flex justify-start items-center py-1"
+    >
       <span class="text-md font-semibold text-gray-300">OPERATING SYSTEM UPDATES</span>
     </div>
     <div
       class="w-full h-full col-start-1 col-span-full row-start-2 row-end-4 grid grid-cols-6 grid-rows-2 items-center gap-x-1"
     >
-      <div class="w-full h-full col-start-1 col-span-1 row-start-1 row-span-full flex justify-center items-center">
+      <div
+        class="w-full h-full col-start-1 col-span-1 row-start-1 row-span-full flex justify-center items-center"
+      >
         <img class="w-full" src="/img/icon/control/ubuntuIco.svg" />
       </div>
-      <div class="w-full h-full col-start-2 col-span-full row-start-1 row-span-1 flex justify-start items-center ml-2">
+      <div
+        class="w-full h-full col-start-2 col-span-full row-start-1 row-span-1 flex justify-start items-center ml-2"
+      >
         <span class="text-lg font-semibold text-gray-400 uppercase">Ubuntu</span>
       </div>
 
-      <div class="w-full h-full col-start-2 col-span-full row-start-2 row-span-1 ml-2 grid grid-cols-4 items-center">
+      <div
+        class="w-full h-full col-start-2 col-span-full row-start-2 row-span-1 ml-2 grid grid-cols-4 items-center"
+      >
         <span class="col-start-1 col-span-3 text-sm font-semibold text-gray-400 uppercase"
           >OPERATING SYSTEM VERSION</span
         >
-        <span class="col-start-4 col-span-1 text-sm font-semibold text-amber-400 uppercase">{{
-          osVersionCurrent
-        }}</span>
+        <span
+          class="col-start-4 col-span-1 text-sm font-semibold text-amber-400 uppercase"
+          >{{ osVersionCurrent }}</span
+        >
       </div>
     </div>
     <div
@@ -30,13 +39,19 @@
       <div
         class="w-full h-full col-start-1 col-span-full row-start-1 row-span-2 grid grid-cols-6 items-center gap-x-2 px-1"
       >
-        <div class="w-full col-start-1 col-span-1 bg-red-700 rounded-sm flex justify-center item-center">
+        <div
+          class="w-full col-start-1 col-span-1 bg-red-700 rounded-sm flex justify-center item-center"
+        >
           <span class="text-sm font-semibold text-gray-300 text-center">{{
-            serverStore.upgradablePackages?.length ? serverStore.upgradablePackages.length : 0
+            serverStore.upgradablePackages?.length
+              ? serverStore.upgradablePackages.length
+              : 0
           }}</span>
         </div>
 
-        <span class="col-start-2 col-span-full text-md font-semibold text-gray-300">AVAILABLE SERVER OS UPDATES</span>
+        <span class="col-start-2 col-span-full text-md font-semibold text-gray-300"
+          >AVAILABLE SERVER OS UPDATES</span
+        >
       </div>
 
       <div
@@ -50,23 +65,32 @@
           @update-package="updatePackage"
         />
       </div>
-      <div class="col-start-1 col-span-full row-start-11 row-span-full w-full h-full grid grid-cols-12 py-2">
+      <div
+        class="col-start-1 col-span-full row-start-11 row-span-full w-full h-full grid grid-cols-12 py-2"
+      >
         <div class="w-full h-full col-start-1 col-end-6 flex justify-center items-center">
           <div
             class="w-full h-full flex justify-evenly items-center bg-[#4d7575] hover:bg-[#243535] rounded-sm active:scale-90 shadow-md shadow-black active:shadow-none transition-all duration-100 ease-in-out cursor-pointer"
             :class="{
-              'opacity-40 pointer-events-none bg-[#3d4244] scale-95': newUpdates.length === 0,
+              'opacity-40 pointer-events-none bg-[#3d4244] scale-95':
+                serverStore.isUpdateProcessing,
             }"
             @click.prevent="updateAll"
           >
-            <span class="text-gray-100 text-sm font-semibold uppercase">{{ $t("updatePanel.all") }}</span>
+            <span class="text-gray-100 text-sm font-semibold uppercase">{{
+              $t("updatePanel.all")
+            }}</span>
             <img class="w-4" src="/img/icon/node-icons/download2.png" alt="icon" />
           </div>
         </div>
-        <div class="w-full h-full col-start-7 col-span-full flex justify-center items-center p-1">
+        <div
+          class="w-full h-full col-start-7 col-span-full flex justify-center items-center p-1"
+        >
           <span class="text-gray-200 text-md font-semibold"
             >{{ $t("updatePanel.auto") }} :
-            <span class="text-md uppercase font-semibold" :class="onOff">{{ stereumApp.autoUpdate }}</span></span
+            <span class="text-md uppercase font-semibold" :class="onOff">{{
+              stereumApp.autoUpdate
+            }}</span></span
           >
         </div>
       </div>
@@ -137,9 +161,9 @@ const getUpgradablePackages = async () => {
 };
 
 const updatePackage = async (item) => {
+  serverStore.isUpdateProcessing = true;
   console.log("update package");
   try {
-    serverStore.isUpdateProcessing = true;
     await ControlService.updatePackage(item.packageName);
     serverStore.isUpdateProcessing = false;
   } catch (error) {
@@ -149,8 +173,10 @@ const updatePackage = async (item) => {
 };
 
 const updateAll = async () => {
+  serverStore.isUpdateProcessing = true;
   try {
     await ControlService.updateOS();
+    serverStore.isUpdateProcessing = false;
   } catch (error) {
     console.log(error);
   }
