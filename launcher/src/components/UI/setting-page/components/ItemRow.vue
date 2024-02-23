e6bbce20c0c02617d0a7009d1d5af2b3a93dc81e
mabasian <54101509+mabasian@users.noreply.github.com>
2024-02-23 18:54:19 +0100

    update the volume slider style

--- launcher/src/components/UI/setting-page/components/ItemRow.vue
+++ launcher/src/components/UI/setting-page/components/ItemRow.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="item-parent w-full h-12 bg-[#242529] rounded-xl shadow-2xl mt-2 flex justify-between items-center p-1">
     <span class="ml-4 font-semibold uppercase">{{ props.title }}</span>
-    <div v-if="hasSlotContent" class="mr-4 w-1/3 rounded-md h-full bg-[#111213] flex flex-col justify-center">
+    <div v-if="hasSlotContent" class="mr-4 w-1/3 rounded-md h-full bg-[#33393E] flex flex-col justify-center">
       <slot></slot>
     </div>
   </div>
--- launcher/src/components/UI/setting-page/components/VolumeSlider.vue
+++ launcher/src/components/UI/setting-page/components/VolumeSlider.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="flex w-full h-full items-center justify-center">
     <span style="color: aliceblue; width: 40px" class="flex justify-start items-center font-semibold">{{ value }}</span
-    ><vue-slider v-model="value" dot-size="14" width="170px" tooltip="none"></vue-slider>
+    ><vue-slider
+      v-model="value"
+      dot-size="16"
+      width="170px"
+      :process-style="{ backgroundColor: '#a1c1ad' }"
+      :tooltip-style="{ backgroundColor: 'pink', borderColor: 'pink' }"
+      tooltip="none"
+    >
+      <template #dot="{ focus }">
+        <div :class="['custom-dot', { focus }]"></div>
+      </template>
+    </vue-slider>
   </div>
 </template>
 
@@ -12,3 +23,16 @@ import { ref } from "vue";
 
 const value = ref(0);
 </script>
+<style scoped>
+.custom-dot {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: #336666;
+  transition: 0.2s;
+  cursor: pointer;
+}
+.custom-dot:active {
+  background-color: #a1c1ad;
+}
+</style>
