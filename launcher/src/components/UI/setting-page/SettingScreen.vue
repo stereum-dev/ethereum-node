18287d25370165f83d07494088b8b91afc5fbf4b
mabasian <54101509+mabasian@users.noreply.github.com>
2024-02-23 14:40:24 +0100

    fix toggle for btns

--- launcher/src/components/UI/setting-page/SettingScreen.vue
+++ launcher/src/components/UI/setting-page/SettingScreen.vue
@@ -4,8 +4,16 @@
 
     <div class="flex justify-start items-start w-56 h-full">
       <SideBar>
-        <SidebarBtn btn-name="general" />
-        <SidebarBtn btn-name="audio" />
+        <SidebarBtn
+          btn-name="general"
+          :is-active="mainBox == 'general' ? 'true' : 'false'"
+          @toggle-active="toggleSettings('general')"
+        />
+        <SidebarBtn
+          btn-name="audio"
+          :is-active="mainBox == 'audio' ? 'true' : 'false'"
+          @toggle-active="toggleSettings('audio')"
+        />
       </SideBar>
     </div>
     <div class="bg-black w-5/6 h-full"><MainBox /></div>
@@ -16,4 +24,11 @@
 import SideBar from "./section/SideBar.vue";
 import MainBox from "./section/MainBox";
 import SidebarBtn from "./components/SidebarBtn";
+import { ref } from "vue";
+
+const mainBox = ref("general");
+
+const toggleSettings = (arg) => {
+  mainBox.value = arg;
+};
 </script>
--- launcher/src/components/UI/setting-page/components/SidebarBtn.vue
+++ launcher/src/components/UI/setting-page/components/SidebarBtn.vue
@@ -1,17 +1,18 @@
 <template>
   <div
     class="btn-parent mt-3 w-11/12 h-12 pr-4 font-semibold text-xl uppercase box-border justify-end flex items-center cursor-pointer"
-    :class="{ 'active-btn': isActive }"
+    :class="{ 'active-btn': active }"
     @click="toggleActive"
   >
     {{ props.btnName }}
     <img
+      class="w-5 h-5 mr-2 ml-2"
       :src="
         props.btnName == 'audio'
           ? '/img/icon/setting-page/volume-icon.png'
-          : isActive
-          ? '/img/icon/setting-page/update-icon-grey.png'
-          : '/img/icon/setting-page/update-icon-black.png'
+          : active
+          ? '/img/icon/setting-page/update-icon-black.png'
+          : '/img/icon/setting-page/update-icon-grey.png'
       "
       :alt="props.btnName"
     />
@@ -19,15 +20,21 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { defineProps, computed, defineEmits } from "vue";
+
 const props = defineProps({
   btnName: String,
+  isActive: String,
 });
 
-const isActive = ref(false);
+// Convert 'isActive' string prop to a boolean value
+const active = computed(() => props.isActive === "true");
+
+// Define emits
+const emit = defineEmits(["toggle-active"]);
 
 const toggleActive = () => {
-  isActive.value = !isActive.value;
+  emit("toggle-active");
 };
 </script>
 
@@ -36,20 +43,20 @@ const toggleActive = () => {
   border: 1px solid #343434;
   box-shadow: 5px 1px 10px 1px rgb(36, 36, 36);
   border-radius: 10px;
-  background: #a1c1ad;
-  color: #000;
+  background: #242529;
+  color: #eee;
+  transition: 0.3s;
 }
 .btn-parent img {
-  widows: 20px;
-  height: 20px;
-  margin-left: 10px;
-  margin-right: 10px;
   transition: 0.3s;
 }
 .active-btn {
   background: #242529;
   color: #eee;
+  background: #a1c1ad;
+  color: #000;
   cursor: default;
+  transition: 0.3s;
 }
 .btn-parent:active {
   box-shadow: 1px 1px 10px 1px #171717 inset;
