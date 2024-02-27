5a4b3a45542d6193a572d10d21db8f2063dd2765
mabasian <54101509+mabasian@users.noreply.github.com>
2024-02-27 21:45:19 +0100

    Fix disable state of staking page (#1725)
    * Fix disable section for staking page
    
    * active disable section

--- launcher/src/components/UI/staking-page/sections/DisabledSection.vue
+++ launcher/src/components/UI/staking-page/sections/DisabledSection.vue
@@ -1,16 +1,17 @@
 <template>
-  <div
-    class="w-7/8 h-[439px] max-h-[489px] grid grid-cols-24 grid-rows-12 items-center bg-[#252525] p-8 absolute top-3 left-11 z-10 rounded-r-t"
-    @mousedown.prevent
-  >
-    <div class="w-full h-full col-start-1 col-span-full row-start-3 row-end-7 flex justify-center items-center">
-      <img class="w-81 mt-2" src="/img/icon/the-staking/staking-disabled.png" alt="icon" />
-    </div>
-
+  <div class="full-body w-full h-full absolute grid grid-cols-24 grid-rows-12 z-10">
     <div
-      class="w-full h-full col-start-1 col-span-full row-start-10 row-end-12 flex justify-center items-center text-center mt-2"
+      class="disable-layer grid grid-cols-12 grid-rows-12 col-start-2 col-end-20 row-start-1 row-end-12 bg-[#252525] z-10 rounded-md"
+      @mousedown.prevent
     >
-      <span class="text-cyan-400 text-2xl font-semibold font-sans">{{ t("disableStake.message") }}</span>
+      <div class="col-start-2 col-end-12 row-start-2 col-span-full row-end-8 flex justify-center items-center">
+        <img class="w-2/3" src="/img/icon/the-staking/staking-disabled.png" alt="icon" />
+      </div>
+      <div
+        class="col-start-2 col-end-12 col-span-full row-start-10 row-end-11 flex justify-center items-center text-center mt-2"
+      >
+        <span class="text-cyan-400 text-2xl font-semibold font-sans">{{ t("disableStake.message") }}</span>
+      </div>
     </div>
   </div>
 </template>
