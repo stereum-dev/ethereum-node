b0f5bd9758eec7baaf57360572836dc65967f73c
daGscheid <david.scheidl@rocklogic.at>
2024-02-29 14:20:49 +0100

    Rename "header-icons" folder to "base-header-icons" to better fit layout

--- launcher/src/components/UI/base-header/components/menu/MenuBody.vue
+++ launcher/src/components/UI/base-header/components/menu/MenuBody.vue
@@ -12,25 +12,25 @@ import { ref } from "vue";
 const items = ref([
   {
     name: "Help",
-    icon: "/img/icon/header-icons/header-help-button.png",
+    icon: "/img/icon/base-header-icons/header-help-button.png",
   },
   {
     name: "Notifications",
-    icon: "/img/icon/header-icons/header-notification-button.png",
+    icon: "/img/icon/base-header-icons/header-notification-button.png",
   },
   {
     name: "Available Update",
-    icon: "/img/icon/header-icons/header-update-button-green.png",
-    activeIcon: "/img/icon/header-icons/header-update-button-blue.png",
+    icon: "/img/icon/base-header-icons/header-update-button-green.png",
+    activeIcon: "/img/icon/base-header-icons/header-update-button-blue.png",
   },
   {
     name: "Settings",
-    icon: "/img/icon/header-icons/header-stereum-settings-button.png",
+    icon: "/img/icon/base-header-icons/header-stereum-settings-button.png",
     path: "/setting",
   },
   {
     name: "Logout",
-    icon: "/img/icon/header-icons/header-logout-button.png",
+    icon: "/img/icon/base-header-icons/header-logout-button.png",
   },
 ]);
 </script>
--- launcher/src/components/UI/base-header/components/modals/NotifModal.vue
+++ launcher/src/components/UI/base-header/components/modals/NotifModal.vue
@@ -133,9 +133,9 @@ export default {
     return {
       qrPage: true,
       beaconchaDashboard: false,
-      banner: "/img/icon/header-icons/notification-modal-selection-stereum-node-monitor.png",
+      banner: "/img/icon/base-header-icons/notification-modal-selection-stereum-node-monitor.png",
       qrCode: "/img/icon/task-manager-icons/turning_circle_blue.gif",
-      ErrorQRCode: "/img/icon/header-icons/notification-modal-dummy-qr-code.png",
+      ErrorQRCode: "/img/icon/base-header-icons/notification-modal-dummy-qr-code.png",
       selectedVal: "",
       machineName: "",
       apiKey: "",
