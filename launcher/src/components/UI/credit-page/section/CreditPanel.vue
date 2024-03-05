c509b613e9e05048740fa2fe01ed6f577e9c3316
mabasian <54101509+mabasian@users.noreply.github.com>
2024-03-05 14:54:45 +0100

    clean additional components and codes

--- launcher/src/components/UI/credit-page/section/CreditPanel.vue
+++ launcher/src/components/UI/credit-page/section/CreditPanel.vue
@@ -1,30 +1,6 @@
 <template>
   <div class="credit-panel_parent h-full">
     <div class="credit-panel_box w-full h-full">
-      <!-- <div class="credit-panel_title">
-          <div class="ttl-box">
-            <div class="credit-panel_title_ico">
-              <img :src="technikalIco" />
-            </div>
-            <div class="credit-panel_title_ttl">
-              <span>{{ $t("creditPanel.techTitle") }}</span>
-            </div>
-          </div>
-        </div> -->
-      <!-- <div class="desc-line">
-          <small>{{ $t("creditPanel.techText") }} </small>
-          <div
-            class="desc-line-button"
-            @click="creditToggle"
-            @mouseenter="cursorLocation = 'Click to open the Stereum donation page'"
-            @mouseleave="cursorLocation = ''"
-          >
-            <div class="desc-line-button_ico">
-              <img :src="financialIco" alt="" />
-            </div>
-            <span>{{ $t("creditPanel.financialPage") }}</span>
-          </div>
-        </div> -->
       <div
         class="credit-header col-start-1 col-end-3 row-start-1 row-end-4 flex w-full h-full justify-center items-center"
       >
@@ -39,31 +15,6 @@
 <script setup>
 import ContributorsList from "./ContributorsList.vue";
 import CreditHeader from "./CreditHeader.vue";
-
-// const financialIco = "./img/icon/credit-page-icons/financial-credit.png";
-// const technikalIco = "./img/icon/credit-page-icons/technical-credit.png";
-
-// const creditToggle = () => {
-//   let url = "https://stereum.net/donate";
-//   window.open(url, "_blank");
-// };
-
-// export default {
-//   components: { ContributorsList, CreditHeader },
-//   data() {
-//     return {
-//       financialIco: "./img/icon/credit-page-icons/financial-credit.png",
-//       technikalIco: "./img/icon/credit-page-icons/technical-credit.png",
-//     };
-//   },
-
-//   methods: {
-//     creditToggle() {
-//       let url = "https://stereum.net/donate";
-//       window.open(url, "_blank");
-//     },
-//   },
-// };
 </script>
 <style scoped>
 .credit-panel_box {
@@ -74,59 +25,7 @@ import CreditHeader from "./CreditHeader.vue";
 .credit-container {
   grid-area: 4/ 1 / 9 / 3;
 }
-.desc-line {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  color: #eee;
-  font-weight: 400;
-  width: 90%;
-  height: 10%;
-}
-.desc-line-button {
-  display: flex;
-  width: 25%;
-  font-size: 80%;
-  height: 4%;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  font-weight: 400;
-  border: 1px solid #343434;
-  background: rgb(42, 42, 42);
-  box-sizing: border-box;
-  box-shadow: 1px 1px 10px 1px rgb(23, 23, 23);
-  border-radius: 10px;
-  position: fixed;
-  left: 70%;
-  top: 11%;
-}
-.desc-line-button span {
-  width: 80%;
-  height: 100%;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  text-transform: uppercase;
-  font-size: 100%;
-  font-weight: 700;
-}
 
-.desc-line-button:hover,
-.desc-line-button:focus {
-  font-weight: 600;
-  background: #313131;
-}
-.desc-line-button_ico {
-  display: flex;
-  height: 100%;
-  width: 20%;
-  justify-content: center;
-  align-items: center;
-}
-.desc-line-button_ico img {
-  width: 50%;
-}
 .credit-panel_parent {
   display: flex;
   flex-direction: column;
@@ -138,100 +37,5 @@ import CreditHeader from "./CreditHeader.vue";
   z-index: 0;
   box-sizing: border-box;
   cursor: default;
-}
-
-.credit-panel_title {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 95%;
-  height: 35px;
-}
-.ttl-box {
-  display: flex;
-  width: 60%;
-  height: 100%;
-  justify-content: flex-start;
-  align-items: center;
-  margin: 0 5%;
-}
-.credit-panel_title_ico {
-  width: 10%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.credit-panel_title_ico img {
-  width: 60%;
-}
-.credit-panel_title_ttl {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  font-size: 130%;
-  color: #eee;
-  text-transform: uppercase;
-  font-weight: 500;
-}
-.base-container {
-  width: 100%;
-  height: 90%;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-}
-.confirm-btn {
-  width: 18%;
-  height: 65%;
-  background: #48e148;
-  border: 1px solid #707070;
-  box-shadow: 1px 1px 10px 1px rgb(23, 23, 23);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 100%;
-  font-weight: 600;
-  color: #fff;
-  border-radius: 20px;
-  cursor: pointer;
-  margin: 5% 0;
-}
-.confirm-btn:hover,
-.confirm-btn:focus {
-  border: 1px solid white;
-  font-weight: 800;
-}
-.division-line {
-  width: 95%;
-  height: 1%;
-  background: #eee;
-}
-
-.footer {
-  background-color: #343434;
-  border-radius: 0 0 7px 7px;
-  width: 100%;
-  height: 5%;
-  display: flex;
-}
-.fade-box-enter-from,
-.fade-box-leave-to {
-  opacity: 0;
-}
-
-.fade-box-enter-active {
-  transition: opacity 0.3s ease-out;
-}
-.fade-box-leave-active {
-  transition: opacity 0.3s ease-in;
-}
-
-.fade-box-enter-to,
-.fade-box-leave-from {
-  opacity: 1;
 }
 </style>
