aa46427df0018a16c694747bb8e41b5d552ba60d
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2024-04-17 15:36:00 +0200

    CHANGE: service title

--- launcher/src/components/UI/node-page/sections/ServiceSection.vue
+++ launcher/src/components/UI/node-page/sections/ServiceSection.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="w-full h-full flex flex-col justify-between items-center pt-1">
     <div class="w-full flex flex-col justify-between items-center px-4 rounded-t-lg">
-      <span class="text-md font-semibold text-gray-200 uppercase">Services</span>
-      <div class="w-full flex justify-between items-center px-1 overflow-hidden">
-        <img v-for="el in getServices" :key="el.name" class="w-5" :src="el.sIcon" alt="icon" />
+      <span class="text-sm font-semibold text-gray-200 uppercase">Server Services</span>
+      <div class="w-full mt-2 flex justify-between items-center px-1 overflow-hidden">
+        <img
+          v-for="el in getServices"
+          :key="el.name"
+          class="w-5"
+          :src="el.sIcon"
+          alt="icon"
+        />
       </div>
     </div>
 
