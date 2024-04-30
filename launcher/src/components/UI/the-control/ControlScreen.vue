4f3df2e95eddf003916b4496f62a87feb4ff6885
mabasian <54101509+mabasian@users.noreply.github.com>
2024-04-30 12:09:14 +0200

    UPDATE: change control screen to composition API

--- launcher/src/components/UI/the-control/ControlScreen.vue
+++ launcher/src/components/UI/the-control/ControlScreen.vue
@@ -12,7 +12,7 @@
               <img src="/img/icon/control-page-icons/arrow-up-1.png" alt="" />
             </div>
             <div ref="pluginsTable" class="plugins-table">
-              <div v-for="(item, index) in installedServices" :key="index" class="plugins-row">
+              <div v-for="(item, index) in serviceStore.installedServices" :key="index" class="plugins-row">
                 <div
                   class="plugins-pending-state"
                   :class="{
@@ -63,7 +63,7 @@
                       </div>
                     </div>
                     <div class="icon-bg">
-                      <div class="seting-icon" @click.stop="openLogPage(item)">
+                      <div class="seting-icon" @click.stop="expertModeHandler(item)">
                         <img src="/img/icon/service-setting-icons/setting8.png" alt="icon" />
                       </div>
                     </div>
@@ -77,7 +77,7 @@
                   @hide-modal="hideExpertMode(item)"
                 />
               </div>
-              <LogsSection v-if="isLogsPageActive" @close-log="closeLogPage" @export-log="exportLogs" />
+              <!-- <LogsSection v-if="isLogsPageActive" @close-log="closeLogPage" @export-log="exportLogs" /> -->
             </div>
             <div class="arrow-down" @click="scrollDown">
               <img src="/img/icon/control-page-icons/arrow-down-1.png" alt="icon" />
@@ -95,141 +95,96 @@
     <!-- End Control main layout -->
   </base-layout>
 </template>
-<script>
-import LogsSection from "../node-page/sections/LogsSection.vue";
+<script setup>
+// injs ro composition kardam, hala mesle nodeScreen bayad log ro inja ham bezarim
+// faghat log be jay expert modal az tarafe service ha bayad baz beshe
+
+// import LogsSection from "../node-page/sections/LogsSection.vue";
 import { useStateHandler } from "@/composables/services";
 import ControlDashboard from "./ControlDashboard.vue";
 import ControlPlugins from "./ControlPlugins.vue";
 import ControlAlert from "./ControlAlert.vue";
-import { saveAs } from "file-saver";
+// import { saveAs } from "file-saver";
 import ExpertWindow from "../node-page/sections/ExpertWindow.vue";
-import { mapWritableState } from "pinia";
-import { useServices } from "../../../store/services";
-import { useNodeStore } from "@/store/theNode";
+import { useServices } from "@/store/services";
+// import { useNodeStore } from "@/store/theNode";
 import { useFooter } from "@/store/theFooter";
-export default {
-  components: {
-    ControlDashboard,
-    ControlPlugins,
-    ControlAlert,
-    LogsSection,
-    ExpertWindow,
-  },
-  data() {
-    return {
-      isExpertWindowOpen: false,
-      expertModeClient: null,
-      pending: this.$t("controlPage.pending"),
-      off: this.$t("controlPage.off"),
-      on: this.$t("controlPage.on"),
-      settingService: this.$t("controlPage.settingService"),
-      isLogsPageActive: false,
-    };
-  },
-  create() {
-    this.installedServices = this.installedServices.map((i) => {
-      return {
-        isServicePending: false,
-        ...i,
-      };
-    });
-  },
-  computed: {
-    ...mapWritableState(useServices, {
-      installedServices: "installedServices",
-      runningServices: "runningServices",
-    }),
-    ...mapWritableState(useNodeStore, {
-      serviceLogs: "serviceLogs",
-      exportLogs: "exportLogs",
-    }),
-    ...mapWritableState(useFooter, {
-      cursorLocation: "cursorLocation",
-      isConsensusRunning: "isConsensusRunning",
-      dialog: "dialog",
-    }),
-    isAnyConsensusRunning() {
-      const consensusServices = this.installedServices.filter((item) => item.category === "consensus");
+// import { useControlStore } from "@/store/theControl";
+import { ref, onMounted, computed, watch } from "vue";
+// import i18n from "@/includes/i18n";
 
-      if (consensusServices.length === 0) {
-        return false;
-      }
+// const t = i18n.global.t;
+const pluginsTable = ref(null);
 
-      return consensusServices.some((item) => item.state === "running");
-    },
-  },
-  watch: {
-    isAnyConsensusRunning(newValue) {
-      this.isConsensusRunning = newValue;
-    },
-  },
-  methods: {
-    scrollUp() {
-      this.$refs.pluginsTable.scrollTop -= 50;
-    },
-    scrollDown() {
-      this.$refs.pluginsTable.scrollTop += 50;
-    },
-    hideExpertMode(el) {
-      this.expertModeClient = el;
-      this.isExpertWindowOpen = false;
-    },
-    expertModeHandler(el) {
-      this.expertModeClient = el;
-      this.isExpertWindowOpen = true;
-      console.log("expertModeHandler", el);
-    },
-    expertModeHandlerAlert(validator) {
-      this.expertModeClient = validator;
-      this.isExpertWindowOpen = true;
-    },
-    stateHandler(item) {
-      useStateHandler(item);
-    },
+const expertModeClient = ref(null);
+const isExpertWindowOpen = ref(false);
+// const pending = t("controlPage.pending");
+// const off = t("controlPage.off");
+// const on = t("controlPage.on");
+// const settingService = t("controlPage.settingService");
+// const isLogsPageActive = ref(false);
 
-    //   const exportLogs = async (client) => {
-    // const currentService = nodeStore.serviceLogs.find(
-    //   (service) => service.config?.serviceID === client.config?.serviceID
-    // );
+// const nodeStore = useNodeStore();
+const serviceStore = useServices();
+// const controlStore = useControlStore();
+const footerStore = useFooter();
 
-    // const fileName = nodeStore.exportLogs ? `${client.name}_150_logs.txt` : `${client.name}_all_logs.txt`;
+onMounted(() => {
+  serviceStore.installedServices = serviceStore.installedServices.map((service) => ({
+    isServicePending: false,
+    ...service,
+  }));
+});
 
-    // // Select the data based on the condition
-    // const data = nodeStore.exportLogs ? currentService.logs.slice(-150).reverse() : currentService.logs.reverse();
+const isAnyConsensusRunning = computed(() => {
+  const consensusServices = serviceStore.installedServices.filter((service) => service.category === "consensus");
+  return consensusServices.length > 0 && consensusServices.some((service) => service.state === "running");
+});
 
-    // const lineByLine = data.map((line, index) => `#${data.length - index}: ${line}`).join("\n\n");
-    // const blob = new Blob([lineByLine], { type: "text/plain;charset=utf-8" });
-    // saveAs(blob, fileName);
-    //   };
+watch(isAnyConsensusRunning, (newValue) => {
+  footerStore.isConsensusRunning = newValue;
+});
 
-    openLogPage(item) {
-      this.isLogsPageActive = true;
-      this.serviceLogs = item;
-    },
-
-    closeLogPage() {
-      this.isLogsPageActive = false;
-      this.serviceLogs = null;
-    },
-
-    // async exportLogs() {
-    //   const currentService = this.serviceLogs.find(
-    //     (service) => service.config?.serviceID === this.serviceLogs.config?.serviceID
-    //   );
-
-    //   const fileName = this.exportLogs
-    //     ? `${this.serviceLogs.name}_150_logs.txt`
-    //     : `${this.serviceLogs.name}_all_logs.txt`;
-
-    //   const data = this.exportLogs ? currentService.logs.slice(-150).reverse() : currentService.logs.reverse();
-
-    //   const lineByLine = data.map((line, index) => `#${data.length - index}: ${line}`).join("\n\n");
-
-    //   const blob = new Blob([lineByLine], { type: "text/plain;charset=utf-8" });
-    //   saveAs(blob, fileName);
-    // },
-  },
+const scrollUp = () => {
+  if (pluginsTable.value) {
+    pluginsTable.value.scrollTop -= 50;
+  }
 };
+
+const scrollDown = () => {
+  if (pluginsTable.value) {
+    pluginsTable.value.scrollTop += 50;
+  }
+};
+
+const hideExpertMode = (el) => {
+  expertModeClient.value = el;
+  isExpertWindowOpen.value = false;
+};
+
+const expertModeHandler = (el) => {
+  expertModeClient.value = el;
+  isExpertWindowOpen.value = true;
+};
+
+const expertModeHandlerAlert = (validator) => {
+  expertModeClient.value = validator;
+  isExpertWindowOpen.value = true;
+};
+
+const stateHandler = (item) => {
+  useStateHandler(item);
+};
+
+// const openLogPage = (item) => {
+//   isLogsPageActive.value = true;
+//   controlStore.serviceLogs = item;
+// };
+
+// const closeLogPage = () => {
+//   isLogsPageActive.value = false;
+//   controlStore.serviceLogs = null;
+// };
 </script>
 
 <style scoped>
