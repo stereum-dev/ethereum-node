a2701d672296c2d1a69106649f5273abeeedf10a
mabasian <54101509+mabasian@users.noreply.github.com>
2024-05-03 06:14:32 +0200

    ADD two factor base btn added

--- launcher/src/components/UI/server-management/components/two-factor-auth/TwoFactorAuth.vue
+++ launcher/src/components/UI/server-management/components/two-factor-auth/TwoFactorAuth.vue
@@ -1,7 +1,15 @@
 <template>
   <div
-    class="2-fa-auth w-full h-full col-start-1 col-span-full row-start-1 row-span-full bg-[#1b1b1d] rounded-md grid grid-cols-12 grid-rows-12 p-2 space-y-2"
-  ></div>
+    class="2-fa-auth w-full h-full col-start-1 col-span-full row-start-1 row-span-full bg-[#1b1b1d] rounded-md grid grid-cols-24 grid-rows-14 p-2"
+  >
+    <span
+      class="top-ttl col-start-1 col-span-full row-start-1 row-span-1 text-gray-100 text-base font-semibold uppercase flex justify-start items-center"
+      >2 FACTOR AUTHENTICATION</span
+    >
+    <TwoFactorBtn class="row-start-3" btn-name="Setup" @btnClick="enableTwoFactor" />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import TwoFactorBtn from "./TwoFactorBtn.vue";
+</script>
