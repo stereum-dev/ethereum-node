8c81e0d95c778a52e1825a982165e8cd12ac9f07
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2024-05-07 13:41:47 +0200

    FIX: exclude common servises from total network

--- launcher/src/components/UI/edit-page/components/edit/header/NetworkDetails.vue
+++ launcher/src/components/UI/edit-page/components/edit/header/NetworkDetails.vue
@@ -5,18 +5,31 @@
     @mouseenter="footerStore.cursorLocation = `${currIs} ${getNetworkName}`"
     @mouseleave="footerStore.cursorLocation = ''"
   >
-    <div v-if="setupStore.isConfigViewActive" class="w-full self-start text-xs font-semibold text-teal-700">
+    <div
+      v-if="setupStore.isConfigViewActive"
+      class="w-full self-start text-xs font-semibold text-teal-700"
+    >
       {{ t("networkDetails.currentNet") }}
     </div>
-    <div v-else class="w-full self-start text-xs font-semibold text-teal-700 overflow-hidden">
+    <div
+      v-else
+      class="w-full self-start text-xs font-semibold text-teal-700 overflow-hidden"
+    >
       TOTAL SETUPS ON SERVER
     </div>
-    <div v-if="setupStore.isConfigViewActive" class="w-full flex justify-center items-center">
+    <div
+      v-if="setupStore.isConfigViewActive"
+      class="w-full flex justify-center items-center"
+    >
       <img v-if="getNetworkIcon" :src="getNetworkIcon" alt="Networks" class="w-5 mr-1" />
-      <span class="text-md text-gray-300 text-left overflow-hidden whitespace-pre">{{ getNetworkName }}</span>
+      <span class="text-md text-gray-300 text-left overflow-hidden whitespace-pre">{{
+        getNetworkName
+      }}</span>
     </div>
     <div v-else class="w-full flex justify-center items-center">
-      <span class="text-md text-gray-300 text-left overflow-hidden whitespace-pre">{{ totalNetworks }}</span>
+      <span class="text-md text-gray-300 text-left overflow-hidden whitespace-pre">{{
+        totalNetworks
+      }}</span>
     </div>
   </div>
 </template>
@@ -47,7 +60,9 @@ const getNetworkIcon = computed(() => {
 
 const totalNetworks = computed(() => {
   let total = 0;
-  total = setupStore.allSetups.map((setup) => setup).length;
+  total = setupStore.allSetups
+    .filter((setup) => setup.network !== "default")
+    .map((setup) => setup).length;
 
   return total;
 });
