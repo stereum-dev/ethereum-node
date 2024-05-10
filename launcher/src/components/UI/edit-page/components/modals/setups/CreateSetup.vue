20c3089470052aedb36d5be4a138678de5a337f1
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2024-05-10 15:41:44 +0200

    FIX: color picker

--- launcher/src/components/UI/edit-page/components/modals/setups/CreateSetup.vue
+++ launcher/src/components/UI/edit-page/components/modals/setups/CreateSetup.vue
@@ -86,9 +86,14 @@
               <div
                 v-for="col in colorPalette"
                 :key="col.name"
-                class="w-6 h-6 rounded-full col-span-1 row-start-2 cursor-pointer mx-1 hover:scale-110 transition-all duration-100 hover:shadow-lg hover:shadow-black hover:border hover:border-gray-500 active:scale-95"
-                :class="`${col.bg}`"
-                @click="getSetupColor(col.name)"
+                class="w-6 h-6 rounded-full col-span-1 row-start-2 cursor-pointer mx-1 hover:scale-110 transition-all duration-100 hover:shadow-lg hover:shadow-black hover:border hover:border-gray-500 active:scale-120"
+                :class="[
+                  `${col.bg}`,
+                  col.isSelected
+                    ? 'border border-gray-300 scale-125 shadow-black  shadow-lg'
+                    : '',
+                ]"
+                @click="getSetupColor(col)"
               >
                 <input
                   type="radio"
@@ -130,18 +135,18 @@ const colorMsg = ref("");
 
 const { loadSetups, loadServices, getAllSetups } = useMultiSetups();
 
-const colorPalette = [
-  { name: "default", bg: "bg-[#336666]" },
-  { name: "blue", bg: "bg-blue-500" },
-  { name: "red", bg: "bg-red-500" },
-  { name: "yellow", bg: "bg-[#F7C566]" },
-  { name: "orange", bg: "bg-[#fb923c]" },
-  { name: "green", bg: "bg-green-500" },
-  { name: "beige", bg: "bg-[#D1BB9E]" },
-  { name: "purple", bg: "bg-violet-500" },
-  { name: "brown", bg: "bg-[#503C3C]" },
-  { name: "gray", bg: "bg-gray-500" },
-];
+const colorPalette = ref([
+  { name: "default", bg: "bg-[#336666]", isSelected: false },
+  { name: "blue", bg: "bg-blue-500", isSelected: false },
+  { name: "red", bg: "bg-red-500", isSelected: false },
+  { name: "yellow", bg: "bg-[#F7C566]", isSelected: false },
+  { name: "orange", bg: "bg-[#fb923c]", isSelected: false },
+  { name: "green", bg: "bg-green-500", isSelected: false },
+  { name: "beige", bg: "bg-[#D1BB9E]", isSelected: false },
+  { name: "purple", bg: "bg-violet-500", isSelected: false },
+  { name: "brown", bg: "bg-[#503C3C]", isSelected: false },
+  { name: "gray", bg: "bg-gray-500", isSelected: false },
+]);
 
 const setupLayouts = computed(() => {
   let layout;
@@ -197,7 +202,12 @@ const validationForm = async () => {
 };
 
 const getSetupColor = (color) => {
-  setupColor.value = color;
+  colorPalette.value.forEach((col) => {
+    col.isSelected = false;
+  });
+  color.isSelected = true;
+  setupColor.value = color.name;
+  console.log("color", setupColor.value);
 };
 
 const confirm = async () => {
