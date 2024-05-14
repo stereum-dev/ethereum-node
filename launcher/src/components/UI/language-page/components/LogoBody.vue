65f76f577b0ed614ea4e2d67112078ca452d189f
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2024-05-14 17:04:17 +0200

    CHANGE: lang animation (#1874)

--- launcher/src/components/UI/language-page/components/LogoBody.vue
+++ launcher/src/components/UI/language-page/components/LogoBody.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="w-full h-full col-start-1 col-span-8 row-start-1 row-span-full grid grid-cols-12 grid-rows-12">
+  <div
+    class="w-full h-full col-start-1 col-end-8 row-start-1 row-span-full grid grid-cols-12 grid-rows-12"
+  >
     <img
-      class="col-start-2 col-end-12 row-start-1 row-end-11"
-      src="/img/icon/language-page-icons/stereum-logo.svg"
-      alt="Logo Icon"
+      class="w-full h-full col-start-2 col-end-12 row-start-1 row-span-full"
+      src="/animation/language/heartcore.gif"
+      alt="Logo Anim"
     />
   </div>
 </template>
