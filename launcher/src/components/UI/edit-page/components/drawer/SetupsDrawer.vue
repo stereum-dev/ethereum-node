7c7ce5fd25242c407fdf39d38d0a3771c53afc28
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2024-06-12 16:11:52 +0200

    UPDATE: setup drawer

--- launcher/src/components/UI/edit-page/components/drawer/SetupsDrawer.vue
+++ launcher/src/components/UI/edit-page/components/drawer/SetupsDrawer.vue
@@ -5,18 +5,18 @@
     <div
       class="col-start-1 col-span-full row-start-1 row-span-1 w-full h-full bg-[#151618] rounded-md p-1 flex justify-center items-center"
     >
-      <div class="w-full h-full rounded-sm bg-red-500 flex justify-center items-center">
-        <span class="text-2xs text-center text-gray-100 font-semibold font-sans uppercase mt-1">Return</span>
+      <div class="w-full h-full rounded-sm flex justify-center items-center">
+        <span
+          class="text-sm text-center text-gray-100 font-normal font-sans uppercase mt-1"
+          >Select A Network</span
+        >
       </div>
     </div>
     <div
       class="col-start-1 col-span-full row-start-2 row-end-14 w-full h-full bg-[#1d1f21] rounded-md grid grid-cols-2 grid-rows-12"
     >
-      <div class="col-start-1 col-span-full row-start-1 row-end-4">
-        <span>Add A Setup</span>
-      </div>
       <div
-        class="col-start-1 col-span-full row-start-4 row-span-full bg-black rounded-md flex flex-col justify-start items-center p-1 overflow-x-hidden overflow-y-auto space-y-1 border border-gray-700"
+        class="col-start-1 col-span-full row-start-1 row-span-full bg-black rounded-b-md flex flex-col justify-start items-center p-1 overflow-x-hidden overflow-y-auto space-y-1 border border-gray-700 scrollbar scrollbar-thumb-gray-500 scrollbar-track-gray-800"
       >
         <div
           v-for="network in manageStore.networkList"
@@ -24,8 +24,13 @@
           class="w-full h-7 bg-[#282a2c] rounded-sm border border-gray-500 items-center mx-auto cursor-pointer hover:bg-gray-700 transition-all duration-100 grid grid-cols-5"
           @click="getNetwork(network)"
         >
-          <img class="col-start-1 col-span-1 w-5 self-center mx-auto" :src="network.icon" alt="Network" />
-          <span class="col-start-2 col-span-full text-left text-xs font-sans text-gray-200 ml-1"
+          <img
+            class="col-start-1 col-span-1 w-5 self-center mx-auto"
+            :src="network.icon"
+            alt="Network"
+          />
+          <span
+            class="col-start-2 col-span-full text-left text-xs font-sans text-gray-200 ml-1"
             >{{ network?.name }}
           </span>
         </div>
@@ -35,7 +40,10 @@
     <div
       class="col-start-1 col-span-full row-start-14 row-span-full w-full h-full bg-[#151618] rounded-md flex flex-col justify-between items-center p-1 shadow-sm shadow-black active:shadow-none border border-gray-700"
     >
-      <span class="text-2xs text-center text-gray-100 font-semibold font-sans uppercase mt-1">Create Custom Setup</span>
+      <span
+        class="text-2xs text-center text-gray-100 font-semibold font-sans uppercase mt-1"
+        >Create Custom Setup</span
+      >
 
       <div
         class="w-full h-8 bg-teal-700 rounded-sm text-center p-1 cursor-pointer hover:bg-teal-900 transition-all duration-100"
