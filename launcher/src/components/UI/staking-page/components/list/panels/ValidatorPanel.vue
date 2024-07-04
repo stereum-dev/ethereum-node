ae4a98b94b3b69f2278d5c4eefe7cda2ffec9f46
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2024-07-04 23:03:55 +0200

    ADD: setup color to its validators

--- launcher/src/components/UI/staking-page/components/list/panels/ValidatorPanel.vue
+++ launcher/src/components/UI/staking-page/components/list/panels/ValidatorPanel.vue
@@ -6,11 +6,16 @@
       <div
         v-for="service in runningValidators"
         :key="service.config?.serviceID"
-        class="col-span-1 colst w-24 h-full flex justify-start items-center bg-[#a7aeb5] hover:bg-slate-300 rounded-sm space-x-1 cursor-pointer transition-all duration-150 px-1"
+        class="col-span-1 colst w-24 h-full flex justify-start items-center hover:bg-slate-300 rounded-sm space-x-1 cursor-pointer transition-all duration-150 px-1"
+        :class="getBgColor(service.setupId).background"
         @click="pickValidator(service)"
       >
         <img class="w-5 h-5" :src="service.icon" alt="Service Icon" @mousedown.prevent />
-        <span class="text-[10px] text-gray-700 font-semibold">{{ service.name }}</span>
+        <span
+          class="text-[10px] font-semibold hover:text-black transition-all duration-150"
+          :class="getBgColor(service.setupId).text"
+          >{{ service.name }}</span
+        >
       </div>
     </div>
   </div>
@@ -18,13 +23,34 @@
 <script setup>
 import { computed } from "vue";
 import { useServices } from "@/store/services";
+import { useSetups } from "@/store/setups";
 
 const emit = defineEmits(["pickValidator"]);
 const serviceStore = useServices();
+const setupStore = useSetups();
 
 const runningValidators = computed(() => {
-  return serviceStore.installedServices.filter((s) => s.category === "validator" && s.state === "running");
+  let validatores;
+  if (setupStore.selectedSetup !== null) {
+    validatores = serviceStore.installedServices.filter(
+      (s) =>
+        s.category === "validator" &&
+        s.state === "running" &&
+        s.setupId === setupStore.selectedSetup?.setupId
+    );
+  } else {
+    validatores = serviceStore.installedServices.filter(
+      (s) => s.category === "validator" && s.state === "running"
+    );
+  }
+  return validatores;
 });
+
+const getBgColor = (setupId) => {
+  const currentSetup = setupStore.allSetups.find((s) => s.setupId === setupId);
+  const setupColor = currentSetup?.color;
+  return setupStore.colorMappings[setupColor];
+};
 
 const pickValidator = (service) => {
   emit("pickValidator", service);
