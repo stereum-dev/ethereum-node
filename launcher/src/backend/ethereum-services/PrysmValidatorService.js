b3d57dd4fc419d4bc399f0aa0859133e30ca3ea3
NeoPlays <80448387+NeoPlays@users.noreply.github.com>
2024-07-22 11:40:53 +0200

    FIX: Prysm Validator Import for existing Setups

--- launcher/src/backend/ethereum-services/PrysmValidatorService.js
+++ launcher/src/backend/ethereum-services/PrysmValidatorService.js
@@ -40,21 +40,22 @@ export class PrysmValidatorService extends NodeService {
       image, //image
       "v3.1.1", //imageVersion
       '/app/cmd/validator/validator --accept-terms-of-use=true --beacon-rpc-provider="' +
-        provider +
-        '" --beacon-rpc-gateway-provider="' +
-        providerGateway +
-        '" --web --' +
-        network +
-        "=true --datadir=" +
-        dataDir +
-        " --wallet-dir=" +
-        walletDir +
-        " --wallet-password-file=" +
-        passwordDir +
-        '/wallet-password --monitoring-host=0.0.0.0 --grpc-gateway-port=7500 --grpc-gateway-host=0.0.0.0 --grpc-gateway-corsdomain="*"  --monitoring-host=0.0.0.0 --monitoring-port=8081 --suggested-fee-recipient=0x0000000000000000000000000000000000000000' +
-        " --graffiti-file=" +
-        graffitiDir +
-        "/graffitis.yaml --enable-builder=true --enable-doppelganger=true", //command
+      provider +
+      '" --beacon-rpc-gateway-provider="' +
+      providerGateway +
+      '" --web --' +
+      network +
+      "=true --datadir=" +
+      dataDir +
+      " --keymanager-token-file=" + walletDir + "/auth-token" +
+      " --wallet-dir=" +
+      walletDir +
+      " --wallet-password-file=" +
+      passwordDir +
+      '/wallet-password --monitoring-host=0.0.0.0 --grpc-gateway-port=7500 --grpc-gateway-host=0.0.0.0 --grpc-gateway-corsdomain="*"  --monitoring-host=0.0.0.0 --monitoring-port=8081 --suggested-fee-recipient=0x0000000000000000000000000000000000000000' +
+      " --graffiti-file=" +
+      graffitiDir +
+      "/graffitis.yaml --enable-builder=true --enable-doppelganger=true", //command
       null, // entrypoint
       null, // env
       ports, //ports
