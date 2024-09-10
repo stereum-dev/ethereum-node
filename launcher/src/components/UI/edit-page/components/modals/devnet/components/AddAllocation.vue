8f4bef3e0d4d74ab1b0ddea1299418dbb9799940
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2024-09-10 15:55:08 +0200

    REFACTOR: clean devnet components

--- launcher/src/components/UI/edit-page/components/modals/devnet/components/AddAllocation.vue
+++ launcher/src/components/UI/edit-page/components/modals/devnet/components/AddAllocation.vue
@@ -22,8 +22,16 @@
 
         <!-- Checkbox for Account Script -->
         <div class="w-full flex items-center py-1">
-          <label for="addAccountScript" class="text-sm text-gray-200 cursor-pointer flex items-center">
-            <input id="addAccountScript" v-model="addAccountScript" type="checkbox" class="mr-2 w-4 h-4" />
+          <label
+            for="addAccountScript"
+            class="text-sm text-gray-200 cursor-pointer flex items-center"
+          >
+            <input
+              id="addAccountScript"
+              v-model="addAccountScript"
+              type="checkbox"
+              class="mr-2 w-4 h-4"
+            />
             Enable Advanced Account Configuration
           </label>
         </div>
@@ -39,9 +47,10 @@
 
         <!-- Display Added Account Details -->
         <div v-else-if="Object.keys(allocData).length > 0" class="w-full">
-          <pre class="w-full h-[100px] p-2 bg-black text-amber-300 rounded-md text-sm overflow-auto max-h-[200px]">{{
-            JSON.stringify(allocData, null, 2)
-          }}</pre>
+          <pre
+            class="w-full h-[100px] p-2 bg-black text-amber-300 rounded-md text-sm overflow-auto max-h-[200px]"
+            >{{ JSON.stringify(allocData, null, 2) }}</pre
+          >
         </div>
 
         <!-- Add Account Button -->
@@ -83,7 +92,10 @@ const placeHolderSample = ref({
 const allocData = ref({});
 
 const buttonDisabled = computed(() => {
-  return (!addAccountScript.value && !accountAddress.value) || (addAccountScript.value && !accountDetails.value);
+  return (
+    (!addAccountScript.value && !accountAddress.value) ||
+    (addAccountScript.value && !accountDetails.value)
+  );
 });
 
 const updateAllocData = () => {
@@ -93,7 +105,9 @@ const updateAllocData = () => {
 watch(() => genesisStore.genesis.alloc, updateAllocData, { immediate: true });
 
 const inputClass = (disabled) => {
-  return `w-full h-[35px] text-sm font-semibold pl-2 rounded-md ${disabled ? "bg-zinc-400 text-gray-600" : "bg-zinc-200 text-gray-800"}`;
+  return `w-full h-[35px] text-sm font-semibold pl-2 rounded-md ${
+    disabled ? "bg-zinc-400 text-gray-600" : "bg-zinc-200 text-gray-800"
+  }`;
 };
 
 const addAccount = () => {
@@ -108,12 +122,9 @@ const addAccount = () => {
 
   if (addAccountScript.value) {
     try {
-      // First, try to parse as JSON
       newAccount = JSON.parse(accountDetails.value);
     } catch (jsonError) {
-      // If JSON parsing fails, try to evaluate as a JavaScript object
       try {
-        // Use Function constructor to safely evaluate the string as a JavaScript object
         newAccount = new Function(`return ${accountDetails.value}`)();
         if (typeof newAccount !== "object" || newAccount === null) {
           throw new Error("Invalid object");
@@ -127,8 +138,6 @@ const addAccount = () => {
   } else {
     newAccount[accountAddress.value] = { balance: balance.value || "0x0" };
   }
-
-  // Update the store with the new account(s)
   Object.keys(newAccount).forEach((address) => {
     genesisStore.updateGenesisAlloc({ [address]: newAccount[address] });
   });
