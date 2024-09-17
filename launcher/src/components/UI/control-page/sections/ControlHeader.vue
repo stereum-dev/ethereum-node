0e63c1720b56d8d861cd0e514aa79a6036072905
mabasian <54101509+mabasian@users.noreply.github.com>
2024-09-17 16:29:30 +0200

    ADD: setup services widget

--- launcher/src/components/UI/control-page/sections/ControlHeader.vue
+++ launcher/src/components/UI/control-page/sections/ControlHeader.vue
@@ -1,24 +1,32 @@
 <template>
-  <div class="control-header col-start-1 col-end-25 bg-red-500 row-start-1 row-span-1 grid grid-cols-24 grid-rows-1 h-full p-1 gap-1">
+  <div class="control-header col-start-1 col-end-25 row-start-1 row-span-1 grid grid-cols-24 grid-rows-1 h-full p-1 gap-1">
     <ThinCard class="machine-name col-start-3 col-span-5"><MachineName /></ThinCard>
     <NetworkStatus class="network-status col-start-8 col-end-12" />
-
     <SetupDetails class="SetupDetails col-start-12 col-span-5" :list="setupsList" @select-setup="selectSetup" @server-view="serverView" />
+    <ThinCard class="setup-services col-start-17 col-span-6"><SetupServices /></ThinCard>
   </div>
 </template>
 
 <script setup>
+import { computed, watch } from "vue";
+
+import { useSetups } from "@/store/setups";
+import { useServices } from "@/store/services";
+import { useControlStore } from "@/store/theControl";
+
+import { useMultiSetups } from "@/composables/multiSetups";
+
 import ThinCard from "../components/cards/ThinCard.vue";
 import MachineName from "../components/widgets/MachineName.vue";
 import NetworkStatus from "../../../layers/NetworkStatus.vue";
 import SetupDetails from "../../edit-page/components/edit/header/SetupDetails.vue";
-import { computed } from "vue";
-import { useSetups } from "@/store/setups";
-import { useMultiSetups } from "@/composables/multiSetups";
+import SetupServices from "../../control-page/components/widgets/SetupServices.vue";
 
 const { getSelectedSetup, getServerView } = useMultiSetups();
 
 const setupStore = useSetups();
+const serviceStore = useServices();
+const controlStore = useControlStore();
 
 const setupsList = computed(() => {
   return setupStore.allSetups;
@@ -32,11 +40,37 @@ const serverView = () => {
   getServerView();
 };
 
-// onUnmounted(() => {
-//   getServerView();
-// });
+const selectedConfigServices = computed(() => {
+  let test = [];
+  const selectedSetup = setupStore.selectedSetup;
 
-// const serverView = () => {
-//   getServerView();
-// };
+  if (selectedSetup && selectedSetup.services) {
+    const selectedServiceIds = selectedSetup.services.map((service) => service.config.serviceID);
+
+    serviceStore.installedServices.forEach((service) => {
+      if (
+        (["execution", "validator", "consensus"].includes(service.category) && selectedServiceIds.includes(service.config.serviceID)) ||
+        service.category === "service"
+      ) {
+        test.push({
+          isServicePending: false,
+          ...service,
+        });
+      }
+    });
+  }
+  return test;
+});
+
+watch(
+  () => setupStore.selectedSetup,
+  () => {
+    if (selectedConfigServices.value && selectedConfigServices.value.length === 0) {
+      controlStore.setupServices = 0;
+    } else {
+      controlStore.setupServices = selectedConfigServices.value.length;
+    }
+  },
+  { immediate: true }
+);
 </script>
--- launcher/src/store/theControl.js
+++ launcher/src/store/theControl.js
@@ -3,6 +3,7 @@ import { defineStore } from "pinia";
 export const useControlStore = defineStore("theControl", {
   state: () => {
     return {
+      setupServices: 0,
       currentConsensusIcon: "",
       currentExecutionIcon: "",
       synchronizationError: false,
