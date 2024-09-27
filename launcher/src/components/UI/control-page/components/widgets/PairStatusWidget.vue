320a4230b5d4552d30fe132466ba467f3f6dad8e
mabasian <54101509+mabasian@users.noreply.github.com>
2024-09-27 16:25:34 +0200

    ADD: pair status widget

---
+++ launcher/src/components/UI/control-page/components/widgets/PairStatusWidget.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="pair-state-parent w-full h-full flex flex-col justify-center items-center">
+    <div class="index-line w-full h-1/2 flex justify-center items-center pl-2">
+      <div class="icon-box w-1/4 h-full flex justify-center items-center">
+        <img class="w-7 h-7" src="/img/icon/service-icons/execution/Geth.png" alt="execution" />
+      </div>
+      <div class="status-box w-3/4 h-full flex justify-center items-center uppercase text-xs text-green-500 font-semibold">
+        {{ (state = "online") }}
+      </div>
+    </div>
+    <div class="index-line w-full h-1/2 flex justify-center items-center pl-2">
+      <div class="icon-box w-1/4 h-full flex justify-center items-center">
+        <img class="w-7 h-7" src="/img/icon/service-icons/consensus/LightHouse.png" alt="execution" />
+      </div>
+      <div class="status-box w-3/4 h-full flex justify-center items-center uppercase text-xs text-red-500 font-semibold">
+        {{ (state = "offline") }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const state = ref("");
+</script>
+
+<style scoped></style>
--- launcher/src/components/UI/control-page/sections/SidebarRight.vue
+++ launcher/src/components/UI/control-page/sections/SidebarRight.vue
@@ -3,7 +3,9 @@
     class="w-full h-full col-start-22 col-end-25 row-start-2 row-span-full grid grid-cols-3 grid-rows-15 items-center gap-1 pt-2 pb-1 pr-1"
   >
     <MiniCard class="col-start-1 col-span-3 row-start-1 row-span-3"><ClientPairWidget /></MiniCard>
-    <MiniCard v-if="controlStore.pickeedService === 'exeCons'" class="col-start-1 col-span-3 row-start-4 row-span-3" />
+    <MiniCard v-if="controlStore.pickeedService === 'exeCons'" class="col-start-1 col-span-3 row-start-4 row-span-3"
+      ><PairStatusWidget
+    /></MiniCard>
     <MiniCard v-if="controlStore.pickeedService === 'exeCons'" class="col-start-1 col-span-3 row-start-7 row-span-3" />
     <MiniCard v-if="controlStore.pickeedService === 'exeCons'" class="col-start-1 col-span-3 row-start-10 row-span-3" />
   </div>
@@ -12,6 +14,7 @@
 <script setup>
 import MiniCard from "../components/cards/MiniCard.vue";
 import ClientPairWidget from "../components/widgets/ClientPairWidget.vue";
+import PairStatusWidget from "../components/widgets/PairStatusWidget.vue";
 
 import { useControlStore } from "@/store/theControl";
 
