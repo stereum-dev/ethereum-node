eb94cd8380f423ea04f55bab444abc4bc8440b1a
mabasian <54101509+mabasian@users.noreply.github.com>
2024-09-30 11:31:56 +0200

    update: stereum format

--- launcher/src/components/UI/control-page/components/widgets/VolumeWidget.vue
+++ launcher/src/components/UI/control-page/components/widgets/VolumeWidget.vue
@@ -4,15 +4,10 @@
       <div class="volume-Ico-container flex justify-center items-center w-full h-4/5">
         <img class="w-3/4" src="/img/icon/control-page-icons/hdd.svg" />
       </div>
-      <span
-        class="w-full h-1/5 flex justify-center items-center text-gray-200 text-2xs font-semibold uppercase"
-        >volume</span
-      >
+      <span class="w-full h-1/5 flex justify-center items-center text-gray-200 text-2xs font-semibold uppercase">volume</span>
     </div>
     <div class="volume-box w-2/3 h-full flex flex-col justify-center items-center p-1">
-      <div
-        class="volume_services flex w-full h-1/3 border rounded-sm border-gray-500 overflow-hidden relative"
-      >
+      <div class="volume_services flex w-full h-1/3 border rounded-sm border-gray-500 overflow-hidden relative">
         <div
           v-for="item in storagestatus"
           :key="item.id"
@@ -26,49 +21,23 @@
       </div>
 
       <div class="hdd-services w-full h-2/3 flex">
-        <div
-          class="left-side w-2/3 h-full flex justify-center items-center text-[45%] text-green-500 uppercase"
-        >
+        <div class="left-side w-2/3 h-full flex justify-center items-center text-[45%] text-green-500 uppercase">
           <span>{{ availDisk }} GB {{ $t("controlPage.free") }} </span
-          ><span class="text-gray-400">
-            / {{ totalDisk }} GB {{ $t("controlPage.total") }}</span
-          >
+          ><span class="text-gray-400"> / {{ totalDisk }} GB {{ $t("controlPage.total") }}</span>
         </div>
         <div class="right-side w-1/3 h-full flex justify-center items-center uppercase">
           <div class="ttl-box w-1/4 h-full flex flex-col justify-center items-center">
-            <div
-              class="title w-full text-[50%] h-1/2 flex justify-center items-center text-gray-200 uppercase mr-1"
-            >
-              write
-            </div>
-            <div
-              class="title w-full text-[50%] h-1/2 flex justify-center items-center text-gray-200 uppercase mr-1"
-            >
-              read
-            </div>
+            <div class="title w-full text-[50%] h-1/2 flex justify-center items-center text-gray-200 uppercase mr-1">write</div>
+            <div class="title w-full text-[50%] h-1/2 flex justify-center items-center text-gray-200 uppercase mr-1">read</div>
           </div>
-          <div
-            class="val-box w-3/4 h-full flex flex-col text-[70%] justify-center font-semibold items-center"
-          >
-            <div
-              class="title w-full h-1/2 flex justify-center items-center text-orange-500 uppercase"
-            >
+          <div class="val-box w-3/4 h-full flex flex-col text-[70%] justify-center font-semibold items-center">
+            <div class="title w-full h-1/2 flex justify-center items-center text-orange-500 uppercase">
               {{ convertWriteValueToMb }}
-              {{
-                controlStore.writeValue / 1024 < 1 && controlStore.writeValue / 1024 > 0
-                  ? "KB"
-                  : "MB"
-              }}
+              {{ controlStore.writeValue / 1024 < 1 && controlStore.writeValue / 1024 > 0 ? "KB" : "MB" }}
             </div>
-            <div
-              class="title w-full h-1/2 flex justify-center items-center text-teal-700 uppercase"
-            >
+            <div class="title w-full h-1/2 flex justify-center items-center text-teal-700 uppercase">
               {{ convertReadValueToMb }}
-              {{
-                controlStore.readValue / 1024 < 1 && controlStore.writeValue / 1024 > 0
-                  ? "KB"
-                  : "MB"
-              }}
+              {{ controlStore.readValue / 1024 < 1 && controlStore.writeValue / 1024 > 0 ? "KB" : "MB" }}
             </div>
           </div>
         </div>
@@ -102,12 +71,8 @@ const storagestatus = computed(() => {
 
     const percentage = (valueInGB / totalDisk.value) * 100;
 
-    const matchingService = serviceStore.installedServices.find(
-      (service) => service.service === item.service
-    );
-    const serviceIcon = matchingService
-      ? matchingService.icon
-      : "/path/to/default/icon.png";
+    const matchingService = serviceStore.installedServices.find((service) => service.service === item.service);
+    const serviceIcon = matchingService ? matchingService.icon : "/path/to/default/icon.png";
 
     return {
       ...item,
