992695448a2ac39994963fbb83ecb1c9ed199a30
NeoPlays <80448387+NeoPlays@users.noreply.github.com>
2024-11-11 19:34:20 +0700

    ADD: CSM Queue Check

--- launcher/src/backend/web3/CSM.js
+++ launcher/src/backend/web3/CSM.js
@@ -1,31 +1,15 @@
-const fs = require('fs');
-const Web3 = require('web3').default;
+const Web3 = require("web3").default;
+const csm_abi = require("./CSModuleABI.json");
+const log = require("electron-log");
 
-// Check if promiseIpc is globally available in the window scope
-let promiseIpc;
-if (typeof window !== "undefined" && window.promiseIpc) {
-  promiseIpc = window.promiseIpc;
-} else {
-  console.error("promiseIpc is not available globally. Ensure it is imported correctly.");
-}
-
-let csm_abi;
-try {
-  csm_abi = JSON.parse(fs.readFileSync('CSModuleABI.json', 'utf8'));
-  console.log("Loaded csm_abi successfully");
-} catch (error) {
-  console.error('Error loading the contract ABI:', error);
-}
-
-const w3 = new Web3('http://127.0.0.1:8545');
-let csm_contract_address;
+const w3 = new Web3("http://127.0.0.1:8545");
 
 async function isConnected() {
   try {
     await w3.eth.getBlockNumber();
     return true; // Connection successful
   } catch (error) {
-    console.error('Error connecting to Ethereum network:', error);
+    log.error("Error connecting to Ethereum network:", error);
     return false; // Connection failed
   }
 }
@@ -34,7 +18,7 @@ async function getChainId() {
   try {
     return await w3.eth.getChainId();
   } catch (error) {
-    console.error('Error retrieving chain ID:', error);
+    log.error("Error retrieving chain ID:", error);
     return null;
   }
 }
@@ -45,15 +29,15 @@ async function getContractAddress() {
     if (chainId !== null) {
       switch (Number(chainId)) {
         case 1:
-          console.log("Connected to Mainnet. ID =", chainId.toString());
+          log.info("Connected to Mainnet. ID =", chainId.toString());
           return "0xdA7dE2ECdDfccC6c3AF10108Db212ACBBf9EA83F";
 
         case 17000:
-          console.log("Connected to Holesky. ID =", chainId.toString());
+          log.info("Connected to Holesky. ID =", chainId.toString());
           return "0x4562c3e63c2e586cD1651B958C22F88135aCAd4f";
 
         default:
-          console.log("Connected to unsupported network. ID =", chainId.toString());
+          log.info("Connected to unsupported network. ID =", chainId.toString());
           return null;
       }
     }
@@ -65,14 +49,14 @@ async function getSyncStatus() {
   try {
     const syncStatus = await w3.eth.isSyncing();
     if (syncStatus === false) {
-    	console.log("Node is in sync ... ");
+      log.info("Node is in sync ... ");
       return true; // Node is in sync
     } else {
-    	console.log("Node is currently syncing ... ");
+      log.info("Node is currently syncing ... ");
       return false; // Node is syncing
     }
   } catch (error) {
-    console.error('Error checking sync status:', error);
+    log.error("Error checking sync status:", error);
     return null;
   }
 }
@@ -81,11 +65,11 @@ async function getContract() {
   try {
     const contractAddress = await getContractAddress();
     if (!csm_abi || !contractAddress) {
-      throw new Error('Missing ABI or contract address.');
+      throw new Error("Missing ABI or contract address.");
     }
     return new w3.eth.Contract(csm_abi, contractAddress);
   } catch (error) {
-    console.error('Error initializing contract:', error);
+    log.error("Error initializing contract:", error);
     return null;
   }
 }
@@ -93,13 +77,13 @@ async function getContract() {
 async function isNodeOperatorActive(contract, nodeOperatorId) {
   try {
     if (!contract) {
-      throw new Error('Contract is not initialized.');
+      throw new Error("Contract is not initialized.");
     }
     const isActive = await contract.methods.getNodeOperatorIsActive(nodeOperatorId).call();
-    console.log("Node Operator is active:", isActive);
+    log.info("Node Operator is active:", isActive);
     return isActive;
   } catch (error) {
-    console.error('Error calling getNodeOperatorIsActive:', error);
+    log.error("Error calling getNodeOperatorIsActive:", error);
     return null;
   }
 }
@@ -107,37 +91,37 @@ async function isNodeOperatorActive(contract, nodeOperatorId) {
 async function getNodeOperatorInfo(contract, nodeOperatorId) {
   try {
     if (!contract) {
-      throw new Error('Contract is not initialized.');
+      throw new Error("Contract is not initialized.");
     }
 
     const nodeOperator = await contract.methods.getNodeOperator(nodeOperatorId).call();
     const enqueuedCount = nodeOperator.enqueuedCount;
 
-    //console.log("Enqueued Count:", enqueuedCount);
+    //log.info("Enqueued Count:", enqueuedCount);
     return enqueuedCount;
   } catch (error) {
-    console.error('Error calling getNodeOperator:', error);
+    log.error("Error calling getNodeOperator:", error);
     return null;
   }
 }
 
 async function getNoneWithdrawnKeys(contract, nodeOperatorId) {
-	try {
-		if(!contract) {
-			throw new Error('Contract is not initialized.');
-			}
-	const noneWithdrawnKeys = await contract.methods.getNodeOperatorNonWithdrawnKeys(nodeOperatorId).call();
-	return noneWithdrawnKeys;
-	} catch (error) {
-		console.error('Error calling getNodeOperatorNonWithdrawnKeys: ', error);
-		return null;
-		}
+  try {
+    if (!contract) {
+      throw new Error("Contract is not initialized.");
+    }
+    const noneWithdrawnKeys = await contract.methods.getNodeOperatorNonWithdrawnKeys(nodeOperatorId).call();
+    return noneWithdrawnKeys;
+  } catch (error) {
+    log.error("Error calling getNodeOperatorNonWithdrawnKeys: ", error);
+    return null;
+  }
 }
 
 async function getSigningKeys(contract, nodeOperatorId, startIndex, keysCount) {
   try {
     if (!contract) {
-      throw new Error('Contract is not initialized.');
+      throw new Error("Contract is not initialized.");
     }
 
     // Call getSigningKeys on the contract
@@ -152,22 +136,22 @@ async function getSigningKeys(contract, nodeOperatorId, startIndex, keysCount) {
       keysArray.push("0x" + signingKeysHex.slice(i, i + KEY_LENGTH));
     }
 
-    //console.log("Signing Keys Array:", keysArray);
+    //log.info("Signing Keys Array:", keysArray);
     return keysArray;
   } catch (error) {
-    console.error('Error calling getSigningKeys:', error);
+    log.error("Error calling getSigningKeys:", error);
     return null;
   }
 }
 
 /**
  * Checks for matching signing keys of a specified Node Operator.
- * 
+ *
  * This function opens an RPC tunnel, verifies that the Ethereum node is in sync,
- * checks the activity status of the given Node Operator, retrieves their non-withdrawn signing keys, 
- * and compares them to an array of provided keys. If matches are found, an array of matching keys 
+ * checks the activity status of the given Node Operator, retrieves their non-withdrawn signing keys,
+ * and compares them to an array of provided keys. If matches are found, an array of matching keys
  * is returned. If no matches are found, it returns `false`. In case of an error, it returns `null`.
- * 
+ *
  * @async
  * @function checkSigningKeys
  * @param {number} nodeOperatorId - The ID of the Node Operator whose signing keys are to be checked.
@@ -176,66 +160,72 @@ async function getSigningKeys(contract, nodeOperatorId, startIndex, keysCount) {
  * - An array of matching signing keys (in hexadecimal format) if any matches are found.
  * - `false` if no matches are found.
  * - `null` if an error occurs during the process.
- * 
+ *
  * @example
  * const nodeOperatorId = 43;
  * const keysArray = ["0xabc123...", "0xdef456...", "0x789xyz..."];
  * const matches = await checkSigningKeys(nodeOperatorId, keysArray);
  * if (matches === false) {
- *   console.log("No matching keys found.");
+ *   log.info("No matching keys found.");
  * } else if (matches === null) {
- *   console.error("An error occurred during the signing keys check.");
+ *   log.error("An error occurred during the signing keys check.");
  * } else {
- *   console.log("Matching keys:", matches);
+ *   log.info("Matching keys:", matches);
  * }
- * 
+ *
  * @throws {Error} Logs and returns `null` if:
  * - The RPC tunnel could not be opened.
  * - The contract or other necessary data could not be retrieved.
  * - Any other unexpected error occurs.
  */
-async function checkSigningKeys(nodeOperatorId, keysArray) {
-
-  if (!promiseIpc) {
-    console.error("promiseIpc is not initialized. Cannot open RPC tunnel.");
-    return null;
-  }
-
+async function checkSigningKeys(keysArray, monitoring) {
   try {
-    console.log("Opening RPC tunnel...");
-    await promiseIpc.send("openRpcTunnel");
+    log.info("Opening RPC tunnel...");
+    await monitoring.openRpcTunnel();
+
+    log.info("Get Node Operator ID from LCOM");
+    const lcomServices = await monitoring.getServiceInfos("LCOMService");
+    if (lcomServices.length < 1) {
+      throw new Error("LCOM service not found");
+    }
+    const nodeOperatorId = lcomServices.find((s) => s.config.env.NO_ID).config.env.NO_ID;
+    if (!nodeOperatorId) {
+      throw new Error("Node Operator ID not found in LCOM Config");
+    }
+    log.info("Node Operator ID:", nodeOperatorId);
+
     // Initialize the contract
     const contract = await getContract();
     if (!contract) {
-      console.error("Failed to initialize contract.");
+      log.error("Failed to initialize contract.");
       return null;
     }
 
     // Check if the node is in sync
     const isSynced = await getSyncStatus();
     if (!isSynced) {
-      console.log("Node is currently syncing...");
+      log.info("Node is currently syncing...");
       return null;
     }
 
     // Check if the Node Operator is active
     const isActive = await isNodeOperatorActive(contract, nodeOperatorId);
     if (!isActive) {
-      console.log("Node Operator is not active.");
+      log.info("Node Operator is not active.");
       return false;
     }
 
     // Retrieve enqueuedCount and ensure it's above 0
     const enqueuedCount = await getNodeOperatorInfo(contract, nodeOperatorId);
     if (enqueuedCount === null || enqueuedCount <= 0) {
-      console.log("No enqueued validators for this Node Operator.");
+      log.info("No enqueued validators for this Node Operator.");
       return false;
     }
 
     // Get the number of non-withdrawn keys for this node operator
     const numberOfNoneWithdrawnKeys = await getNoneWithdrawnKeys(contract, nodeOperatorId);
     if (numberOfNoneWithdrawnKeys === null || numberOfNoneWithdrawnKeys <= 0) {
-      console.log("No non-withdrawn keys available.");
+      log.info("No non-withdrawn keys available.");
       return false;
     }
 
@@ -246,25 +236,22 @@ async function checkSigningKeys(nodeOperatorId, keysArray) {
     }
 
     // Check for matches with the provided keys array
-    const matchingKeys = signingKeys.filter(key => keysArray.includes(key));
+    const matchingKeys = signingKeys.filter((key) => keysArray.includes(key));
 
     if (matchingKeys.length > 0) {
-      //console.log("Matching keys found:", matchingKeys);
+      //log.info("Matching keys found:", matchingKeys);
       return matchingKeys;
     } else {
-      console.log("No matching keys found.");
+      log.info("No matching keys found.");
       return false;
     }
   } catch (error) {
-    console.error("Error in checkSigningKeys:", error);
+    log.error("Error in checkSigningKeys:", error);
     return null;
-  }
-  finally {
-    console.log("Closing RPC tunnel...");
-    await promiseIpc.send("closeRpcTunnel");
+  } finally {
+    log.info("Closing RPC tunnel...");
+    await monitoring.closeRpcTunnel();
   }
 }
 
-module.exports = {
-  checkSigningKeys
-};
+export default checkSigningKeys;
