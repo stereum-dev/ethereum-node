3e3d4c4f5dde46ce91ab15919606feaed18297af
NeoPlays <80448387+NeoPlays@users.noreply.github.com>
2024-12-04 11:31:40 +0100

    fix format

--- launcher/src/components/UI/staking-page/components/list/panels/InsertPanel.vue
+++ launcher/src/components/UI/staking-page/components/list/panels/InsertPanel.vue
@@ -2,15 +2,10 @@
   <div
     class="animate__animated animate__fadeIn w-full h-full max-h-[32px] col-start-1 col-span-full rounded-sm flex justify-center items-center cursor-pointer shadow-md shadow-gray-800"
     :class="
-      isUploadDisabled || stakingStore.isStakingDisabled
-        ? 'opacity-25 pointer-events-none bg-[#243535]'
-        : 'bg-[#336666] hover:bg-[#234545]'
+      isUploadDisabled || stakingStore.isStakingDisabled ? 'opacity-25 pointer-events-none bg-[#243535]' : 'bg-[#336666] hover:bg-[#234545]'
     "
   >
-    <div
-      class="w-full h-full flex justify-evenly items-center"
-      @click="openUploadHandler"
-    >
+    <div class="w-full h-full flex justify-evenly items-center" @click="openUploadHandler">
       <input
         ref="fileInput"
         type="file"
@@ -20,15 +15,8 @@
         accept="application/json,text/plain"
         @change="getFileValue"
       />
-      <span class="text-md font-semibold text-gray-200">{{
-        t("insertValidator.insertText")
-      }}</span>
-      <img
-        class="w-6 h-6"
-        src="/img/icon/staking-page-icons/validator-import.svg"
-        alt="icon"
-        @mousedown.prevent
-      />
+      <span class="text-md font-semibold text-gray-200">{{ t("insertValidator.insertText") }}</span>
+      <img class="w-6 h-6" src="/img/icon/staking-page-icons/validator-import.svg" alt="icon" @mousedown.prevent />
     </div>
   </div>
 </template>
@@ -48,16 +36,11 @@ const fileInput = ref(null);
 
 // Check if there is a doppelganger key processing
 const isUploadDisabled = computed(() => {
-  return stakingStore.doppelgangerKeys.some(
-    (doppelKey) =>
-      doppelKey.serviceID === stakingStore.selectedServiceToFilter?.config?.serviceID
-  );
+  return stakingStore.doppelgangerKeys.some((doppelKey) => doppelKey.serviceID === stakingStore.selectedServiceToFilter?.config?.serviceID);
 });
 
 const openUploadHandler = () => {
-  let validator = serviceStore.installedServices.filter(
-    (s) => s.category === "validator"
-  );
+  let validator = serviceStore.installedServices.filter((s) => s.category === "validator");
   if (validator.length && validator.map((e) => e.state).includes("running")) {
     fileInput.value.click();
   }
--- launcher/src/components/UI/staking-page/components/list/rows/KeyRow.vue
+++ launcher/src/components/UI/staking-page/components/list/rows/KeyRow.vue
@@ -2,15 +2,10 @@ import { computed } from 'vue';
 <template>
   <div
     class="w-full h-8 rounded-full grid grid-cols-24 items-center p-1 cursor-pointer animate__animated animate__slideInLeft animate__delay-0.5s mt-1"
-    :class="[
-      props.item?.selected ? 'bg-blue-400 ' : 'bg-gray-700 ',
-      props.item?.showExitText ? 'bg-red-500 z-10' : '',
-    ]"
+    :class="[props.item?.selected ? 'bg-blue-400 ' : 'bg-gray-700 ', props.item?.showExitText ? 'bg-red-500 z-10' : '']"
     @click="selectKey(props.item)"
   >
-    <div
-      class="col-start-1 col-span-2 self-center overflow-hidden flex justify-start items-center space-x-2"
-    >
+    <div class="col-start-1 col-span-2 self-center overflow-hidden flex justify-start items-center space-x-2">
       <div
         class="w-6 h-6 rounded-full cursor-pointer p-[2px]"
         :class="setupStore.getBGColor(props.item?.color)"
@@ -24,13 +19,7 @@ import { computed } from 'vue';
           alt="Key Icon"
           @mousedown.prevent
         />
-        <img
-          v-else
-          class="w-full h-full"
-          src="/img/icon/staking-page-icons/key-icon.png"
-          alt="Key Icon"
-          @mousedown.prevent
-        />
+        <img v-else class="w-full h-full" src="/img/icon/staking-page-icons/key-icon.png" alt="Key Icon" @mousedown.prevent />
       </div>
       <div
         class="w-6 h-6 rounded-full cursor-pointer p-[2px]"
@@ -58,11 +47,9 @@ import { computed } from 'vue';
       @mouseenter="footerStore.cursorLocation = `${props.item.key}`"
       @mouseleave="footerStore.cursorLocation = ''"
     >
-      <span
-        class="text-center font-semibold text-[10px]"
-        :class="props.item?.selected ? 'text-gray-800' : 'text-gray-300'"
-        >{{ displayText }}</span
-      >
+      <span class="text-center font-semibold text-[10px]" :class="props.item?.selected ? 'text-gray-800' : 'text-gray-300'">{{
+        displayText
+      }}</span>
     </div>
 
     <img
@@ -103,10 +90,7 @@ import { computed } from 'vue';
       >{{ props.item.balance }}</span
     >
 
-    <div
-      class="h-full col-start-17 col-span-full bg-[#151618] rounded-full grid grid-cols-6 items-center"
-      @mousedown.prevent
-    >
+    <div class="h-full col-start-17 col-span-full bg-[#151618] rounded-full grid grid-cols-6 items-center" @mousedown.prevent>
       <div
         v-if="getValidatorClients.service !== 'LCOMService'"
         class="col-span-1 w-full h-full rounded-md justify-self-center flex justify-center items-center"
@@ -229,11 +213,9 @@ const t = i18n.global.t;
 //Key Status Icons
 const activeStatusIcon = "/img/icon/staking-page-icons/validator-state-active.png";
 const slashedStatusIcon = "/img/icon/staking-page-icons/validator-state-slashed.png";
-const depositStatusIcon =
-  "/img/icon/staking-page-icons/validator-state-not-deposited.png";
+const depositStatusIcon = "/img/icon/staking-page-icons/validator-state-not-deposited.png";
 const offlineStatusIcon = "/img/icon/staking-page-icons/validator-state-offline.png";
-const pendingStatusIcon =
-  "/img/icon/staking-page-icons/validator-state-in-activation-queue.png";
+const pendingStatusIcon = "/img/icon/staking-page-icons/validator-state-in-activation-queue.png";
 const exitedStatusIcon = "/img/icon/staking-page-icons/validator-state-exited.png";
 const apiProblems = "/img/icon/staking-page-icons/validator-state-unknown.png";
 const queuedStatusIcon = "/img/icon/staking-page-icons/csm-q.png";
@@ -370,9 +352,7 @@ const checkValidatorKeyType = computed(() => {
   const { item } = props;
   if (!item?.dvt) return null;
 
-  const service = serviceStore.installedServices.find(
-    (service) => service?.config?.serviceID === item?.validatorID
-  );
+  const service = serviceStore.installedServices.find((service) => service?.config?.serviceID === item?.validatorID);
 
   const serviceKeyTypes = {
     LCOMService: "csm",
