9d39f01ff449733421174f789881b52827f3bdc2
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2025-01-07 18:30:36 +0100

    FIX: validator panel distance

--- launcher/src/components/UI/staking-page/components/list/panels/ValidatorPanel.vue
+++ launcher/src/components/UI/staking-page/components/list/panels/ValidatorPanel.vue
@@ -2,16 +2,16 @@
   <div
     class="animate__animated animate__fadeIn w-full h-full max-h-[32px] col-start-1 col-span-full bg-[#3e4347] rounded-sm flex justify-center items-center cursor-pointer p-1"
   >
-    <div class="w-full h-full grid grid-cols-4 bg-[#171D22]">
+    <div class="w-full h-full grid grid-cols-7 bg-[#171D22]">
       <div
         v-for="service in runningValidators"
         :key="service.config?.serviceID"
-        class="col-span-1 colst w-24 h-full flex justify-start items-center hover:bg-slate-300 rounded-sm space-x-1 cursor-pointer transition-all duration-150 px-1"
+        class="col-span-1 w-22 h-full flex justify-start items-center hover:bg-slate-300 rounded-sm space-x-1 cursor-pointer transition-all duration-150 px-1 border border-gray-700"
         :class="getBgColor(service.setupId)?.background"
         @click="pickValidator(service)"
       >
-        <img class="w-5 h-5" :src="service.icon" alt="Service Icon" @mousedown.prevent />
-        <span class="text-[10px] font-semibold hover:text-black transition-all duration-150" :class="getBgColor(service.setupId)?.text">{{
+        <img class="w-4 h-4" :src="service.icon" alt="Service Icon" @mousedown.prevent />
+        <span class="text-[9px] font-medium hover:text-black transition-all duration-150" :class="getBgColor(service.setupId)?.text">{{
           service.name
         }}</span>
       </div>
