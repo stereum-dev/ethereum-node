69e7ab8d9e50fc5d00883b90a8aba09082ac172c
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2025-01-10 12:40:34 +0100

    CHANGE: node and edit sidebar buttons

--- launcher/src/components/UI/edit-page/sections/SidebarSection.vue
+++ launcher/src/components/UI/edit-page/sections/SidebarSection.vue
@@ -1,68 +1,87 @@
 <template>
-  <aside class="flex flex-col items-center w-18 h-full custom-gradient" @pointerdown.prevent.stop @mousedown.prevent.stop>
+  <aside
+    class="flex flex-col items-center w-18 h-full custom-gradient"
+    @pointerdown.prevent.stop
+    @mousedown.prevent.stop
+  >
     <div class="w-full grid grid-rows-3 mt-20 p-1 gap-y-5">
       <div
-        class="col-span-1 row-start-1 row-end-2 p-1 rounded-md text-gray-700 focus:outline-nones transition-colors duration-200 hover:bg-[#23272a] flex justify-center items-center"
-        @click="hoverRouter"
+        class="col-span-1 row-start-1 row-end-2 p-1 rounded-md text-gray-700 bg-[#23272a] flex justify-center items-center scale-90 hover:scale-105 active:scale-100 border border-[#23272a] hover:border-gray-500 duration-150 hover:shadow-md shadow-[#23272a] transition-all"
         @mouseenter="footerStore.cursorLocation = `${toNode}`"
         @mouseleave="footerStore.cursorLocation = ''"
       >
-        <img class="w-8 h-5" src="/img/icon/edit-node-icons/back-to-node.png" alt="Manage Icon" />
-      </div>
-      <Transition name="slide-fade">
         <router-link
-          v-if="routerHovered"
           to="/node"
-          class="w-fit h-9 absolute col-span-1 row-start-1 row-end-2 py-1 px-2 rounded-md bg-gray-700 border border-gray-500 flex justify-between items-center space-x-2 ml-1 transition duration-200 shadow-md shadow-[#23272a] z-50"
+          class="w-fit h-full flex justify-between items-center"
           @mouseleave="routerHovered = false"
         >
-          <img class="w-6 h-4 mr-1" src="/img/icon/edit-node-icons/back-to-node.png" alt="Manage Icon" />
-          <span class="text-sm text-gray-200 font-semibold">{{ $t("sidebarSect.toNode") }}</span>
+          <img
+            class="w-6 h-4 mr-1"
+            src="/img/icon/edit-node-icons/back-to-node.png"
+            alt="Manage Icon"
+          />
         </router-link>
-      </Transition>
+      </div>
+
       <button
         v-if="setupStore.isEditConfigViewActive && setupStore.selectedSetup"
-        class="col-span-1 row-start-2 row-end-3 p-1 rounded-md text-gray-700 focus:outline-nones transition-colors duration-200 hover:bg-[#23272a] flex justify-center items-center"
+        class="col-span-1 row-start-2 row-end-3 p-1 rounded-md text-gray-700 bg-[#23272a] flex justify-center items-center scale-90 hover:scale-105 active:scale-100 border border-[#23272a] hover:border-gray-500 duration-150 hover:shadow-md shadow-[#23272a] transition-all ease-in-out"
         :class="disabledForDevnet ? 'pointer-events-none opacity-50' : ''"
-        @click="hoverNetwork"
+        @click="networkModal"
         @mouseenter="footerStore.cursorLocation = `${setchNet}`"
         @mouseleave="footerStore.cursorLocation = ''"
       >
-        <img class="w-6" src="/img/icon/edit-node-icons/change-network.png" alt="Network" />
+        <img
+          class="w-6"
+          src="/img/icon/edit-node-icons/change-network.png"
+          alt="Network"
+        />
       </button>
-      <Transition name="slide-fade">
+      <!-- <Transition name="slide-fade">
         <button
           v-if="networkHovered"
           class="w-fit h-9 absolute row-start-2 row-end-3 py-1 px-2 rounded-md duration-200 bg-gray-700 border border-gray-500 flex justify-between items-center z-10 space-x-2 ml-1"
           :class="disabledForDevnet ? 'pointer-events-none opacity-50' : ''"
           @mouseleave="networkHovered = false"
-          @click="networkModal"
         >
-          <img class="w-4 mr-1" src="/img/icon/edit-node-icons/change-network.png" alt="Network Icon" />
+          <img
+            class="w-4 mr-1"
+            src="/img/icon/edit-node-icons/change-network.png"
+            alt="Network Icon"
+          />
           <span class="text-sm text-gray-200 font-semibold">{{ setchNet }}</span>
         </button>
-      </Transition>
+      </Transition> -->
 
       <button
         v-if="!setupStore.isEditConfigViewActive && setupStore.selectedSetup === null"
-        class="row-start-2 row-end-3 p-1 rounded-md text-gray-700 focus:outline-nones transition-colors duration-200 hover:bg-[#23272a] flex justify-center items-center"
-        @click="hoverNuke"
+        class="col-span-1 row-start-2 row-end-3 p-1 rounded-md text-gray-700 bg-[#23272a] flex justify-center items-center scale-90 hover:scale-105 active:scale-100 border border-[#23272a] hover:border-gray-500 duration-150 hover:shadow-md shadow-[#23272a] transition-all ease-in-out"
         @mouseenter="footerStore.cursorLocation = `${nukTheNud}`"
         @mouseleave="footerStore.cursorLocation = ''"
+        @click="nukeNode"
       >
-        <img class="w-6" src="/img/icon/edit-node-icons/nuke-node.png" alt="Export Icon" />
+        <img
+          class="w-6"
+          src="/img/icon/edit-node-icons/nuke-node.png"
+          alt="Export Icon"
+        />
       </button>
-      <Transition name="slide-fade">
+      <!-- <Transition name="slide-fade">
         <button
           v-if="nukeHovered"
           class="w-fit h-9 absolute row-start-2 row-end-3 py-1 px-2 rounded-md duration-200 bg-gray-700 border border-gray-500 flex justify-between items-center z-10 space-x-2 ml-1"
           @mouseleave="nukeHovered = false"
-          @click="nukeNode"
         >
-          <img class="w-4" src="/img/icon/edit-node-icons/nuke-node.png" alt="Export Icon" />
-          <span class="text-xs text-gray-200 font-semibold">{{ $t("sidebarSect.nukNod") }}</span>
+          <img
+            class="w-4"
+            src="/img/icon/edit-node-icons/nuke-node.png"
+            alt="Export Icon"
+          />
+          <span class="text-xs text-gray-200 font-semibold">{{
+            $t("sidebarSect.nukNod")
+          }}</span>
         </button>
-      </Transition>
+      </Transition> -->
     </div>
   </aside>
 </template>
@@ -79,8 +98,6 @@ const setchNet = t("sidebarSect.setchNet");
 const nukTheNud = t("sidebarSect.nukTheNud");
 
 const routerHovered = ref(false);
-const networkHovered = ref(false);
-const nukeHovered = ref(false);
 const footerStore = useFooter();
 const setupStore = useSetups();
 
@@ -98,32 +115,32 @@ const networkModal = () => {
   emit("networkModal");
 };
 
-const hoverRouter = () => {
-  if (routerHovered.value || networkHovered.value || nukeHovered.value) {
-    routerHovered.value = false;
-    networkHovered.value = false;
-    nukeHovered.value = false;
-  }
-  routerHovered.value = true;
-};
+// const hoverRouter = () => {
+//   if (routerHovered.value || networkHovered.value || nukeHovered.value) {
+//     routerHovered.value = false;
+//     networkHovered.value = false;
+//     nukeHovered.value = false;
+//   }
+//   routerHovered.value = true;
+// };
 
-const hoverNetwork = () => {
-  if (routerHovered.value || networkHovered.value || nukeHovered.value) {
-    routerHovered.value = false;
-    networkHovered.value = false;
-    nukeHovered.value = false;
-  }
-  networkHovered.value = true;
-};
+// const hoverNetwork = () => {
+//   if (routerHovered.value || networkHovered.value || nukeHovered.value) {
+//     routerHovered.value = false;
+//     networkHovered.value = false;
+//     nukeHovered.value = false;
+//   }
+//   networkHovered.value = true;
+// };
 
-const hoverNuke = () => {
-  if (routerHovered.value || networkHovered.value || nukeHovered.value) {
-    routerHovered.value = false;
-    networkHovered.value = false;
-    nukeHovered.value = false;
-  }
-  nukeHovered.value = true;
-};
+// const hoverNuke = () => {
+//   if (routerHovered.value || networkHovered.value || nukeHovered.value) {
+//     routerHovered.value = false;
+//     networkHovered.value = false;
+//     nukeHovered.value = false;
+//   }
+//   nukeHovered.value = true;
+// };
 </script>
 
 <style scoped>
--- launcher/src/components/UI/node-page/sections/SidebarSection.vue
+++ launcher/src/components/UI/node-page/sections/SidebarSection.vue
@@ -1,47 +1,71 @@
 <template>
-  <aside class="flex flex-col items-center w-18 h-full custom-gradient" @pointerdown.prevent.stop @mousedown.prevent.stop>
+  <aside
+    class="flex flex-col items-center w-18 h-full custom-gradient"
+    @pointerdown.prevent.stop
+    @mousedown.prevent.stop
+  >
     <div class="w-full max-h-[144px] grid grid-rows-3 mt-20 p-1 gap-y-5">
       <div
-        class="col-span-1 row-start-1 row-end-2 p-1 rounded-md text-gray-700 focus:outline-nones transition-colors duration-200 hover:bg-[#23272a] flex justify-center items-center cursor-pointer"
-        @click="hoverRouter"
+        class="col-span-1 row-start-1 row-end-2 p-1 rounded-md text-gray-700 bg-[#23272a] flex justify-center items-center scale-90 hover:scale-105 active:scale-100 border border-[#23272a] hover:border-gray-500 duration-150 hover:shadow-md shadow-[#23272a] transition-all ease-in-out"
         @mouseenter="footerStore.cursorLocation = `${toEdit}`"
         @mouseleave="footerStore.cursorLocation = ''"
       >
-        <img class="w-7" src="/img/icon/node-page-icons/edit-node-icon.png" alt="Manage Icon" />
+        <router-link
+          to="/edit"
+          class="w-full h-full flex justify-between items-center"
+          @mouseleave="routerHovered = false"
+        >
+          <img
+            class="w-6 mr-1"
+            src="/img/icon/node-page-icons/edit-node-icon.png"
+            alt="Manage Icon"
+          />
+        </router-link>
       </div>
-      <Transition name="slide-fade">
+      <!-- <Transition name="slide-fade">
         <router-link
           v-if="routerHovered"
           to="/edit"
           class="w-fit h-9 absolute col-span-1 row-start-1 row-end-2 py-1 px-2 rounded-md bg-gray-700 border border-gray-500 flex justify-between items-center z-10 space-x-2 ml-1 transition duration-200 shadow-md shadow-[#23272a]"
           @mouseleave="routerHovered = false"
         >
-          <img class="w-6 mr-1" src="/img/icon/node-page-icons/edit-node-icon.png" alt="Manage Icon" />
+          <img
+            class="w-6 mr-1"
+            src="/img/icon/node-page-icons/edit-node-icon.png"
+            alt="Manage Icon"
+          />
           <span class="text-sm text-gray-200 font-semibold">{{ toEdit }}</span>
         </router-link>
-      </Transition>
+      </Transition> -->
 
-      <button v-if="isloading" class="row-start-2 row-end-3 p-1 rounded-md relative flex justify-center items-center">
-        <img v-if="loading" src="/animation/loading/turning-circle-blue.gif" alt="loading" />
+      <button
+        v-if="isloading"
+        class="col-span-1 row-start-2 row-end-3 p-1 rounded-md text-gray-700 bg-[#23272a] flex justify-center items-center scale-90 hover:scale-105 active:scale-100 border border-[#23272a] hover:border-gray-500 duration-150 hover:shadow-md shadow-[#23272a] transition-all ease-in-out"
+      >
+        <img
+          v-if="loading"
+          src="/animation/loading/turning-circle-blue.gif"
+          alt="loading"
+        />
       </button>
       <button
         v-else-if="checkStatus"
-        class="row-start-2 row-end-3 p-1 rounded-md transition-colors duration-200 hover:bg-[#23272a] flex justify-center items-center"
+        class="col-span-1 row-start-2 row-end-3 p-1 rounded-md text-gray-700 bg-[#23272a] flex justify-center items-center scale-90 hover:scale-105 active:scale-100 border border-[#23272a] hover:border-gray-500 duration-150 hover:shadow-md shadow-[#23272a] transition-all ease-in-out"
         @click="hoverPower"
         @mouseenter="footerStore.cursorLocation = `${trnOn}`"
         @mouseleave="footerStore.cursorLocation = ''"
       >
-        <img class="w-5" src="/img/icon/node-page-icons/turn-on.png" alt="Stop Icon" />
+        <img class="w-4" src="/img/icon/node-page-icons/turn-on.png" alt="Stop Icon" />
       </button>
 
       <button
         v-else-if="!checkStatus"
-        class="row-start-2 row-end-3 p-1 rounded-md text-gray-700 focus:outline-nones transition-colors duration-200 hover:bg-[#23272a] flex justify-center items-center"
+        class="col-span-1 row-start-2 row-end-3 p-1 rounded-md text-gray-700 bg-[#23272a] flex justify-center items-center scale-90 hover:scale-105 active:scale-100 border border-[#23272a] hover:border-gray-500 duration-150 hover:shadow-md shadow-[#23272a] transition-all ease-in-out"
         @click="hoverPower"
         @mouseenter="footerStore.cursorLocation = `${trnOff}`"
         @mouseleave="footerStore.cursorLocation = ''"
       >
-        <img class="w-5" src="/img/icon/node-page-icons/turn-off.png" alt="Stop Icon" />
+        <img class="w-4" src="/img/icon/node-page-icons/turn-off.png" alt="Stop Icon" />
       </button>
       <Transition name="slide-fade">
         <button
@@ -50,7 +74,11 @@
           @mouseleave="powerHovered = false"
           @click="showPowerModal"
         >
-          <img class="w-4 mr-1" src="/img/icon/node-page-icons/turn-on.png" alt="Stop Icon" />
+          <img
+            class="w-4 mr-1"
+            src="/img/icon/node-page-icons/turn-on.png"
+            alt="Stop Icon"
+          />
           <span class="text-sm text-gray-200 font-semibold">{{ trnOn }}</span>
         </button>
 
@@ -60,30 +88,27 @@
           @mouseleave="powerHovered = false"
           @click="showPowerModal"
         >
-          <img class="w-4 mr-1" src="/img/icon/node-page-icons/turn-off.png" alt="Stop Icon" />
+          <img
+            class="w-4 mr-1"
+            src="/img/icon/node-page-icons/turn-off.png"
+            alt="Stop Icon"
+          />
           <span class="text-xs text-gray-200">{{ trnOff }}</span>
         </button>
       </Transition>
-      <Transition name="slide-fade">
-        <button
-          v-if="!exportHovered"
-          class="row-start-3 row-end-4 p-1 rounded-md text-gray-700 focus:outline-nones transition-colors duration-200 hover:bg-[#23272a] flex justify-center items-center"
-          @click="hoverExport"
-          @mouseenter="footerStore.cursorLocation = `${expNode}`"
-          @mouseleave="footerStore.cursorLocation = ''"
-        >
-          <img class="w-5" src="/img/icon/node-page-icons/export-config-icon.png" alt="Export Icon" />
-        </button>
-        <button
-          v-else
-          class="w-fit h-9 absolute row-start-2 row-end-3 py-1 px-2 rounded-md duration-200 bg-gray-700 border border-gray-500 flex justify-between items-center z-10 space-x-2 ml-1 top-[42%]"
-          @mouseleave="exportHovered = false"
-          @click="exportData"
-        >
-          <img class="w-4" src="/img/icon/node-page-icons/export-config-icon.png" alt="Export Icon" />
-          <span class="text-xs text-gray-200 font-semibold">{{ expNode }}</span>
-        </button>
-      </Transition>
+
+      <button
+        class="col-span-1 row-start-3 row-end-4 p-1 rounded-md text-gray-700 bg-[#23272a] flex justify-center items-center scale-90 hover:scale-105 active:scale-100 border border-[#23272a] hover:border-gray-500 duration-150 hover:shadow-md shadow-[#23272a] transition-all ease-in-out"
+        @click="exportData"
+        @mouseenter="footerStore.cursorLocation = `${expNode}`"
+        @mouseleave="footerStore.cursorLocation = ''"
+      >
+        <img
+          class="w-4"
+          src="/img/icon/node-page-icons/export-config-icon.png"
+          alt="Export Icon"
+        />
+      </button>
     </div>
     <StateModal
       v-if="nodeStore.runNodePowerModal"
@@ -136,7 +161,9 @@ const isloading = computed({
 });
 
 const checkStatus = computed(() => {
-  let servicesToManage = serviceStore.installedServices.filter((service) => service.name !== "Notifications");
+  let servicesToManage = serviceStore.installedServices.filter(
+    (service) => service.name !== "Notifications"
+  );
   return !servicesToManage.some((s) => s.state == "running");
 });
 
@@ -155,7 +182,9 @@ const stateButtonHandler = async (state) => {
   closeUpdatePowerStateModal();
   try {
     //this is the temporary solution until notification service is exiting correctly
-    let servicesToManage = serviceStore.installedServices.filter((service) => service.name !== "Notifications");
+    let servicesToManage = serviceStore.installedServices.filter(
+      (service) => service.name !== "Notifications"
+    );
 
     let promises = servicesToManage.map(async (service, index) => {
       new Promise((resolve) => setTimeout(resolve, index * 1000)).then(() => {
@@ -203,14 +232,14 @@ const exportData = async () => {
   }
 };
 
-const hoverRouter = () => {
-  if (routerHovered.value || exportHovered.value || powerHovered.value) {
-    routerHovered.value = false;
-    exportHovered.value = false;
-    powerHovered.value = false;
-  }
-  routerHovered.value = true;
-};
+// const hoverRouter = () => {
+//   if (routerHovered.value || exportHovered.value || powerHovered.value) {
+//     routerHovered.value = false;
+//     exportHovered.value = false;
+//     powerHovered.value = false;
+//   }
+//   routerHovered.value = true;
+// };
 
 const hoverPower = () => {
   if (routerHovered.value || exportHovered.value || powerHovered.value) {
@@ -221,14 +250,14 @@ const hoverPower = () => {
   powerHovered.value = true;
 };
 
-const hoverExport = () => {
-  if (routerHovered.value || exportHovered.value || powerHovered.value) {
-    routerHovered.value = false;
-    exportHovered.value = false;
-    powerHovered.value = false;
-  }
-  exportHovered.value = true;
-};
+// const hoverExport = () => {
+//   if (routerHovered.value || exportHovered.value || powerHovered.value) {
+//     routerHovered.value = false;
+//     exportHovered.value = false;
+//     powerHovered.value = false;
+//   }
+//   exportHovered.value = true;
+// };
 </script>
 
 <style scoped>
