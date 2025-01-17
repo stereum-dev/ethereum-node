97b2917b49f9760e9c42404aaa5faec99d261fbb
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2025-01-17 10:38:27 +0100

    FIX: undefined values handling

--- launcher/src/components/UI/control-page/components/widgets/ChartCpu.vue
+++ launcher/src/components/UI/control-page/components/widgets/ChartCpu.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="theCpuParent flex w-full h-full justify-center items-center relative">
     <div class="cpuIco w-1/3 h-full flex flex-col justify-center items-center">
-      <div class="cpuIco-container flex justify-center items-center w-full h-4/5 relative p-2">
+      <div
+        class="cpuIco-container flex justify-center items-center w-full h-4/5 relative p-2"
+      >
         <img
           class="w-3/4"
           src="/img/icon/control-page-icons/cpuIcon.svg"
           :style="{
             filter:
-              controlStore.tempCPU === null || controlStore.tempCPU === undefined || controlStore.tempCPU === ''
+              controlStore.tempCPU === null ||
+              controlStore.tempCPU === undefined ||
+              controlStore.tempCPU === ''
                 ? ''
                 : tempCPU === 'green'
                 ? 'invert(40%) sepia(90%) saturate(500%) hue-rotate(90deg)'
@@ -24,10 +28,21 @@
           >{{ controlStore.tempCPU }}</span
         >
       </div>
-      <span class="w-full h-1/5 flex justify-center items-center text-gray-200 text-2xs font-semibold uppercase">cpu</span>
+      <span
+        class="w-full h-1/5 flex justify-center items-center text-gray-200 text-2xs font-semibold uppercase"
+        >cpu</span
+      >
     </div>
-    <div v-if="chartSeries && chartOptions" class="cpuCountPart w-2/3 h-full flex justify-start items-center pr-1">
-      <VueApexCharts v-if="chartSeries" :options="chartOptions" :series="chartSeries" class="full-size-chart" />
+    <div
+      v-if="chartSeries && chartOptions"
+      class="cpuCountPart w-2/3 h-full flex justify-start items-center pr-1"
+    >
+      <VueApexCharts
+        v-if="chartSeries"
+        :options="chartOptions"
+        :series="chartSeries"
+        class="full-size-chart"
+      />
     </div>
   </div>
 </template>
@@ -50,7 +65,7 @@ let pollingInterval;
 const chartSeries = computed(() => [
   {
     name: "CPU Usage",
-    data: chartData.value.length ? chartData.value : [[Date.now(), 0]],
+    data: chartData.value?.length ? chartData.value : [[Date.now(), 0]],
   },
 ]);
 
@@ -127,7 +142,7 @@ const updateChartData = () => {
   const currentTime = Date.now();
   chartData.value.push([currentTime, cpu.value]);
 
-  if (chartData.value.length > 200) {
+  if (chartData.value?.length > 200) {
     chartData.value.shift();
   }
 };
