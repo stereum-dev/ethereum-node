dcd1fa057a8625ad895548a1a21b71f028f9e18e
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2025-01-23 16:29:16 +0100

    FIX: obol participation row

--- launcher/src/components/UI/staking-page/components/management/components/val-rewards/ParticipationRow.vue
+++ launcher/src/components/UI/staking-page/components/management/components/val-rewards/ParticipationRow.vue
@@ -3,7 +3,7 @@
     <div class="h-full col-start-1 col-end-4 self-center flex justify-start items-center px-2 rounded-l-full">
       <span class="text-[8px] text-amber-300 font-normal font-sans text-left">PARTICIPATION</span>
     </div>
-    <div class="w-full bg-black h-full max-h-4 col-start-4 col-span-full rounded-r-full self-center flex justify-center items-center px-1">
+    <div class="w-full bg-black h-full max-h-4 col-start-4 col-span-full rounded-full self-center flex justify-center items-center px-1">
       <span class="text-[8px] text-gray-300 font-normal">{{ getObolParticipation }}</span>
     </div>
   </div>
