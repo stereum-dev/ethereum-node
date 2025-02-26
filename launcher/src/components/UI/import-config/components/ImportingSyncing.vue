9e90c753b8f2a7df8102cfccdfe0317540237b1a
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2025-02-26 15:34:29 +0100

    FIX: consensus sync on import configs

--- launcher/src/components/UI/import-config/components/ImportingSyncing.vue
+++ launcher/src/components/UI/import-config/components/ImportingSyncing.vue
@@ -1,16 +1,32 @@
 <template>
-  <div class="w-full h-full col-start-1 col-span-full row-start-1 row-span-full grid grid-cols-24 grid-rows-12 p-1">
+  <div
+    class="w-full h-full col-start-1 col-span-full row-start-1 row-span-full grid grid-cols-24 grid-rows-12 p-1"
+  >
     <div
       class="w-full h-full col-start-5 col-end-21 row-start-3 row-end-11 grid grid-cols-12 grid-rows-12 p-2 mx-auto bg-[#1E2429] rounded-md"
     >
-      <div class="col-start-1 col-span-full row-start-1 row-span-1 rounded-md py-1 px-2 flex justify-between items-center">
-        <span class="text-sm text-gray-200 font-normal uppercase">{{ $t("importingSyncing.message") }}</span>
+      <div
+        class="col-start-1 col-span-full row-start-1 row-span-1 rounded-md py-1 px-2 flex justify-between items-center"
+      >
+        <span class="text-sm text-gray-200 font-normal uppercase">{{
+          $t("importingSyncing.message")
+        }}</span>
       </div>
-      <div class="col-start-1 col-span-full row-start-2 row-span-1 overflow-x-hidden grid grid-cols-6 bg-teal-600 px-4 py-1 rounded-md">
-        <span class="col-start-1 col-span-2 text-sm text-gray-200 font-semibold uppercase">{{ $t("importingSyncing.client") }}</span>
-        <span class="col-start-4 col-span-2 text-sm text-gray-200 font-semibold uppercase">{{ $t("importingSyncing.syncSrc") }}</span>
+      <div
+        class="col-start-1 col-span-full row-start-2 row-span-1 overflow-x-hidden grid grid-cols-6 bg-teal-600 px-4 py-1 rounded-md"
+      >
+        <span
+          class="col-start-1 col-span-2 text-sm text-gray-200 font-semibold uppercase"
+          >{{ $t("importingSyncing.client") }}</span
+        >
+        <span
+          class="col-start-4 col-span-2 text-sm text-gray-200 font-semibold uppercase"
+          >{{ $t("importingSyncing.syncSrc") }}</span
+        >
       </div>
-      <div class="col-start-1 col-span-full row-start-3 row-span-full rounded-md p-1 grid grid-cols-6 grid-rows-5 gap-2">
+      <div
+        class="col-start-1 col-span-full row-start-3 row-span-full rounded-md p-1 grid grid-cols-6 grid-rows-5 gap-2"
+      >
         <div
           v-if="configServices.some((s) => s.category === 'execution')"
           class="w-full h-full col-start-1 col-span-full row-start-2 row-span-1"
@@ -56,7 +72,9 @@ export default {
     // eslint-disable-next-line vue/return-in-computed-property
     nextRouteHandler() {
       if (!this.btnActive) {
-        return this.checkPointSync !== "" || !this.checkPointSync ? "importingVerify" : "disabled";
+        return this.checkPointSync !== "" || !this.checkPointSync
+          ? "importingVerify"
+          : "disabled";
       } else if (this.btnActive) {
         return "importingVerify";
       }
@@ -91,8 +109,13 @@ export default {
       }
     },
     filterServices() {
-      this.executionClient = this.configServices.filter((service) => service.category === "execution")[0];
-      this.consensusClient = this.configServices.filter((service) => service.category === "consensus")[0];
+      this.executionClient = this.configServices.filter(
+        (service) => service.category === "execution"
+      )[0];
+
+      this.consensusClient = this.configServices.filter(
+        (service) => service.category === "consensus"
+      )[0];
     },
   },
 };
