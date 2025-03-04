6fbbb046ecbf93a16ae0f1cb2b382999f52b27c2
gbayasgalan <73820006+gbayasgalan@users.noreply.github.com>
2025-03-04 17:44:44 +0100

    FIX: format

--- launcher/src/components/UI/one-click/components/sync/SyncFooter.vue
+++ launcher/src/components/UI/one-click/components/sync/SyncFooter.vue
@@ -1,24 +1,17 @@
 <template>
-  <div
-    class="w-full h-full col-start-1 col-span-full row-start-11 row-span-2 grid grid-cols-12 gird-rows-2 px-4"
-    @mousedown.prevent
-  >
+  <div class="w-full h-full col-start-1 col-span-full row-start-11 row-span-2 grid grid-cols-12 gird-rows-2 px-4" @mousedown.prevent>
     <router-link
       :to="backRoute"
       class="col-start-1 col-span-2 row-start-1 row-span-2 justify-self-end w-[120px] h-12 bg-[#264744] hover:bg-[#447a75] rounded-full py-2 px-4 flex justify-center items-center hover:scale-110 hover:shadow-lg hover:shadow-[#1e2a29] transition-all duration-300 ease-in-out active:scale-100 active:shadow-none"
     >
-      <span class="text-gray-200 text-xl font-semibold text-center uppercase">{{
-        $t("customFooter.back")
-      }}</span>
+      <span class="text-gray-200 text-xl font-semibold text-center uppercase">{{ $t("customFooter.back") }}</span>
     </router-link>
     <router-link
       :to="nextRoute"
       :class="{ 'opacity-50 pointer-events-none ': disabledButton }"
       class="col-start-11 col-span-1 row-start-1 row-span-2 w-[120px] h-12 bg-[#264744] hover:bg-[#447a75] rounded-full py-2 px-4 flex justify-center items-center hover:scale-110 hover:shadow-lg hover:shadow-[#1e2a29] transition-all duration-300 ease-in-out active:scale-100 active:shadow-none cursor-pointer"
     >
-      <span class="text-gray-200 text-xl font-semibold text-center uppercase">{{
-        $t("customFooter.next")
-      }}</span>
+      <span class="text-gray-200 text-xl font-semibold text-center uppercase">{{ $t("customFooter.next") }}</span>
     </router-link>
   </div>
 </template>
@@ -41,9 +34,7 @@ const disabledButton = computed(() => {
 });
 
 const monitoringMevboost = computed(() => {
-  return installStore.selectedPreset?.includedPlugins.some(
-    (plugin) => plugin.service === "FlashbotsMevBoostService"
-  );
+  return installStore.selectedPreset?.includedPlugins.some((plugin) => plugin.service === "FlashbotsMevBoostService");
 });
 
 watchEffect(() => {
