b91d64c7a97691195308c6626c7e4e0f2ef3c356
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2025-03-25 16:45:23 +0100

    REFACTOR: drawer services for op

--- launcher/src/components/UI/edit-page/components/drawer/ServicesDrawer.vue
+++ launcher/src/components/UI/edit-page/components/drawer/ServicesDrawer.vue
@@ -95,7 +95,7 @@ const filteredServices = computed(() => {
       return service?.service !== "CustomService";
     })
     .filter((service) => {
-      const opServices = ["L2GethService", "OpNodeBeaconService", "OpGethService"];
+      const opServices = ["L2GethService", "OpNodeBeaconService", "OpGethService", "OpErigonService", "OpRethService"];
       if (setupStore.selectedSetup?.setupType === "OP") {
         return opServices.includes(service?.service);
       } else {
