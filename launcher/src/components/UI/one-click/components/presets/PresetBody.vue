492bb9756f3bf56a962738603831e7e7bcd892f9
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2025-04-03 14:12:42 +0200

    FIX: format

--- launcher/src/components/UI/one-click/components/presets/PresetBody.vue
+++ launcher/src/components/UI/one-click/components/presets/PresetBody.vue
@@ -1,21 +1,11 @@
 import { ref, onMounted, watch } from 'vue';
 <template>
-  <div
-    class="w-full h-full col-start-1 col-span-full row-start-3 row-end-11 grid grid-cols-12 grid-rows-7 p-2 mx-auto"
-  >
-    <div
-      class="w-full h-full col-start-3 col-end-11 row-start-1 row-span-full bg-[#1E2429] rounded-md grid grid-cols-12 grid-rows-7 p-2"
-    >
-      <div
-        class="col-start-1 col-span-full row-start-1 row-span-1 flex justify-center items-center"
-      >
-        <span class="text-center text-gray-200 text-sm uppercase">{{
-          $t("oneClick.chooseNetwork")
-        }}</span>
+  <div class="w-full h-full col-start-1 col-span-full row-start-3 row-end-11 grid grid-cols-12 grid-rows-7 p-2 mx-auto">
+    <div class="w-full h-full col-start-3 col-end-11 row-start-1 row-span-full bg-[#1E2429] rounded-md grid grid-cols-12 grid-rows-7 p-2">
+      <div class="col-start-1 col-span-full row-start-1 row-span-1 flex justify-center items-center">
+        <span class="text-center text-gray-200 text-sm uppercase">{{ $t("oneClick.chooseNetwork") }}</span>
       </div>
-      <div
-        class="col-start-1 col-span-full row-start-2 row-span-full grid grid-cols-12 grid-rows-7 pt-5"
-      >
+      <div class="col-start-1 col-span-full row-start-2 row-span-full grid grid-cols-12 grid-rows-7 pt-5">
         <div
           class="col-start-4 col-span-6 row-start-1 row-span-1 bg-gray-200 rounded-md grid grid-cols-6 cursor-pointer"
           @click="dropdownHandler"
@@ -26,10 +16,9 @@ import { ref, onMounted, watch } from 'vue';
             :src="displayItem?.icon"
             alt="Arrow icon"
           />
-          <span
-            class="col-start-2 col-end-6 justify-self-center self-center text-center text-gray-800 text-lg font-semibold"
-            >{{ displayItem?.name ? displayItem?.name : displayItem }}</span
-          >
+          <span class="col-start-2 col-end-6 justify-self-center self-center text-center text-gray-800 text-lg font-semibold">{{
+            displayItem?.name ? displayItem?.name : displayItem
+          }}</span>
 
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -54,11 +43,7 @@ import { ref, onMounted, watch } from 'vue';
               :class="item?.state === 'disabled' ? 'pointer-events-none opacity-50' : ''"
               @click="getNetwork(item)"
             >
-              <img
-                class="h-[30px] col-start-1 col-end-2 self-center justify-self-center"
-                :src="item.icon"
-                alt="service Icon"
-              />
+              <img class="h-[30px] col-start-1 col-end-2 self-center justify-self-center" :src="item.icon" alt="service Icon" />
               <span
                 class="col-start-3 col-end-6 px-4 py-1 flex justify-start items-center outline-0 whitespace-nowrap cursor-pointer text-lg text-gray-200 font-semibold"
                 >{{ item.name }}</span
@@ -74,9 +59,7 @@ import { ref, onMounted, watch } from 'vue';
             :key="preset.name"
             class="col-span-1 row-span-1 justify-self-center self-center hover:border hover:border-teal-500 rounded-md hover:shadow-lg hover:shadow-[#050505] transition-all duration-300 ease-in-out active:scale-100 active:shadow-none cursor-pointer"
             :class="{
-              hidden:
-                !manageStore.currentNetwork?.support?.includes(preset.name) ||
-                !displayItem?.name,
+              hidden: !manageStore.currentNetwork?.support?.includes(preset.name) || !displayItem?.name,
             }"
             @click="getPreset(preset)"
           >
@@ -138,10 +121,7 @@ const getNetwork = (network) => {
 };
 
 const getPresetIcon = (preset) => {
-  if (
-    clickStore.selectedNetwork?.network === "op-sepolia" &&
-    preset?.name === "op and eth full node"
-  ) {
+  if (clickStore.selectedNetwork?.network === "op-sepolia" && preset?.name === "op and eth full node") {
     return preset.sepIcon;
   } else {
     return preset.icon;
