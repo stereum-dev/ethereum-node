1f9ebec978a919751623ab4683e626eaf2751310
Neo <80448387+NeoPlays@users.noreply.github.com>
2025-09-26 13:39:02 +0200

    CHORE: update relay list (#2279)

--- launcher/src/store/nodeManage.js
+++ launcher/src/store/nodeManage.js
@@ -47,8 +47,6 @@ export const useNodeManage = defineStore("nodeManage", {
             "https://0xac6e77dfe25ecd6110b8e780608cce0dab71fdd5ebea22a16c0205200f2f8e2e3ad3b71d3499c54ad14d6c21b41a37ae@boost-relay.flashbots.net",
           sepolia:
             "https://0x845bd072b7cd566f02faeb0a4033ce9399e42839ced64e8b2adcfc859ed1e8e1a5a293336a49feac6d9a5edb779be53a@boost-relay-sepolia.flashbots.net",
-          holesky:
-            "https://0xafa4c6985aa049fb79dd37010438cfebeb0f2bd42b115b89dd678dab0670c1de38da0c4e9138c9290a398ecd9a0b3110@boost-relay-holesky.flashbots.net",
           hoodi:
             "https://0xafa4c6985aa049fb79dd37010438cfebeb0f2bd42b115b89dd678dab0670c1de38da0c4e9138c9290a398ecd9a0b3110@boost-relay-hoodi.flashbots.net",
           id: 1,
@@ -61,7 +59,6 @@ export const useNodeManage = defineStore("nodeManage", {
           name: "BloXroute MAX PROFIT",
           mainnet:
             "https://0x8b5d2e73e2a3a55c6c87b8b6eb92e0149a125c852751db1422fa951e42a09b82c142c3ea98d0d9930b056a3bc9896b8f@bloxroute.max-profit.blxrbdn.com",
-
           id: 2,
           isSelected: false,
           isRemoved: false,
@@ -82,7 +79,6 @@ export const useNodeManage = defineStore("nodeManage", {
           name: "Manifold",
           mainnet:
             "https://0x98650451ba02064f7b000f5768cf0cf4d4e492317d82871bdc87ef841a0743f69f0f1eea11168503240ac35d101c9135@mainnet-relay.securerpc.com",
-
           id: 6,
           isSelected: false,
           isRemoved: false,
@@ -106,8 +102,6 @@ export const useNodeManage = defineStore("nodeManage", {
           icon: "/img/icon/one-click-icons/mevboost-icons/aestus.png",
           name: "Aestus",
           mainnet: "https://0xa15b52576bcbf1072f4a011c0f99f9fb6c66f3e1ff321f11f461d15e31b1cb359caa092c71bbded0bae5b5ea401aab7e@aestus.live",
-          holesky:
-            "https://0xab78bf8c781c58078c3beb5710c57940874dd96aef2835e7742c866b4c7c0406754376c2c8285a36c630346aa5c5f833@holesky.aestus.live",
           hoodi:
             "https://0x98f0ef62f00780cf8eb06701a7d22725b9437d4768bb19b363e882ae87129945ec206ec2dc16933f31d983f8225772b6@hoodi.aestus.live",
           id: 8,
@@ -120,7 +114,6 @@ export const useNodeManage = defineStore("nodeManage", {
           name: "Agnostic",
           mainnet:
             "https://0xa7ab7a996c8584251c8f925da3170bdfd6ebc75d50f5ddc4050a6fdc77f2a3b5fce2cc750d0865e05d7228af97d69561@agnostic-relay.net",
-
           id: 9,
           isSelected: false,
           isRemoved: false,
@@ -151,14 +144,16 @@ export const useNodeManage = defineStore("nodeManage", {
           freeCensorship: false,
         },
         {
-          icon: "/img/icon/one-click-icons/mevboost-icons/bloxroute.png",
-          name: "BloXroute",
-          holesky:
-            "https://0x821f2a65afb70e7f2e820a925a9b4c80a159620582c1766b1b09729fec178b11ea22abb3a51f07b288be815a1a2ff516@bloxroute.holesky.blxrbdn.com",
+          icon: "/img/icon/one-click-icons/mevboost-icons/ultra-sound-relay.png",
+          name: "Ultra Sound Money - Filtering",
+          mainnet:
+            "https://0xa1559ace749633b997cb3fdacffb890aeebdb0f5a3b6aaa7eeeaf1a38af0a8fe88b9e4b1f61f236d2e64d95733327a62@relay-filtered.ultrasound.money",
+          hoodi:
+            "https://0xb1559beef7b5ba3127485bbbb090362d9f497ba64e177ee2c8e7db74746306efad687f2cf8574e38d70067d40ef136dc@relay-filtered-hoodi.ultrasound.money",
           id: 12,
           isSelected: false,
           isRemoved: false,
-          freeCensorship: true,
+          freeCensorship: false,
         },
       ],
 
