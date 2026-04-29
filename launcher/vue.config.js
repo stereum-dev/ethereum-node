e8525d97f8c0139fcc7605ee0451fd2bc6dffd64
Neo <80448387+NeoPlays@users.noreply.github.com>
2026-04-29 13:40:00 +0200

    Update x64ArchFiles pattern in vue.config.js

--- launcher/vue.config.js
+++ launcher/vue.config.js
@@ -31,7 +31,7 @@ module.exports = {
           entitlements: "./node_modules/@sapien99/vue-cli-plugin-electron-builder-notarize/entitlements.mac.inherit.plist",
           gatekeeperAssess: false,
           artifactName: "Stereum-Launcher-${version}.${ext}",
-          x64ArchFiles: "**/node_modules/cpu-features/build/Release/cpufeatures.node",
+          x64ArchFiles: "**/*.node",
         },
         win: {
           signtoolOptions: {
